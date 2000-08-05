18cd1fc7a969380f3db41310390b4aff1621d2da
Harlan Stenn <stenn@ntp.org>
2000-08-04 23:45:23 -0500

    Many files:
    * configure.in: AC_REPLACE_FUNCS(strdup)
    * libntp/strdup.c (strdup): Added.
    * libntp/Makefile.am (EXTRA_libntp_a_SOURCES): Added strdup.c

---
+++ libntp/strdup.c
@@ -0,0 +1,17 @@
+char *
+strdup(
+	const char *s
+	)
+{
+        char *cp;
+
+        if (s) {
+                cp = (char *) malloc((unsigned) (strlen(s)+1));
+                if (cp) {
+                        (void) strcpy(cp, s);
+		}
+        } else {
+                cp = (char *) NULL;
+	}
+        return(cp);
+}
