db8fe157dd9932c249f740232fe6f896751866aa
Harlan Stenn <stenn@ntp.org>
2002-02-19 03:17:33 -0500

    TrueTime 560 refclock. Some configure.in cleanup.

--- libntp/clocktypes.c
+++ libntp/clocktypes.c
@@ -90,8 +90,10 @@ struct clktype clktypes[] = {
 	  "HOPF_S"},
 	{ REFCLK_HOPF_PCI,	"hopf Elektronic PCI receiver (39)",
 	  "HOPF_P"},
-	{ REFCLK_JJY,	"JJY receiver (40)",
-	  "JJY" },
+	{ REFCLK_JJY,		"JJY receiver (40)",
+	  "JJY"},
+	{ REFCLK_TT560,		"TrueTime 560 IRIG-B decoder (40)",
+	  "TT_IRIG"},
 	{ -1,			"", "" }
 };
 
