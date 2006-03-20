684e9949e1636723cbc7cbbde443dc5c98d17bd2
Danny Mayer <mayer@ntp.org>
2006-03-20 03:14:48 -0500

    Additional fixes for Coverity items

--- ntpd/ntp_restrict.c
+++ ntpd/ntp_restrict.c
@@ -527,7 +527,7 @@ hack_restrict(
 				rl6->addr6 = addr6;
 				rl6->mask6 = mask6;
 				rl6->mflags = (u_short)mflags;
-				if (rlprev6) {
+				if (rlprev6 != NULL) {
 					rl6->next = rlprev6->next;
 					rlprev6->next = rl6;
 				} else {
@@ -569,7 +569,7 @@ hack_restrict(
 			if (rl6 != 0 &&
 			    !IN6_IS_ADDR_UNSPECIFIED(&rl6->addr6)
 			    && !(rl6->mflags & RESM_INTERFACE)) {
-				if (rlprev6) {
+				if (rlprev6 != NULL) {
 					rlprev6->next = rl6->next;
 				} else {
 					restrictlist6 = rl6->next;
