9823e8859ab6bdccf2ccb878d4a26087ee3c16f2
Harlan Stenn <stenn@ntp.org>
2006-04-09 09:00:52 -0500

    New release numbering style support

---
+++ packageinfo.sh
@@ -0,0 +1,17 @@
+# version=Protocol.Major.Minor
+# odd minor numbers are for -dev, even minor numbers are for -stable
+proto=4
+major=2
+minor=1
+version=${proto}.${major}.${minor}
+# Point.  3 cases:
+# - Numeric values increment
+# - empty 'increments' to 1
+# - NEW (for example) 'increments' to empty
+point=1
+# Special.  Normally unused.  A suffix.
+#special=ag
+special=
+# ReleaseCandidate. 'yes' or 'no'.
+releasecandidate=yes
+#releasecandidate=no
