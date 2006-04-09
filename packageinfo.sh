974ae8dd6266057ca170f204430c22f4753f6806
Harlan Stenn <stenn@ntp.org>
2006-04-09 02:13:54 -0500

    update

--- packageinfo.sh
+++ packageinfo.sh
@@ -1,4 +1,14 @@
+# version=Protocol.Major.Minor
+# odd minor numbers are for -dev, even minor numbers are for -stable
 version=4.2.1
-point=
+# Point.  3 cases:
+# - Numeric values increment
+# - empty 'increments' to 1
+# - NEW (for example) 'increments' to empty
+point=1
+# Special.  Normally unused.  A suffix.
 special=ag
+#special=
+# ReleaseCandidate. 'yes' or 'no'.
+releasecandidate=yes
 releasecandidate=no
