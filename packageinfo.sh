9c0c3a5997779e29456233bf686aa58c147ec78f
Harlan Stenn <stenn@ntp.org>
2006-04-12 08:39:22 -0500

    update

--- packageinfo.sh
+++ packageinfo.sh
@@ -2,7 +2,7 @@
 # odd minor numbers are for -dev, even minor numbers are for -stable
 proto=4
 major=2
-minor=2
+minor=1
 version=${proto}.${major}.${minor}
 # Point.  3 cases:
 # - Numeric values increment
@@ -13,7 +13,7 @@ point=NEW
 #special=ag
 special=
 # ReleaseCandidate. 'yes' or 'no'.
-#releasecandidate=yes
-releasecandidate=no
+releasecandidate=yes
+#releasecandidate=no
 # ChangeLog tag
 CLTAG=NTP_4_2_0
