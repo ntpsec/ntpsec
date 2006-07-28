9fa11a5ef54a03f7d685f4d2b080b58adca6524b
Harlan Stenn <stenn@ntp.org>
2006-07-28 05:20:54 -0500

    Ready for release

--- packageinfo.sh
+++ packageinfo.sh
@@ -14,8 +14,8 @@ point=3
 #special=ag
 special=
 # ReleaseCandidate. 'yes' or 'no'.
-releasecandidate=yes
-#releasecandidate=no
+#releasecandidate=yes
+releasecandidate=no
 # ChangeLog tag
 CLTAG=NTP_4_2_0
 ###
@@ -23,4 +23,4 @@ CLTAG=NTP_4_2_0
 # - Numeric values increment
 # - empty 'increments' to 1
 # - NEW 'increments' to empty
-rcpoint=2
+rcpoint=NEW
