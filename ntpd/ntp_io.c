d0853ea7b5f811a7a5238389de9f4e62834f3635
Frank Kardel <kardel@informatik.uni-erlangen.de>
2006-11-05 08:28:53 -0500

    ntp_io.c:  pass correct address length for setsockopt
    (..., IP_MULTICAST_IF, ...)  (sizeof(struct sockaddr_in*) != sizeof(struct in_addr) on 64-bit platforms)

--- ntpd/ntp_io.c
+++ ntpd/ntp_io.c
@@ -1633,7 +1633,7 @@ enable_multicast_if(struct interface *iface, struct sockaddr_storage *maddr)
 	case AF_INET:
 		if (setsockopt(iface->fd, IPPROTO_IP, IP_MULTICAST_IF,
 		   (char *)&(((struct sockaddr_in*)&iface->sin)->sin_addr.s_addr),
-		    sizeof(struct sockaddr_in*)) == -1) {
+		    sizeof(struct in_addr)) == -1) {
 			netsyslog(LOG_ERR,
 			"setsockopt IP_MULTICAST_IF failure: %m on socket %d, addr %s for multicast address %s",
 			iface->fd, stoa(&iface->sin), stoa(maddr));
