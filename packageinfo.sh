8a269ff1c2a2dbe6a05fdb809503a2ddbff80fd5
Harlan Stenn <stenn@ntp.org>
2006-12-28 01:50:07 -0500

    ntp-4.2.4

--- packageinfo.sh
+++ packageinfo.sh
@@ -3,19 +3,19 @@ repotype=dev
 # odd minor numbers are for -dev, even minor numbers are for -stable
 proto=4
 major=2
-minor=3
+minor=4
 version=${proto}.${major}.${minor}
 # Point.  3 cases:
 # - Numeric values increment
 # - empty 'increments' to 1
 # - NEW 'increments' to empty
-point=70
+point=NEW
 # Special.  Normally unused.  A suffix.
 #special=ag
 special=
 # ReleaseCandidate. 'yes' or 'no'.
-releasecandidate=yes
-#releasecandidate=no
+#releasecandidate=yes
+releasecandidate=no
 # ChangeLog tag
 CLTAG=NTP_4_2_0
 ###
@@ -24,4 +24,6 @@ CLTAG=NTP_4_2_0
 # - GO triggers a release
 # - - rcpoint gets set to 0
 # - - releasecandidate gets set to no
-rcpoint=GRONK
+# - GRONK is for -dev
+rcpoint=GO
+#rcpoint=GRONK
