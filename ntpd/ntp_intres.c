5ff62ad7ee7f3e11bd3ef5d2b34274e9daf102f6
Frank Kardel <kardel@informatik.uni-erlangen.de>
2007-01-13 11:36:29 -0500

    ntp_intres.c:  support dynamic keyword

--- ntpd/ntp_intres.c
+++ ntpd/ntp_intres.c
@@ -1059,7 +1059,7 @@ readconf(
 		}
 
 		if ((intval[TOK_FLAGS] & ~(FLAG_AUTHENABLE | FLAG_PREFER |
-		    FLAG_NOSELECT | FLAG_BURST | FLAG_IBURST | FLAG_SKEY))
+		    FLAG_NOSELECT | FLAG_BURST | FLAG_IBURST | FLAG_SKEY | FLAG_DYNAMIC))
 		    != 0) {
 			msyslog(LOG_ERR, "invalid flags (%ld) in file %s",
 				intval[TOK_FLAGS], name);
@@ -1079,6 +1079,8 @@ readconf(
 		    flags |= CONF_FLAG_IBURST;
 		if (intval[TOK_FLAGS] & FLAG_SKEY)
 		    flags |= CONF_FLAG_SKEY;
+		if (intval[TOK_FLAGS] & FLAG_DYNAMIC)
+		    flags |= CONF_FLAG_DYNAMIC;
 
 		/*
 		 * This is as good as we can check it.  Add it in.
