3c8e9227eec0400be9fce8f5146e5bb7a76b373c
Harlan Stenn <stenn@ntp.org>
2007-05-26 12:05:51 -0500

    NTP_4_2_5P37

--- packageinfo.sh
+++ packageinfo.sh
@@ -9,7 +9,7 @@ version=${proto}.${major}.${minor}
 # - Numeric values increment
 # - empty 'increments' to 1
 # - NEW 'increments' to empty
-point=36
+point=37
 # Special.  Normally unused.  A suffix.
 #special=ag
 special=
