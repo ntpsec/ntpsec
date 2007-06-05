7562d385a9f51874c2a8de3ab4927149dd4c97e9
Harlan Stenn <stenn@ntp.org>
2007-06-05 11:45:25 -0500

    NTP_4_2_5P45

--- sntp/sntp-opts.c
+++ sntp/sntp-opts.c
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (sntp-opts.c)
  *  
- *  It has been AutoGen-ed  Saturday June  2, 2007 at 07:43:33 AM EDT
+ *  It has been AutoGen-ed  Tuesday June  5, 2007 at 07:44:16 AM EDT
  *  From the definitions    sntp-opts.def
  *  and the template file   options
  *
@@ -513,7 +513,7 @@ static tOptDesc optDesc[ OPTION_CT ] = {
  */
 tSCC   zPROGNAME[]   = "SNTP";
 tSCC   zUsageTitle[] =
-"sntp - standard SNTP program - Ver. 4.2.5p44\n\
+"sntp - standard SNTP program - Ver. 4.2.5p45\n\
 USAGE:  %s [ -<flag> | --<name> ]...\n";
 tSCC   zRcName[]     = ".ntprc";
 tSCC*  apzHomeList[] = {
--- util/ntp-keygen-opts.h
+++ util/ntp-keygen-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntp-keygen-opts.h)
  *  
- *  It has been AutoGen-ed  Saturday June  2, 2007 at 07:43:42 AM EDT
+ *  It has been AutoGen-ed  Tuesday June  5, 2007 at 07:44:26 AM EDT
  *  From the definitions    ntp-keygen-opts.def
  *  and the template file   options
  *
@@ -79,8 +79,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    24
-#define NTP_KEYGEN_VERSION       "4.2.5p44"
-#define NTP_KEYGEN_FULL_VERSION  "ntp-keygen (ntp) - Create a NTP host key - Ver. 4.2.5p44"
+#define NTP_KEYGEN_VERSION       "4.2.5p45"
+#define NTP_KEYGEN_FULL_VERSION  "ntp-keygen (ntp) - Create a NTP host key - Ver. 4.2.5p45"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
