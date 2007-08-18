f5cc3581619306a052d9c0f1d2d6a6fed9f24029
Harlan Stenn <stenn@ntp.org>
2007-08-18 11:46:58 -0500

    NTP_4_2_5P69

--- ntpq/ntpq-opts.c
+++ ntpq/ntpq-opts.c
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpq-opts.c)
  *  
- *  It has been AutoGen-ed  Friday August 17, 2007 at 07:44:34 AM EDT
+ *  It has been AutoGen-ed  Saturday August 18, 2007 at 07:45:17 AM EDT
  *  From the definitions    ntpq-opts.def
  *  and the template file   options
  *
@@ -410,7 +410,7 @@ static tOptDesc optDesc[ OPTION_CT ] = {
  */
 tSCC   zPROGNAME[]   = "NTPQ";
 tSCC   zUsageTitle[] =
-"ntpq - standard NTP query program - Ver. 4.2.5p68\n\
+"ntpq - standard NTP query program - Ver. 4.2.5p69\n\
 USAGE:  %s [ -<flag> [<val>] | --<name>[{=| }<val>] ]... [ host ...]\n";
 tSCC   zRcName[]     = ".ntprc";
 tSCC*  apzHomeList[] = {
--- sntp/sntp-opts.h
+++ sntp/sntp-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (sntp-opts.h)
  *  
- *  It has been AutoGen-ed  Friday August 17, 2007 at 07:45:01 AM EDT
+ *  It has been AutoGen-ed  Saturday August 18, 2007 at 07:45:45 AM EDT
  *  From the definitions    sntp-opts.def
  *  and the template file   options
  *
@@ -147,8 +147,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    13
-#define SNTP_VERSION       "4.2.5p68"
-#define SNTP_FULL_VERSION  "sntp - standard SNTP program - Ver. 4.2.5p68"
+#define SNTP_VERSION       "4.2.5p69"
+#define SNTP_FULL_VERSION  "sntp - standard SNTP program - Ver. 4.2.5p69"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
