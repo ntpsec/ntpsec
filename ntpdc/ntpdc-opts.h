ecb52ffb21e37b260ca0f87bda6c8d41a36b63df
Harlan Stenn <stenn@ntp.org>
2007-11-05 11:45:45 -0500

    NTP_4_2_5P94

--- ntpdc/ntpdc-opts.h
+++ ntpdc/ntpdc-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpdc-opts.h)
  *  
- *  It has been AutoGen-ed  Wednesday October 31, 2007 at 06:44:07 AM EST
+ *  It has been AutoGen-ed  Monday November  5, 2007 at 06:43:44 AM EST
  *  From the definitions    ntpdc-opts.def
  *  and the template file   options
  *
@@ -70,8 +70,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    15
-#define NTPDC_VERSION       "4.2.5p93"
-#define NTPDC_FULL_VERSION  "ntpdc - vendor-specific NTP query program - Ver. 4.2.5p93"
+#define NTPDC_VERSION       "4.2.5p94"
+#define NTPDC_FULL_VERSION  "ntpdc - vendor-specific NTP query program - Ver. 4.2.5p94"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
--- ntpq/ntpq-opts.c
+++ ntpq/ntpq-opts.c
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpq-opts.c)
  *  
- *  It has been AutoGen-ed  Wednesday October 31, 2007 at 06:44:38 AM EST
+ *  It has been AutoGen-ed  Monday November  5, 2007 at 06:44:16 AM EST
  *  From the definitions    ntpq-opts.def
  *  and the template file   options
  *
@@ -410,7 +410,7 @@ static tOptDesc optDesc[ OPTION_CT ] = {
  */
 tSCC   zPROGNAME[]   = "NTPQ";
 tSCC   zUsageTitle[] =
-"ntpq - standard NTP query program - Ver. 4.2.5p93\n\
+"ntpq - standard NTP query program - Ver. 4.2.5p94\n\
 USAGE:  %s [ -<flag> [<val>] | --<name>[{=| }<val>] ]... [ host ...]\n";
 tSCC   zRcName[]     = ".ntprc";
 tSCC*  apzHomeList[] = {
