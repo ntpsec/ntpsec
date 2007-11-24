1d858358676e37349b8e9bc8fea429ec148157c0
Harlan Stenn <stenn@ntp.org>
2007-11-24 11:45:27 -0500

    NTP_4_2_5P102

--- ntpdc/ntpdc-opts.c
+++ ntpdc/ntpdc-opts.c
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpdc-opts.c)
  *  
- *  It has been AutoGen-ed  Friday November 23, 2007 at 06:46:57 AM EST
+ *  It has been AutoGen-ed  Saturday November 24, 2007 at 06:43:23 AM EST
  *  From the definitions    ntpdc-opts.def
  *  and the template file   options
  *
@@ -462,7 +462,7 @@ static tOptDesc optDesc[ OPTION_CT ] = {
  */
 tSCC   zPROGNAME[]   = "NTPDC";
 tSCC   zUsageTitle[] =
-"ntpdc - vendor-specific NTP query program - Ver. 4.2.5p101\n\
+"ntpdc - vendor-specific NTP query program - Ver. 4.2.5p102\n\
 USAGE:  %s [ -<flag> [<val>] | --<name>[{=| }<val>] ]... [ host ...]\n";
 tSCC   zRcName[]     = ".ntprc";
 tSCC*  apzHomeList[] = {
