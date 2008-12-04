6c005b4f24a1cde286a1a8f6f53bb2c6bdd64042
Harlan Stenn <stenn@ntp.org>
2008-12-04 11:53:18 -0500

    NTP_4_2_5P148

--- ntpd/ntpd-opts.c
+++ ntpd/ntpd-opts.c
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpd-opts.c)
  *  
- *  It has been AutoGen-ed  Thursday November 27, 2008 at 06:38:57 AM EST
+ *  It has been AutoGen-ed  Thursday December  4, 2008 at 06:37:41 AM EST
  *  From the definitions    ntpd-opts.def
  *  and the template file   options
  *
@@ -853,7 +853,7 @@ static tOptDesc optDesc[ OPTION_CT ] = {
  */
 tSCC   zPROGNAME[]   = "NTPD";
 tSCC   zUsageTitle[] =
-"ntpd - NTP daemon program - Ver. 4.2.5p147\n\
+"ntpd - NTP daemon program - Ver. 4.2.5p148\n\
 USAGE:  %s [ -<flag> [<val>] | --<name>[{=| }<val>] ]...\n";
 #define zRcName     NULL
 #define apzHomeList NULL
--- util/ntp-keygen-opts.h
+++ util/ntp-keygen-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntp-keygen-opts.h)
  *  
- *  It has been AutoGen-ed  Thursday November 27, 2008 at 06:53:28 AM EST
+ *  It has been AutoGen-ed  Thursday December  4, 2008 at 06:52:14 AM EST
  *  From the definitions    ntp-keygen-opts.def
  *  and the template file   options
  *
@@ -78,8 +78,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    23
-#define NTP_KEYGEN_VERSION       "4.2.5p147"
-#define NTP_KEYGEN_FULL_VERSION  "ntp-keygen (ntp) - Create a NTP host key - Ver. 4.2.5p147"
+#define NTP_KEYGEN_VERSION       "4.2.5p148"
+#define NTP_KEYGEN_FULL_VERSION  "ntp-keygen (ntp) - Create a NTP host key - Ver. 4.2.5p148"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
