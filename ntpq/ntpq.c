489dc69f0fc10040ade52a35bec55b5e1f3a900e
Dave Hart <davehart@gmail.com>
2009-04-18 13:51:18 -0500

    ntpq.c:  quiet gcc, msc type warnings from decodeint calls to [hex|oct]toint

--- ntpq/ntpq.c
+++ ntpq/ntpq.c
@@ -2058,8 +2058,8 @@ decodeint(
 {
 	if (*str == '0') {
 		if (*(str+1) == 'x' || *(str+1) == 'X')
-		    return hextoint(str+2, val);
-		return octtoint(str, val);
+		    return hextoint(str+2, (u_long *)val);
+		return octtoint(str, (u_long *)val);
 	}
 	return atoint(str, val);
 }
