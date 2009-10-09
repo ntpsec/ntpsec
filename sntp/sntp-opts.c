7f889b5d0139e0ee2331332674f45855e003c79c
Harlan Stenn <stenn@ntp.org>
2009-10-09 06:25:40 -0500

    NTP_4_2_5P230_RC

--- sntp/sntp-opts.c
+++ sntp/sntp-opts.c
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (sntp-opts.c)
  *  
- *  It has been AutoGen-ed  October  7, 2009 at 06:40:04 AM by AutoGen 5.9.9pre5
+ *  It has been AutoGen-ed  October  9, 2009 at 02:24:13 AM by AutoGen 5.9.9pre5
  *  From the definitions    sntp-opts.def
  *  and the template file   options
  *
@@ -458,7 +458,7 @@ static tOptDesc optDesc[ OPTION_CT ] = {
  */
 tSCC   zPROGNAME[]   = "SNTP";
 tSCC   zUsageTitle[] =
-"sntp - standard SNTP program - Ver. 4.2.5p229\n\
+"sntp - standard SNTP program - Ver. 4.2.5p230-RC\n\
 USAGE:  %s [ -<flag> [<val>] | --<name>[{=| }<val>] ]... ...\n";
 tSCC   zRcName[]     = ".ntprc";
 tSCC*  apzHomeList[] = {
