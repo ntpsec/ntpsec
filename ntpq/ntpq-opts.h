9133beaf9bc86cf8972916a5a2ae4cf4ead9655e
Harlan Stenn <stenn@ntp.org>
2009-10-26 10:45:09 -0500

    NTP_4_2_5P237_RC

--- ntpq/ntpq-opts.h
+++ ntpq/ntpq-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpq-opts.h)
  *  
- *  It has been AutoGen-ed  October 22, 2009 at 06:40:06 AM by AutoGen 5.9.9pre5
+ *  It has been AutoGen-ed  October 26, 2009 at 05:37:51 AM by AutoGen 5.9.9pre5
  *  From the definitions    ntpq-opts.def
  *  and the template file   options
  *
@@ -69,8 +69,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    14
-#define NTPQ_VERSION       "4.2.5p236-RC"
-#define NTPQ_FULL_VERSION  "ntpq - standard NTP query program - Ver. 4.2.5p236-RC"
+#define NTPQ_VERSION       "4.2.5p237-RC"
+#define NTPQ_FULL_VERSION  "ntpq - standard NTP query program - Ver. 4.2.5p237-RC"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
