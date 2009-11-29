3ec48e5345dbb6b66debea7d04158723cc172ac9
Hal Murray <hmurray@megapathdsl.net>
2009-11-29 02:30:09 -0500

    configure.ac:  Add --enable-force-defer-DNS...
    ...Helps debugging deferred DNS lookups.
    
    ChangeLog: Bug 761: internal resolver does not seem to honor -4/-6
    qualifiers
    
    ntp_config.c: Bug 761: internal resolver does not seem to honor -4/-6
    qualifiers
    
    FORCE_DEFER_DNS helps debugging.  Pass no_needed to ntp_intres, first
    step on Bug 975.
    
    ntp_intres.c: Bug 761: internal resolver does not seem to honor -4/-6
    qualifiers
    
    Take no_needed from ntp_config, first step on Bug 975.

--- ntpd/ntp_intres.c
+++ ntpd/ntp_intres.c
@@ -61,6 +61,9 @@ struct conf_entry {
 	struct conf_entry *ce_next;
 	char *ce_name;			/* name to resolve */
 	struct conf_peer ce_config;	/* config info for peer */
+	int no_needed;			/* number of addresses needed (pool) */
+	/*  no_needed isn't used yet: It's needed to fix bug-975 */
+	int type;			/* -4 and -6 flags */
 	sockaddr_u peer_store;		/* address info for both fams */
 };
 #define	ce_peeraddr	ce_config.peeraddr
@@ -119,15 +122,17 @@ static	int resolve_value;	/* next value of resolve timer */
  * is supposed to consist of entries in the following order
  */
 #define	TOK_HOSTNAME	0
-#define	TOK_HMODE	1
-#define	TOK_VERSION	2
-#define TOK_MINPOLL	3
-#define TOK_MAXPOLL	4
-#define	TOK_FLAGS	5
-#define TOK_TTL		6
-#define	TOK_KEYID	7
-#define TOK_KEYSTR	8
-#define	NUMTOK		9
+#define	TOK_NEEDED	1
+#define	TOK_TYPE	2
+#define	TOK_HMODE	3
+#define	TOK_VERSION	4
+#define	TOK_MINPOLL	5
+#define	TOK_MAXPOLL	6
+#define	TOK_FLAGS	7
+#define	TOK_TTL		8
+#define	TOK_KEYID	9
+#define	TOK_KEYSTR	10
+#define	NUMTOK		11
 
 #define	MAXLINESIZE	512
 
@@ -150,7 +155,7 @@ char *req_file;		/* name of the file with configuration info */
 static	void	checkparent	(void);
 static	struct conf_entry *
 		removeentry	(struct conf_entry *);
-static	void	addentry	(char *, int, int, int, int, u_int,
+static	void	addentry	(char *, int, int, int, int, int, int, u_int,
 				   int, keyid_t, char *);
 static	int	findhostaddr	(struct conf_entry *);
 static	void	openntp		(void);
@@ -425,6 +430,8 @@ removeentry(
 static void
 addentry(
 	char *name,
+	int no_needed,
+	int type,
 	int mode,
 	int version,
 	int minpoll,
@@ -440,9 +447,9 @@ addentry(
 #ifdef DEBUG
 	if (debug > 1)
 		msyslog(LOG_INFO, 
-		    "intres: <%s> %d %d %d %d %x %d %x %s", name, mode,
-		    version, minpoll, maxpoll, flags, ttl, keyid,
-		    keystr);
+		    "intres: <%s> %d %d %d %d %d %d %x %d %x %s",
+		    name, no_needed, type, mode, version,
+		    minpoll, maxpoll, flags, ttl, keyid, keystr);
 #endif
 	ce = emalloc(sizeof(*ce));
 	ce->ce_name = estrdup(name);
@@ -455,6 +462,9 @@ addentry(
 	ce->ce_version = (u_char)version;
 	ce->ce_minpoll = (u_char)minpoll;
 	ce->ce_maxpoll = (u_char)maxpoll;
+	ce->no_needed = no_needed;	/* Not used after here. */
+					/* Start of fixing bug-975 */
+	ce->type = type;
 	ce->ce_flags = (u_char)flags;
 	ce->ce_ttl = (u_char)ttl;
 	ce->ce_keyid = keyid;
@@ -512,7 +522,9 @@ findhostaddr(
 			entry->ce_name));
 
 		memset(&hints, 0, sizeof(hints));
-		hints.ai_family = AF_UNSPEC;
+		hints.ai_family = entry->type;
+		hints.ai_socktype = SOCK_DGRAM;
+		hints.ai_protocol = IPPROTO_UDP;
 		/*
 		 * If IPv6 is not available look only for v4 addresses
 		 */
@@ -1174,9 +1186,11 @@ readconf(
 		/*
 		 * This is as good as we can check it.  Add it in.
 		 */
-		addentry(token[TOK_HOSTNAME], (int)intval[TOK_HMODE],
-			 (int)intval[TOK_VERSION], (int)intval[TOK_MINPOLL],
-			 (int)intval[TOK_MAXPOLL], flags, (int)intval[TOK_TTL],
+		addentry(token[TOK_HOSTNAME],
+			 (int)intval[TOK_NEEDED], (int)intval[TOK_TYPE],
+			 (int)intval[TOK_HMODE], (int)intval[TOK_VERSION],
+			 (int)intval[TOK_MINPOLL], (int)intval[TOK_MAXPOLL],
+			 flags, (int)intval[TOK_TTL],
 			 intval[TOK_KEYID], token[TOK_KEYSTR]);
 	}
 }
