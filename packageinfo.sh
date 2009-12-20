9d1a9ca1266129a86af27641778caf4c73e3169e
Harlan Stenn <stenn@ntp.org>
2009-12-20 03:05:09 -0500

    Start the 4.2.6p1-RC cycle

--- packageinfo.sh
+++ packageinfo.sh
@@ -8,22 +8,35 @@
 #   UpdatePoint script on the ntp.org tarball build machine.  Changes
 #   required by the typical ntp.org release sequences are below.
 #
+## DEV:
+#
 # To bump the -dev point (p) number, UpdatePoint needs no changes here.
+#
 # To start a -RC cycle in -dev leading to the next -stable release,
 #   set prerelease=rc.
+#
 # To move from dev -RC to new -stable and -dev major/minor version, set
 #   minor and/or major to the new version, repotype to match the new
 #   minor version, empty prerelease, and set point=NEW.  UpdatePoint
 #   will empty point and rcpoint, and set betapoint=0.
+#
+## STABLE:
+#
 # To start a -stable beta cycle, which normally occurs before a -stable
 #   -RC1 during the runup to a -stable point release, UpdatePoint needs
 #   no changes here.  Both betapoint and point will be incremented, and
 #   prerelease will be set to beta.
+#
 # To move on from -stable beta to RC set prerelease=rc.
+#
 # To fall back from -stable RC to beta set prerelease=beta.
+#
 # To skip over -stable beta1 directly to -RC1, set prerelease=rc.
+#
 # To skip all -stable prereleases and move from one primary or point 
 #   release directly to the next point release, set rcpoint=GO.
+#
+##
 #
 # To see what UpdatePoint will do without modifying packageinfo.sh as it
 # does by default, use the -t/--test option before the repo type:
@@ -47,7 +60,7 @@ version=${proto}.${major}.${minor}
 special=
 
 # prerelease can be empty, 'beta', or 'rc'.
-prerelease=
+prerelease=rc
 
 # ChangeLog starting tag (see also CommitLog-4.1.0)
 CLTAG=NTP_4_2_0
