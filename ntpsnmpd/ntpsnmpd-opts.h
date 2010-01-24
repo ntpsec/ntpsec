40c072dd76c2ecd25bb5746813432657639ab609
Harlan Stenn <stenn@ntp.org>
2010-01-24 11:44:49 -0500

    NTP_4_2_7P10

--- ntpsnmpd/ntpsnmpd-opts.h
+++ ntpsnmpd/ntpsnmpd-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpsnmpd-opts.h)
  *  
- *  It has been AutoGen-ed  January 13, 2010 at 10:25:51 AM by AutoGen 5.10
+ *  It has been AutoGen-ed  January 24, 2010 at 11:38:14 AM by AutoGen 5.10
  *  From the definitions    ntpsnmpd-opts.def
  *  and the template file   options
  *
@@ -62,8 +62,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    7
-#define NTPSNMPD_VERSION       "4.2.7p9"
-#define NTPSNMPD_FULL_VERSION  "ntpsnmpd - NTP SNMP MIB agent - Ver. 4.2.7p9"
+#define NTPSNMPD_VERSION       "4.2.7p10"
+#define NTPSNMPD_FULL_VERSION  "ntpsnmpd - NTP SNMP MIB agent - Ver. 4.2.7p10"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
