9f867939d947acf87b7a390e3b7a2226876d312a
Harlan Stenn <stenn@ntp.org>
2010-02-03 10:36:19 -0500

    NTP_4_2_7P15

--- ntpd/ntpd-opts.c
+++ ntpd/ntpd-opts.c
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpd-opts.c)
  *  
- *  It has been AutoGen-ed  February  2, 2010 at 10:25:52 AM by AutoGen 5.10
+ *  It has been AutoGen-ed  February  3, 2010 at 10:26:36 AM by AutoGen 5.10
  *  From the definitions    ntpd-opts.def
  *  and the template file   options
  *
@@ -956,7 +956,7 @@ static tOptDesc optDesc[ OPTION_CT ] = {
  */
 tSCC   zPROGNAME[]   = "NTPD";
 tSCC   zUsageTitle[] =
-"ntpd - NTP daemon program - Ver. 4.2.7p14\n\
+"ntpd - NTP daemon program - Ver. 4.2.7p15\n\
 USAGE:  %s [ -<flag> [<val>] | --<name>[{=| }<val>] ]...\n";
 #define zRcName     NULL
 #define apzHomeList NULL
--- sntp/sntp-opts.h
+++ sntp/sntp-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (sntp-opts.h)
  *  
- *  It has been AutoGen-ed  February  2, 2010 at 10:33:13 AM by AutoGen 5.10
+ *  It has been AutoGen-ed  February  3, 2010 at 10:35:08 AM by AutoGen 5.10
  *  From the definitions    sntp-opts.def
  *  and the template file   options
  *
@@ -72,8 +72,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    17
-#define SNTP_VERSION       "4.2.7p14"
-#define SNTP_FULL_VERSION  "sntp - standard SNTP program - Ver. 4.2.7p14"
+#define SNTP_VERSION       "4.2.7p15"
+#define SNTP_FULL_VERSION  "sntp - standard SNTP program - Ver. 4.2.7p15"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
