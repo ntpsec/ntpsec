a94b764f166df6610371da5c1b1bdcec0802703b
Harlan Stenn <stenn@ntp.org>
2010-02-05 10:31:56 -0500

    NTP_4_2_7P17

--- ntpsnmpd/ntpsnmpd-opts.c
+++ ntpsnmpd/ntpsnmpd-opts.c
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpsnmpd-opts.c)
  *  
- *  It has been AutoGen-ed  February  4, 2010 at 10:26:56 AM by AutoGen 5.10
+ *  It has been AutoGen-ed  February  5, 2010 at 10:26:58 AM by AutoGen 5.10
  *  From the definitions    ntpsnmpd-opts.def
  *  and the template file   options
  *
@@ -233,7 +233,7 @@ static tOptDesc optDesc[ OPTION_CT ] = {
  */
 tSCC   zPROGNAME[]   = "NTPSNMPD";
 tSCC   zUsageTitle[] =
-"ntpsnmpd - NTP SNMP MIB agent - Ver. 4.2.7p16\n\
+"ntpsnmpd - NTP SNMP MIB agent - Ver. 4.2.7p17\n\
 USAGE:  %s [ -<flag> | --<name> ]...\n";
 tSCC   zRcName[]     = ".ntprc";
 tSCC*  apzHomeList[] = {
--- sntp/sntp-opts.h
+++ sntp/sntp-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (sntp-opts.h)
  *  
- *  It has been AutoGen-ed  February  4, 2010 at 10:31:04 AM by AutoGen 5.10
+ *  It has been AutoGen-ed  February  5, 2010 at 10:30:57 AM by AutoGen 5.10
  *  From the definitions    sntp-opts.def
  *  and the template file   options
  *
@@ -72,8 +72,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    17
-#define SNTP_VERSION       "4.2.7p16"
-#define SNTP_FULL_VERSION  "sntp - standard SNTP program - Ver. 4.2.7p16"
+#define SNTP_VERSION       "4.2.7p17"
+#define SNTP_FULL_VERSION  "sntp - standard SNTP program - Ver. 4.2.7p17"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
