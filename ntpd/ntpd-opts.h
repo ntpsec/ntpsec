f313e7e4726a24a38beb6810e52e1e3786f6e3e7
Harlan Stenn <stenn@ntp.org>
2010-06-04 06:10:49 -0500

    NTP_4_2_7P33

--- ntpd/ntpd-opts.h
+++ ntpd/ntpd-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpd-opts.h)
  *  
- *  It has been AutoGen-ed  May 19, 2010 at 10:07:13 AM by AutoGen 5.10
+ *  It has been AutoGen-ed  June  3, 2010 at 10:08:16 AM by AutoGen 5.10
  *  From the definitions    ntpd-opts.def
  *  and the template file   options
  *
@@ -90,8 +90,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    35
-#define NTPD_VERSION       "4.2.7p32"
-#define NTPD_FULL_VERSION  "ntpd - NTP daemon program - Ver. 4.2.7p32"
+#define NTPD_VERSION       "4.2.7p33"
+#define NTPD_FULL_VERSION  "ntpd - NTP daemon program - Ver. 4.2.7p33"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
--- sntp/sntp-opts.h
+++ sntp/sntp-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (sntp-opts.h)
  *  
- *  It has been AutoGen-ed  May 19, 2010 at 10:09:19 AM by AutoGen 5.10
+ *  It has been AutoGen-ed  June  4, 2010 at 02:10:26 AM by AutoGen 5.10
  *  From the definitions    sntp-opts.def
  *  and the template file   options
  *
@@ -72,8 +72,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    17
-#define SNTP_VERSION       "4.2.7p32"
-#define SNTP_FULL_VERSION  "sntp - standard SNTP program - Ver. 4.2.7p32"
+#define SNTP_VERSION       "4.2.7p33"
+#define SNTP_FULL_VERSION  "sntp - standard SNTP program - Ver. 4.2.7p33"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
