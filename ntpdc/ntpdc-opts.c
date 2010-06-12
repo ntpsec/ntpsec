965c05d5f0c338c9646e9f6b6881e0b33632c4f7
Harlan Stenn <stenn@ntp.org>
2010-06-12 08:25:31 -0500

    NTP_4_2_7P35

--- ntpdc/ntpdc-opts.c
+++ ntpdc/ntpdc-opts.c
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpdc-opts.c)
  *  
- *  It has been AutoGen-ed  June  5, 2010 at 06:44:42 AM by AutoGen 5.10
+ *  It has been AutoGen-ed  June 12, 2010 at 04:23:43 AM by AutoGen 5.10
  *  From the definitions    ntpdc-opts.def
  *  and the template file   options
  *
@@ -452,7 +452,7 @@ static tOptDesc optDesc[ OPTION_CT ] = {
  */
 tSCC   zPROGNAME[]   = "NTPDC";
 tSCC   zUsageTitle[] =
-"ntpdc - vendor-specific NTP query program - Ver. 4.2.7p34\n\
+"ntpdc - vendor-specific NTP query program - Ver. 4.2.7p35\n\
 USAGE:  %s [ -<flag> [<val>] | --<name>[{=| }<val>] ]... [ host ...]\n";
 tSCC   zRcName[]     = ".ntprc";
 tSCC*  apzHomeList[] = {
--- ntpsnmpd/ntpsnmpd-opts.h
+++ ntpsnmpd/ntpsnmpd-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpsnmpd-opts.h)
  *  
- *  It has been AutoGen-ed  June  5, 2010 at 06:45:12 AM by AutoGen 5.10
+ *  It has been AutoGen-ed  June 12, 2010 at 04:24:04 AM by AutoGen 5.10
  *  From the definitions    ntpsnmpd-opts.def
  *  and the template file   options
  *
@@ -63,8 +63,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    8
-#define NTPSNMPD_VERSION       "4.2.7p34"
-#define NTPSNMPD_FULL_VERSION  "ntpsnmpd - NTP SNMP MIB agent - Ver. 4.2.7p34"
+#define NTPSNMPD_VERSION       "4.2.7p35"
+#define NTPSNMPD_FULL_VERSION  "ntpsnmpd - NTP SNMP MIB agent - Ver. 4.2.7p35"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
