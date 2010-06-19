3c756e19f8073ec8d19e7fa7b7dfbd5ae9edeb96
Harlan Stenn <stenn@ntp.org>
2010-06-19 10:09:44 -0500

    NTP_4_2_7P37

--- sntp/sntp-opts.h
+++ sntp/sntp-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (sntp-opts.h)
  *  
- *  It has been AutoGen-ed  June 15, 2010 at 10:09:21 AM by AutoGen 5.10
+ *  It has been AutoGen-ed  June 19, 2010 at 10:09:22 AM by AutoGen 5.10
  *  From the definitions    sntp-opts.def
  *  and the template file   options
  *
@@ -72,8 +72,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    17
-#define SNTP_VERSION       "4.2.7p36"
-#define SNTP_FULL_VERSION  "sntp - standard SNTP program - Ver. 4.2.7p36"
+#define SNTP_VERSION       "4.2.7p37"
+#define SNTP_FULL_VERSION  "sntp - standard SNTP program - Ver. 4.2.7p37"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
