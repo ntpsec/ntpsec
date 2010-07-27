ddce9fb5b0b15f78729aa0e400472032c729a9db
Linus Karlsson <karlsson@ntp.org>
2010-07-26 21:27:05 -0500

    Boilerplate for sntp tests, and tests for KoD-database.

---
+++ tests/sntp/sntptest.cpp
@@ -0,0 +1,9 @@
+#include "sntptest.h"
+
+/*
+ * This file contains various constants that libntp needs to be set
+ *  and that is normally defined in ntpd/ntpq/...
+ */
+
+volatile int debug = 0;
+const char *progname = "sntptest";
---
+++ tests/sntp/sntptest.h
@@ -0,0 +1,4 @@
+#include "main.h"
+
+class sntptest : public ntptest {
+};
