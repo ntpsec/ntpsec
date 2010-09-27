cdd0afa81147beb67263e08bd9c87754ce54745c
Harlan Stenn <stenn@ntp.org>
2010-09-27 10:11:56 -0500

    NTP_4_2_7P57

--- ntpq/ntpq-opts.h
+++ ntpq/ntpq-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpq-opts.h)
  *  
- *  It has been AutoGen-ed  September 25, 2010 at 09:43:49 PM by AutoGen 5.10
+ *  It has been AutoGen-ed  September 27, 2010 at 10:09:40 AM by AutoGen 5.10
  *  From the definitions    ntpq-opts.def
  *  and the template file   options
  *
@@ -69,8 +69,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    14
-#define NTPQ_VERSION       "4.2.7p56"
-#define NTPQ_FULL_VERSION  "ntpq - standard NTP query program - Ver. 4.2.7p56"
+#define NTPQ_VERSION       "4.2.7p57"
+#define NTPQ_FULL_VERSION  "ntpq - standard NTP query program - Ver. 4.2.7p57"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
--- ntpsnmpd/ntpsnmpd-opts.c
+++ ntpsnmpd/ntpsnmpd-opts.c
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpsnmpd-opts.c)
  *  
- *  It has been AutoGen-ed  September 25, 2010 at 09:44:02 PM by AutoGen 5.10
+ *  It has been AutoGen-ed  September 27, 2010 at 10:09:55 AM by AutoGen 5.10
  *  From the definitions    ntpsnmpd-opts.def
  *  and the template file   options
  *
@@ -256,7 +256,7 @@ static tOptDesc optDesc[ OPTION_CT ] = {
  */
 tSCC   zPROGNAME[]   = "NTPSNMPD";
 tSCC   zUsageTitle[] =
-"ntpsnmpd - NTP SNMP MIB agent - Ver. 4.2.7p56\n\
+"ntpsnmpd - NTP SNMP MIB agent - Ver. 4.2.7p57\n\
 USAGE:  %s [ -<flag> [<val>] | --<name>[{=| }<val>] ]...\n";
 tSCC   zRcName[]     = ".ntprc";
 tSCC*  apzHomeList[] = {
