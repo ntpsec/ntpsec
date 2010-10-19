89f38f4ccf563c52083985834b43bacbbbce7512
Harlan Stenn <stenn@ntp.org>
2010-10-19 10:10:23 -0500

    NTP_4_2_7P66

--- ntpsnmpd/ntpsnmpd-opts.h
+++ ntpsnmpd/ntpsnmpd-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpsnmpd-opts.h)
  *  
- *  It has been AutoGen-ed  October 16, 2010 at 05:16:57 AM by AutoGen 5.10
+ *  It has been AutoGen-ed  October 19, 2010 at 10:08:46 AM by AutoGen 5.10
  *  From the definitions    ntpsnmpd-opts.def
  *  and the template file   options
  *
@@ -63,8 +63,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    8
-#define NTPSNMPD_VERSION       "4.2.7p65"
-#define NTPSNMPD_FULL_VERSION  "ntpsnmpd - NTP SNMP MIB agent - Ver. 4.2.7p65"
+#define NTPSNMPD_VERSION       "4.2.7p66"
+#define NTPSNMPD_FULL_VERSION  "ntpsnmpd - NTP SNMP MIB agent - Ver. 4.2.7p66"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
--- util/ntp-keygen-opts.h
+++ util/ntp-keygen-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntp-keygen-opts.h)
  *  
- *  It has been AutoGen-ed  October 16, 2010 at 05:19:41 AM by AutoGen 5.10
+ *  It has been AutoGen-ed  October 19, 2010 at 10:10:02 AM by AutoGen 5.10
  *  From the definitions    ntp-keygen-opts.def
  *  and the template file   options
  *
@@ -78,8 +78,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    23
-#define NTP_KEYGEN_VERSION       "4.2.7p65"
-#define NTP_KEYGEN_FULL_VERSION  "ntp-keygen (ntp) - Create a NTP host key - Ver. 4.2.7p65"
+#define NTP_KEYGEN_VERSION       "4.2.7p66"
+#define NTP_KEYGEN_FULL_VERSION  "ntp-keygen (ntp) - Create a NTP host key - Ver. 4.2.7p66"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
