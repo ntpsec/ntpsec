ec04f2c97dbcacbe3305d079ab7994e231327dcb
Harlan Stenn <stenn@ntp.org>
2010-10-22 08:31:07 -0500

    NTP_4_2_7P68

--- ntpq/ntpq-opts.c
+++ ntpq/ntpq-opts.c
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpq-opts.c)
  *  
- *  It has been AutoGen-ed  October 21, 2010 at 10:08:17 AM by AutoGen 5.10
+ *  It has been AutoGen-ed  October 22, 2010 at 08:27:25 AM by AutoGen 5.10
  *  From the definitions    ntpq-opts.def
  *  and the template file   options
  *
@@ -427,7 +427,7 @@ static tOptDesc optDesc[ OPTION_CT ] = {
  */
 tSCC   zPROGNAME[]   = "NTPQ";
 tSCC   zUsageTitle[] =
-"ntpq - standard NTP query program - Ver. 4.2.7p67\n\
+"ntpq - standard NTP query program - Ver. 4.2.7p68\n\
 USAGE:  %s [ -<flag> [<val>] | --<name>[{=| }<val>] ]... [ host ...]\n";
 tSCC   zRcName[]     = ".ntprc";
 tSCC*  apzHomeList[] = {
