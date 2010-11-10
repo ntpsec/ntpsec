6efeeb731dd296af49471ed15bbc74bb470ca727
Dave Hart <davehart@gmail.com>
2010-11-09 20:08:29 -0500

    [Bug 1715] sntp utilitiesTest.IPv6Address failed.

--- sntp/tests/utilities.cpp
+++ sntp/tests/utilities.cpp
@@ -22,11 +22,7 @@ protected:
 	addrinfo CreateAddrinfo(sockaddr_u* sock) {
 		addrinfo a;
 		a.ai_family = sock->sa.sa_family;
-		if (a.ai_family == AF_INET) {
-			a.ai_addrlen = sizeof(sockaddr_in);
-		} else {
-			a.ai_addrlen = sizeof(sockaddr_in6);
-		}
+		a.ai_addrlen = SIZEOF_SOCKADDR(a.ai_family);
 		a.ai_addr = &sock->sa;
 		return a;
 	}
@@ -80,22 +76,20 @@ TEST_F(utilitiesTest, IPv4Address) {
 
 TEST_F(utilitiesTest, IPv6Address) {
 	const struct in6_addr address = {
-		0x20, 0x01, 0x0d, 0xb8,
-        0x85, 0xa3, 0x08, 0xd3, 
-        0x13, 0x19, 0x8a, 0x2e,
-        0x03, 0x70, 0x73, 0x34
-	};
+						0x20, 0x01, 0x0d, 0xb8,
+						0x85, 0xa3, 0x08, 0xd3, 
+						0x13, 0x19, 0x8a, 0x2e,
+						0x03, 0x70, 0x73, 0x34
+					};
+	const char * expected = "2001:db8:85a3:8d3:1319:8a2e:370:7334";
+	sockaddr_u	input;
+	addrinfo	inputA;
 
-	const char* expected =
-		"2001:db8:85a3:8d3:1319:8a2e:370:7334";
-
-	sockaddr_u input;
 	input.sa6.sin6_family = AF_INET6;
 	input.sa6.sin6_addr = address;
-
-	addrinfo inputA = CreateAddrinfo(&input);
-
 	EXPECT_STREQ(expected, ss_to_str(&input));
+
+	inputA = CreateAddrinfo(&input);
 	EXPECT_STREQ(expected, addrinfo_to_str(&inputA));
 }
 
