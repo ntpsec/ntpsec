6f85d67f086ffbd3cf1d3bd85d488c29957f953f
Harlan Stenn <stenn@ntp.org>
2010-12-29 08:13:01 -0500

    NTP_4_2_7P105

--- ntpq/ntpq-opts.h
+++ ntpq/ntpq-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpq-opts.h)
  *  
- *  It has been AutoGen-ed  December 28, 2010 at 04:33:25 AM by AutoGen 5.11.6pre7
+ *  It has been AutoGen-ed  December 29, 2010 at 08:09:28 AM by AutoGen 5.11.6pre7
  *  From the definitions    ntpq-opts.def
  *  and the template file   options
  *
@@ -67,8 +67,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    14
-#define NTPQ_VERSION       "4.2.7p104"
-#define NTPQ_FULL_VERSION  "ntpq - standard NTP query program - Ver. 4.2.7p104"
+#define NTPQ_VERSION       "4.2.7p105"
+#define NTPQ_FULL_VERSION  "ntpq - standard NTP query program - Ver. 4.2.7p105"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
--- ntpsnmpd/ntpsnmpd-opts.h
+++ ntpsnmpd/ntpsnmpd-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpsnmpd-opts.h)
  *  
- *  It has been AutoGen-ed  December 28, 2010 at 04:33:36 AM by AutoGen 5.11.6pre7
+ *  It has been AutoGen-ed  December 29, 2010 at 08:09:40 AM by AutoGen 5.11.6pre7
  *  From the definitions    ntpsnmpd-opts.def
  *  and the template file   options
  *
@@ -61,8 +61,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    8
-#define NTPSNMPD_VERSION       "4.2.7p104"
-#define NTPSNMPD_FULL_VERSION  "ntpsnmpd - NTP SNMP MIB agent - Ver. 4.2.7p104"
+#define NTPSNMPD_VERSION       "4.2.7p105"
+#define NTPSNMPD_FULL_VERSION  "ntpsnmpd - NTP SNMP MIB agent - Ver. 4.2.7p105"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
