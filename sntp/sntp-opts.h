2fe7ee074193113e264883ba7b4deba0f8e9167d
Harlan Stenn <stenn@ntp.org>
2011-01-06 08:55:50 -0500

    NTP_4_2_7P112

--- sntp/sntp-opts.h
+++ sntp/sntp-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (sntp-opts.h)
  *  
- *  It has been AutoGen-ed  January  5, 2011 at 08:44:25 AM by AutoGen 5.11.6pre7
+ *  It has been AutoGen-ed  January  6, 2011 at 08:54:01 AM by AutoGen 5.11.6pre7
  *  From the definitions    sntp-opts.def
  *  and the template file   options
  *
@@ -70,8 +70,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    17
-#define SNTP_VERSION       "4.2.7p111"
-#define SNTP_FULL_VERSION  "sntp - standard SNTP program - Ver. 4.2.7p111"
+#define SNTP_VERSION       "4.2.7p112"
+#define SNTP_FULL_VERSION  "sntp - standard SNTP program - Ver. 4.2.7p112"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
--- util/ntp-keygen-opts.h
+++ util/ntp-keygen-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntp-keygen-opts.h)
  *  
- *  It has been AutoGen-ed  January  5, 2011 at 08:46:17 AM by AutoGen 5.11.6pre7
+ *  It has been AutoGen-ed  January  6, 2011 at 08:55:26 AM by AutoGen 5.11.6pre7
  *  From the definitions    ntp-keygen-opts.def
  *  and the template file   options
  *
@@ -77,8 +77,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    24
-#define NTP_KEYGEN_VERSION       "4.2.7p111"
-#define NTP_KEYGEN_FULL_VERSION  "ntp-keygen (ntp) - Create a NTP host key - Ver. 4.2.7p111"
+#define NTP_KEYGEN_VERSION       "4.2.7p112"
+#define NTP_KEYGEN_FULL_VERSION  "ntp-keygen (ntp) - Create a NTP host key - Ver. 4.2.7p112"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
