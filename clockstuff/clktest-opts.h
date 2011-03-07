ee37970c757cf06f4ea6c4b6486ea743cfdeb6b7
Harlan Stenn <stenn@ntp.org>
2011-03-07 10:06:50 -0500

    NTP_4_2_7P137

--- clockstuff/clktest-opts.h
+++ clockstuff/clktest-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (clktest-opts.h)
  *  
- *  It has been AutoGen-ed  March  2, 2011 at 07:24:45 AM by AutoGen 5.11.6
+ *  It has been AutoGen-ed  March  6, 2011 at 11:32:50 AM by AutoGen 5.11.6
  *  From the definitions    clktest-opts.def
  *  and the template file   options
  *
@@ -63,8 +63,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    10
-#define CLKTEST_VERSION       "4.2.7p136"
-#define CLKTEST_FULL_VERSION  "clktest - test the clock line discipline - Ver. 4.2.7p136"
+#define CLKTEST_VERSION       "4.2.7p137"
+#define CLKTEST_FULL_VERSION  "clktest - test the clock line discipline - Ver. 4.2.7p137"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
--- ntpq/ntpq-opts.h
+++ ntpq/ntpq-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpq-opts.h)
  *  
- *  It has been AutoGen-ed  March  2, 2011 at 07:24:28 AM by AutoGen 5.11.6
+ *  It has been AutoGen-ed  March  6, 2011 at 11:31:18 AM by AutoGen 5.11.6
  *  From the definitions    ntpq-opts.def
  *  and the template file   options
  *
@@ -67,8 +67,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    14
-#define NTPQ_VERSION       "4.2.7p136"
-#define NTPQ_FULL_VERSION  "ntpq - standard NTP query program - Ver. 4.2.7p136"
+#define NTPQ_VERSION       "4.2.7p137"
+#define NTPQ_FULL_VERSION  "ntpq - standard NTP query program - Ver. 4.2.7p137"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
