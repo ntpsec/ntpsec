33d0ecb76ae75929c2abc38e7bce410d8dedf129
Harlan Stenn <stenn@ntp.org>
2011-04-05 07:50:37 -0500

    NTP_4_2_7P146

--- sntp/sntp-opts.c
+++ sntp/sntp-opts.c
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (sntp-opts.c)
  *  
- *  It has been AutoGen-ed  April  4, 2011 at 11:15:35 AM by AutoGen 5.11.6
+ *  It has been AutoGen-ed  April  5, 2011 at 07:43:46 AM by AutoGen 5.11.6
  *  From the definitions    sntp-opts.def
  *  and the template file   options
  *
@@ -642,7 +642,7 @@ static tOptDesc optDesc[ OPTION_CT ] = {
  */
 static char const zPROGNAME[5] = "SNTP";
 static char const zUsageTitle[154] =
-"sntp - standard Simple Network Time Protocol program - Ver. 4.2.7p145\n\
+"sntp - standard Simple Network Time Protocol program - Ver. 4.2.7p146\n\
 USAGE:  %s [ -<flag> [<val>] | --<name>[{=| }<val>] ]... \\\n\
 \t\t[ hostname-or-IP ...]\n";
 static char const zRcName[7] = ".ntprc";
