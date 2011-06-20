87e3bf030ef46c6b4c614ff4559df442620a44e1
Harlan Stenn <stenn@ntp.org>
2011-06-20 09:46:53 -0500

    NTP_4_2_7P184

--- ntpdc/ntpdc-opts.h
+++ ntpdc/ntpdc-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpdc-opts.h)
  *  
- *  It has been AutoGen-ed  June 19, 2011 at 11:16:20 AM by AutoGen 5.11.10pre10
+ *  It has been AutoGen-ed  June 20, 2011 at 09:45:54 AM by AutoGen 5.11.10pre10
  *  From the definitions    ntpdc-opts.def
  *  and the template file   options
  *
@@ -81,8 +81,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    15
-#define NTPDC_VERSION       "4.2.7p183"
-#define NTPDC_FULL_VERSION  "ntpdc 4.2.7p183"
+#define NTPDC_VERSION       "4.2.7p184"
+#define NTPDC_FULL_VERSION  "ntpdc 4.2.7p184"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
--- ntpq/ntpq-opts.h
+++ ntpq/ntpq-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpq-opts.h)
  *  
- *  It has been AutoGen-ed  June 19, 2011 at 11:17:15 AM by AutoGen 5.11.10pre10
+ *  It has been AutoGen-ed  June 20, 2011 at 09:46:05 AM by AutoGen 5.11.10pre10
  *  From the definitions    ntpq-opts.def
  *  and the template file   options
  *
@@ -80,8 +80,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    14
-#define NTPQ_VERSION       "4.2.7p183"
-#define NTPQ_FULL_VERSION  "ntpq 4.2.7p183"
+#define NTPQ_VERSION       "4.2.7p184"
+#define NTPQ_FULL_VERSION  "ntpq 4.2.7p184"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
