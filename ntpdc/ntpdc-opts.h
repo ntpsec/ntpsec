cf2a1665cb0a44fcfed1bff8e445c31254d36b31
Harlan Stenn <stenn@ntp.org>
2011-07-13 09:55:12 -0500

    NTP_4_2_7P190

--- ntpdc/ntpdc-opts.h
+++ ntpdc/ntpdc-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpdc-opts.h)
  *  
- *  It has been AutoGen-ed  July 11, 2011 at 06:20:20 AM by AutoGen 5.12
+ *  It has been AutoGen-ed  July 13, 2011 at 09:44:16 AM by AutoGen 5.12
  *  From the definitions    ntpdc-opts.def
  *  and the template file   options
  *
@@ -81,8 +81,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    15
-#define NTPDC_VERSION       "4.2.7p189"
-#define NTPDC_FULL_VERSION  "ntpdc 4.2.7p189"
+#define NTPDC_VERSION       "4.2.7p190"
+#define NTPDC_FULL_VERSION  "ntpdc 4.2.7p190"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
--- ntpsnmpd/ntpsnmpd-opts.h
+++ ntpsnmpd/ntpsnmpd-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpsnmpd-opts.h)
  *  
- *  It has been AutoGen-ed  July 11, 2011 at 06:24:15 AM by AutoGen 5.12
+ *  It has been AutoGen-ed  July 13, 2011 at 09:48:59 AM by AutoGen 5.12
  *  From the definitions    ntpsnmpd-opts.def
  *  and the template file   options
  *
@@ -74,8 +74,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    8
-#define NTPSNMPD_VERSION       "4.2.7p189"
-#define NTPSNMPD_FULL_VERSION  "ntpsnmpd 4.2.7p189"
+#define NTPSNMPD_VERSION       "4.2.7p190"
+#define NTPSNMPD_FULL_VERSION  "ntpsnmpd 4.2.7p190"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
