afb033f2992d22984c9254b5493c1b789a5ed483
Harlan Stenn <stenn@ntp.org>
2011-08-04 19:17:36 -0500

    libopts/file.c fix from Bruce Korb (arg-type=file)

--- sntp/libopts/file.c
+++ sntp/libopts/file.c
@@ -89,8 +89,10 @@ optionFileCheck(tOptions* pOpts, tOptDesc* pOD,
         case FTYPE_MODE_MAY_EXIST:
         {
             char * p = strrchr(pOD->optArg.argString, DIRCH);
-            if (p != NULL)
-                *p = NUL;
+            if (p == NULL)
+                break; /* assume "." always exists. */
+
+            *p = NUL;
             if (  (stat(pOD->optArg.argString, &sb) != 0)
                || (errno = EINVAL, ! S_ISDIR(sb.st_mode)) ){
                 fprintf(stderr, zFSOptError, errno, strerror(errno),
