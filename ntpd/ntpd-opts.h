3e984a020e195df05709cc7bb7d77b6b754327de
Harlan Stenn <stenn@ntp.org>
2011-10-05 09:03:57 -0500

    NTP_4_2_7P220

--- ntpd/ntpd-opts.h
+++ ntpd/ntpd-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpd-opts.h)
  *  
- *  It has been AutoGen-ed  October  4, 2011 at 10:49:41 AM by AutoGen 5.12
+ *  It has been AutoGen-ed  October  5, 2011 at 09:02:00 AM by AutoGen 5.12
  *  From the definitions    ntpd-opts.def
  *  and the template file   options
  *
@@ -102,8 +102,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    36
-#define NTPD_VERSION       "4.2.7p219"
-#define NTPD_FULL_VERSION  "ntpd 4.2.7p219"
+#define NTPD_VERSION       "4.2.7p220"
+#define NTPD_FULL_VERSION  "ntpd 4.2.7p220"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
