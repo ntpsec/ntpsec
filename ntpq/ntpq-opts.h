3afc84f210f427e3aa0bc5a9baac099993dfc71f
Harlan Stenn <stenn@ntp.org>
2011-10-11 09:09:30 -0500

    NTP_4_2_7P222

--- ntpq/ntpq-opts.h
+++ ntpq/ntpq-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpq-opts.h)
  *  
- *  It has been AutoGen-ed  October 10, 2011 at 05:58:16 AM by AutoGen 5.12
+ *  It has been AutoGen-ed  October 11, 2011 at 09:08:36 AM by AutoGen 5.12
  *  From the definitions    ntpq-opts.def
  *  and the template file   options
  *
@@ -80,8 +80,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    14
-#define NTPQ_VERSION       "4.2.7p221"
-#define NTPQ_FULL_VERSION  "ntpq 4.2.7p221"
+#define NTPQ_VERSION       "4.2.7p222"
+#define NTPQ_FULL_VERSION  "ntpq 4.2.7p222"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
