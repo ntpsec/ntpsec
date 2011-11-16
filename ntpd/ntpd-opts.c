4fdc06e7c6dc8156923a5f4c9922e9468f967948
Harlan Stenn <stenn@ntp.org>
2011-11-16 03:15:06 -0500

    NTP_4_2_7P235

--- ntpd/ntpd-opts.c
+++ ntpd/ntpd-opts.c
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpd-opts.c)
  *  
- *  It has been AutoGen-ed  November  7, 2011 at 11:07:58 PM by AutoGen 5.12
+ *  It has been AutoGen-ed  November 16, 2011 at 03:13:26 AM by AutoGen 5.12
  *  From the definitions    ntpd-opts.def
  *  and the template file   options
  *
@@ -52,7 +52,7 @@ extern FILE * option_usage_fp;
 /* TRANSLATORS: choose the translation for option names wisely because you
                 cannot ever change your mind. */
 static char const zCopyright[333] =
-"ntpd 4.2.7p234\n\
+"ntpd 4.2.7p235\n\
 Copyright (C) 1970-2011 The University of Delaware, David L. Mills, and/or others, all rights reserved.\n\
 This is free software. It is licensed for use, modification and\n\
 redistribution under the terms of the NTP License, copies of which\n\
@@ -1028,7 +1028,7 @@ static tOptDesc optDesc[OPTION_CT] = {
  */
 static char const zPROGNAME[5] = "NTPD";
 static char const zUsageTitle[133] =
-"ntpd - NTP daemon program - Ver. 4.2.7p234\n\
+"ntpd - NTP daemon program - Ver. 4.2.7p235\n\
 USAGE:  %s [ -<flag> [<val>] | --<name>[{=| }<val>] ]... \\\n\
 \t\t[ <server1> ... <serverN> ]\n";
 #define zRcName     NULL
--- ntpdc/ntpdc-opts.h
+++ ntpdc/ntpdc-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpdc-opts.h)
  *  
- *  It has been AutoGen-ed  November  7, 2011 at 11:08:20 PM by AutoGen 5.12
+ *  It has been AutoGen-ed  November 16, 2011 at 03:13:56 AM by AutoGen 5.12
  *  From the definitions    ntpdc-opts.def
  *  and the template file   options
  *
@@ -81,8 +81,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    15
-#define NTPDC_VERSION       "4.2.7p234"
-#define NTPDC_FULL_VERSION  "ntpdc 4.2.7p234"
+#define NTPDC_VERSION       "4.2.7p235"
+#define NTPDC_FULL_VERSION  "ntpdc 4.2.7p235"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
