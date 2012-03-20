492e05ee1eb49452d777e98b5d0e3a2cf21d7946
Harlan Stenn <stenn@ntp.org>
2012-03-20 11:25:09 -0500

    NTP_4_2_7P265

--- ntpdc/ntpdc-opts.h
+++ ntpdc/ntpdc-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpdc-opts.h)
  *  
- *  It has been AutoGen-ed  March 14, 2012 at 07:57:11 AM by AutoGen 5.14
+ *  It has been AutoGen-ed  March 20, 2012 at 11:24:07 AM by AutoGen 5.14
  *  From the definitions    ntpdc-opts.def
  *  and the template file   options
  *
@@ -82,8 +82,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    15
-#define NTPDC_VERSION       "4.2.7p264"
-#define NTPDC_FULL_VERSION  "ntpdc 4.2.7p264"
+#define NTPDC_VERSION       "4.2.7p265"
+#define NTPDC_FULL_VERSION  "ntpdc 4.2.7p265"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
