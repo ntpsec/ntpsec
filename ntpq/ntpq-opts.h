e57acc0387274f2115128924e67fbc362d50ef2e
Harlan Stenn <stenn@ntp.org>
2012-06-15 11:22:51 -0500

    NTP_4_2_7P282

--- ntpq/ntpq-opts.h
+++ ntpq/ntpq-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpq-opts.h)
  *  
- *  It has been AutoGen-ed  June 14, 2012 at 11:21:59 AM by AutoGen 5.14
+ *  It has been AutoGen-ed  June 15, 2012 at 11:20:20 AM by AutoGen 5.14
  *  From the definitions    ntpq-opts.def
  *  and the template file   options
  *
@@ -81,8 +81,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    14
-#define NTPQ_VERSION       "4.2.7p281"
-#define NTPQ_FULL_VERSION  "ntpq 4.2.7p281"
+#define NTPQ_VERSION       "4.2.7p282"
+#define NTPQ_FULL_VERSION  "ntpq 4.2.7p282"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
--- ntpsnmpd/ntpsnmpd-opts.h
+++ ntpsnmpd/ntpsnmpd-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpsnmpd-opts.h)
  *  
- *  It has been AutoGen-ed  June 14, 2012 at 11:22:15 AM by AutoGen 5.14
+ *  It has been AutoGen-ed  June 15, 2012 at 11:20:35 AM by AutoGen 5.14
  *  From the definitions    ntpsnmpd-opts.def
  *  and the template file   options
  *
@@ -75,8 +75,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    8
-#define NTPSNMPD_VERSION       "4.2.7p281"
-#define NTPSNMPD_FULL_VERSION  "ntpsnmpd 4.2.7p281"
+#define NTPSNMPD_VERSION       "4.2.7p282"
+#define NTPSNMPD_FULL_VERSION  "ntpsnmpd 4.2.7p282"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
