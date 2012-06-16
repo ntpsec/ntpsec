89312be918b356765951faa77662ec2d070dfd2d
Harlan Stenn <stenn@ntp.org>
2012-06-16 11:23:18 -0500

    NTP_4_2_7P283

--- ntpdc/ntpdc-opts.h
+++ ntpdc/ntpdc-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpdc-opts.h)
  *  
- *  It has been AutoGen-ed  June 15, 2012 at 11:20:08 AM by AutoGen 5.14
+ *  It has been AutoGen-ed  June 16, 2012 at 11:20:35 AM by AutoGen 5.14
  *  From the definitions    ntpdc-opts.def
  *  and the template file   options
  *
@@ -82,8 +82,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    15
-#define NTPDC_VERSION       "4.2.7p282"
-#define NTPDC_FULL_VERSION  "ntpdc 4.2.7p282"
+#define NTPDC_VERSION       "4.2.7p283"
+#define NTPDC_FULL_VERSION  "ntpdc 4.2.7p283"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
--- util/ntp-keygen-opts.h
+++ util/ntp-keygen-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntp-keygen-opts.h)
  *  
- *  It has been AutoGen-ed  June 15, 2012 at 11:21:05 AM by AutoGen 5.14
+ *  It has been AutoGen-ed  June 16, 2012 at 11:21:31 AM by AutoGen 5.14
  *  From the definitions    ntp-keygen-opts.def
  *  and the template file   options
  *
@@ -92,8 +92,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    25
-#define NTP_KEYGEN_VERSION       "4.2.7p282"
-#define NTP_KEYGEN_FULL_VERSION  "ntp-keygen (ntp) 4.2.7p282"
+#define NTP_KEYGEN_VERSION       "4.2.7p283"
+#define NTP_KEYGEN_FULL_VERSION  "ntp-keygen (ntp) 4.2.7p283"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
