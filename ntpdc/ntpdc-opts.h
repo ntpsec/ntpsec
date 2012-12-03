5dfaf63298bf66f60357e5289910c62bb0c1d88b
Harlan Stenn <stenn@ntp.org>
2012-12-03 05:37:26 -0500

    NTP_4_2_7P330

--- ntpdc/ntpdc-opts.h
+++ ntpdc/ntpdc-opts.h
@@ -1,7 +1,7 @@
 /*  
  *  EDIT THIS FILE WITH CAUTION  (ntpdc-opts.h)
  *  
- *  It has been AutoGen-ed  December  1, 2012 at 11:31:58 AM by AutoGen 5.16.2
+ *  It has been AutoGen-ed  December  3, 2012 at 05:26:46 AM by AutoGen 5.16.2
  *  From the definitions    ntpdc-opts.def
  *  and the template file   options
  *
@@ -82,8 +82,8 @@ typedef enum {
 } teOptIndex;
 
 #define OPTION_CT    15
-#define NTPDC_VERSION       "4.2.7p329"
-#define NTPDC_FULL_VERSION  "ntpdc 4.2.7p329"
+#define NTPDC_VERSION       "4.2.7p330"
+#define NTPDC_FULL_VERSION  "ntpdc 4.2.7p330"
 
 /*
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
