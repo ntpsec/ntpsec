95b8de2fb121c89d5fb83babec946d28101eb410
Harlan Stenn <stenn@ntp.org>
2013-10-12 11:26:40 -0500

    NTP_4_2_7P391

--- ntpd/ntpd-opts.h
+++ ntpd/ntpd-opts.h
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (ntpd-opts.h)
  *
- *  It has been AutoGen-ed  September 26, 2013 at 11:34:12 AM by AutoGen 5.18.1pre5
+ *  It has been AutoGen-ed  October 12, 2013 at 11:22:29 AM by AutoGen 5.18.1pre5
  *  From the definitions    ntpd-opts.def
  *  and the template file   options
  *
@@ -105,9 +105,9 @@ typedef enum {
 /** count of all options for ntpd */
 #define OPTION_CT    37
 /** ntpd version */
-#define NTPD_VERSION       "4.2.7p390"
+#define NTPD_VERSION       "4.2.7p391"
 /** Full ntpd version text */
-#define NTPD_FULL_VERSION  "ntpd 4.2.7p390"
+#define NTPD_FULL_VERSION  "ntpd 4.2.7p391"
 
 /**
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
--- util/ntp-keygen-opts.c
+++ util/ntp-keygen-opts.c
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (ntp-keygen-opts.c)
  *
- *  It has been AutoGen-ed  September 26, 2013 at 11:36:31 AM by AutoGen 5.18.1pre5
+ *  It has been AutoGen-ed  October 12, 2013 at 11:25:51 AM by AutoGen 5.18.1pre5
  *  From the definitions    ntp-keygen-opts.def
  *  and the template file   options
  *
@@ -72,7 +72,7 @@ extern FILE * option_usage_fp;
  *  static const strings for ntp-keygen options
  */
 static char const ntp_keygen_opt_strs[2357] =
-/*     0 */ "ntp-keygen (ntp) 4.2.7p390\n"
+/*     0 */ "ntp-keygen (ntp) 4.2.7p391\n"
             "Copyright (C) 1970-2013 The University of Delaware, all rights reserved.\n"
             "This is free software. It is licensed for use, modification and\n"
             "redistribution under the terms of the NTP License, copies of which\n"
@@ -163,14 +163,14 @@ static char const ntp_keygen_opt_strs[2357] =
 /*  2136 */ "no-load-opts\0"
 /*  2149 */ "no\0"
 /*  2152 */ "NTP_KEYGEN\0"
-/*  2163 */ "ntp-keygen (ntp) - Create a NTP host key - Ver. 4.2.7p390\n"
+/*  2163 */ "ntp-keygen (ntp) - Create a NTP host key - Ver. 4.2.7p391\n"
             "Usage:  %s [ -<flag> [<val>] | --<name>[{=| }<val>] ]...\n\0"
 /*  2279 */ "$HOME\0"
 /*  2285 */ ".\0"
 /*  2287 */ ".ntprc\0"
 /*  2294 */ "http://bugs.ntp.org, bugs@ntp.org\0"
 /*  2328 */ "\n\0"
-/*  2330 */ "ntp-keygen (ntp) 4.2.7p390";
+/*  2330 */ "ntp-keygen (ntp) 4.2.7p391";
 
 /**
  *  imbits option description:
@@ -1364,7 +1364,7 @@ static void bogus_function(void) {
      translate option names.
    */
   /* referenced via ntp_keygenOptions.pzCopyright */
-  puts(_("ntp-keygen (ntp) 4.2.7p390\n\
+  puts(_("ntp-keygen (ntp) 4.2.7p391\n\
 Copyright (C) 1970-2013 The University of Delaware, all rights reserved.\n\
 This is free software. It is licensed for use, modification and\n\
 redistribution under the terms of the NTP License, copies of which\n\
@@ -1462,14 +1462,14 @@ provided \"as is\" without express or implied warranty.\n"));
   puts(_("load options from a config file"));
 
   /* referenced via ntp_keygenOptions.pzUsageTitle */
-  puts(_("ntp-keygen (ntp) - Create a NTP host key - Ver. 4.2.7p390\n\
+  puts(_("ntp-keygen (ntp) - Create a NTP host key - Ver. 4.2.7p391\n\
 Usage:  %s [ -<flag> [<val>] | --<name>[{=| }<val>] ]...\n"));
 
   /* referenced via ntp_keygenOptions.pzExplain */
   puts(_("\n"));
 
   /* referenced via ntp_keygenOptions.pzFullVersion */
-  puts(_("ntp-keygen (ntp) 4.2.7p390"));
+  puts(_("ntp-keygen (ntp) 4.2.7p391"));
 
   /* referenced via ntp_keygenOptions.pzFullUsage */
   puts(_("<<<NOT-FOUND>>>"));
