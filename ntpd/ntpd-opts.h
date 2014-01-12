46ee37ffaec9ac3d42ff9db2331c940f90912875
Harlan Stenn <stenn@ntp.org>
2014-01-12 10:07:09 -0500

    NTP_4_2_7P411

--- ntpd/ntpd-opts.h
+++ ntpd/ntpd-opts.h
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (ntpd-opts.h)
  *
- *  It has been AutoGen-ed  January  8, 2014 at 11:39:00 AM by AutoGen 5.18.3pre11
+ *  It has been AutoGen-ed  January 10, 2014 at 11:03:50 AM by AutoGen 5.18.3pre11
  *  From the definitions    ntpd-opts.def
  *  and the template file   options
  *
@@ -105,9 +105,9 @@ typedef enum {
 /** count of all options for ntpd */
 #define OPTION_CT    37
 /** ntpd version */
-#define NTPD_VERSION       "4.2.7p410"
+#define NTPD_VERSION       "4.2.7p411"
 /** Full ntpd version text */
-#define NTPD_FULL_VERSION  "ntpd 4.2.7p410"
+#define NTPD_FULL_VERSION  "ntpd 4.2.7p411"
 
 /**
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
--- sntp/sntp-opts.h
+++ sntp/sntp-opts.h
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (sntp-opts.h)
  *
- *  It has been AutoGen-ed  January  8, 2014 at 11:45:19 AM by AutoGen 5.18.3pre11
+ *  It has been AutoGen-ed  January 10, 2014 at 10:45:18 AM by AutoGen 5.18.3pre11
  *  From the definitions    sntp-opts.def
  *  and the template file   options
  *
@@ -91,9 +91,9 @@ typedef enum {
 /** count of all options for sntp */
 #define OPTION_CT    23
 /** sntp version */
-#define SNTP_VERSION       "4.2.7p410"
+#define SNTP_VERSION       "4.2.7p411"
 /** Full sntp version text */
-#define SNTP_FULL_VERSION  "sntp 4.2.7p410"
+#define SNTP_FULL_VERSION  "sntp 4.2.7p411"
 
 /**
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
