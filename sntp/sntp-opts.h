97500fed5497b27774b493aa3fd1f665cb5a32e9
Harlan Stenn <stenn@ntp.org>
2014-01-27 09:13:58 -0500

    NTP_4_2_7P412

--- sntp/sntp-opts.h
+++ sntp/sntp-opts.h
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (sntp-opts.h)
  *
- *  It has been AutoGen-ed  January 10, 2014 at 10:45:18 AM by AutoGen 5.18.3pre11
+ *  It has been AutoGen-ed  January 20, 2014 at 10:50:16 AM by AutoGen 5.18.3pre11
  *  From the definitions    sntp-opts.def
  *  and the template file   options
  *
@@ -91,9 +91,9 @@ typedef enum {
 /** count of all options for sntp */
 #define OPTION_CT    23
 /** sntp version */
-#define SNTP_VERSION       "4.2.7p411"
+#define SNTP_VERSION       "4.2.7p412"
 /** Full sntp version text */
-#define SNTP_FULL_VERSION  "sntp 4.2.7p411"
+#define SNTP_FULL_VERSION  "sntp 4.2.7p412"
 
 /**
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
