8a4a219b0836fafbf2b23b9df8f16f971d5bbbed
Harlan Stenn <stenn@ntp.org>
2014-02-10 20:34:39 -0500

    NTP_4_2_7P421

--- packageinfo.sh
+++ packageinfo.sh
@@ -83,7 +83,7 @@ CLTAG=NTP_4_2_0
 # - Numeric values increment
 # - empty 'increments' to 1
 # - NEW 'increments' to empty
-point=420
+point=421
 
 ### betapoint is normally modified by script.
 # ntp-stable Beta number (betapoint)
