8c0695398a1877b20a96051b9fa54740c5dc0267
Harlan Stenn <stenn@ntp.org>
2014-03-09 11:46:54 -0500

    NTP_4_2_7P432

--- ntpsnmpd/ntpsnmpd-opts.h
+++ ntpsnmpd/ntpsnmpd-opts.h
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (ntpsnmpd-opts.h)
  *
- *  It has been AutoGen-ed  March  5, 2014 at 11:10:48 AM by AutoGen 5.18.3pre11
+ *  It has been AutoGen-ed  March  9, 2014 at 11:43:54 AM by AutoGen 5.18.3pre11
  *  From the definitions    ntpsnmpd-opts.def
  *  and the template file   options
  *
@@ -76,9 +76,9 @@ typedef enum {
 /** count of all options for ntpsnmpd */
 #define OPTION_CT    8
 /** ntpsnmpd version */
-#define NTPSNMPD_VERSION       "4.2.7p431"
+#define NTPSNMPD_VERSION       "4.2.7p432"
 /** Full ntpsnmpd version text */
-#define NTPSNMPD_FULL_VERSION  "ntpsnmpd 4.2.7p431"
+#define NTPSNMPD_FULL_VERSION  "ntpsnmpd 4.2.7p432"
 
 /**
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
