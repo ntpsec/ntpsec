553c3be44e9549d6c10981f1941eb6dc5fafe591
Harlan Stenn <stenn@ntp.org>
2014-08-09 11:04:13 -0500

    NTP_4_2_7P457

--- ntpd/ntpd-opts.h
+++ ntpd/ntpd-opts.h
@@ -105,9 +105,9 @@ typedef enum {
 /** count of all options for ntpd */
 #define OPTION_CT    37
 /** ntpd version */
-#define NTPD_VERSION       "4.2.7p456"
+#define NTPD_VERSION       "4.2.7p457"
 /** Full ntpd version text */
-#define NTPD_FULL_VERSION  "ntpd 4.2.7p456"
+#define NTPD_FULL_VERSION  "ntpd 4.2.7p457"
 
 /**
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
--- ntpq/ntpq-opts.h
+++ ntpq/ntpq-opts.h
@@ -82,9 +82,9 @@ typedef enum {
 /** count of all options for ntpq */
 #define OPTION_CT    14
 /** ntpq version */
-#define NTPQ_VERSION       "4.2.7p456"
+#define NTPQ_VERSION       "4.2.7p457"
 /** Full ntpq version text */
-#define NTPQ_FULL_VERSION  "ntpq 4.2.7p456"
+#define NTPQ_FULL_VERSION  "ntpq 4.2.7p457"
 
 /**
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
