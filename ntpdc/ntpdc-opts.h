7adcf4f206b856c61e9972577d0496de7794a229
Harlan Stenn <stenn@ntp.org>
2014-09-01 08:58:56 -0500

    NTP_4_2_7P469

--- ntpdc/ntpdc-opts.h
+++ ntpdc/ntpdc-opts.h
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (ntpdc-opts.h)
  *
- *  It has been AutoGen-ed  August 31, 2014 at 08:57:33 AM by AutoGen 5.18.4
+ *  It has been AutoGen-ed  September  1, 2014 at 08:56:16 AM by AutoGen 5.18.4
  *  From the definitions    ntpdc-opts.def
  *  and the template file   options
  *
@@ -83,9 +83,9 @@ typedef enum {
 /** count of all options for ntpdc */
 #define OPTION_CT    15
 /** ntpdc version */
-#define NTPDC_VERSION       "4.2.7p468"
+#define NTPDC_VERSION       "4.2.7p469"
 /** Full ntpdc version text */
-#define NTPDC_FULL_VERSION  "ntpdc 4.2.7p468"
+#define NTPDC_FULL_VERSION  "ntpdc 4.2.7p469"
 
 /**
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
--- ntpq/ntpq-opts.h
+++ ntpq/ntpq-opts.h
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (ntpq-opts.h)
  *
- *  It has been AutoGen-ed  August 31, 2014 at 08:57:55 AM by AutoGen 5.18.4
+ *  It has been AutoGen-ed  September  1, 2014 at 08:56:38 AM by AutoGen 5.18.4
  *  From the definitions    ntpq-opts.def
  *  and the template file   options
  *
@@ -83,9 +83,9 @@ typedef enum {
 /** count of all options for ntpq */
 #define OPTION_CT    15
 /** ntpq version */
-#define NTPQ_VERSION       "4.2.7p468"
+#define NTPQ_VERSION       "4.2.7p469"
 /** Full ntpq version text */
-#define NTPQ_FULL_VERSION  "ntpq 4.2.7p468"
+#define NTPQ_FULL_VERSION  "ntpq 4.2.7p469"
 
 /**
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
