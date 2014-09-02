91ec7eb46a3315cc94bd189d00ec06768a96f778
Harlan Stenn <stenn@ntp.org>
2014-09-02 08:58:48 -0500

    NTP_4_2_7P470

--- util/ntp-keygen-opts.h
+++ util/ntp-keygen-opts.h
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (ntp-keygen-opts.h)
  *
- *  It has been AutoGen-ed  September  1, 2014 at 08:57:33 AM by AutoGen 5.18.4
+ *  It has been AutoGen-ed  September  2, 2014 at 08:57:25 AM by AutoGen 5.18.5pre1
  *  From the definitions    ntp-keygen-opts.def
  *  and the template file   options
  *
@@ -94,9 +94,9 @@ typedef enum {
 /** count of all options for ntp-keygen */
 #define OPTION_CT    26
 /** ntp-keygen version */
-#define NTP_KEYGEN_VERSION       "4.2.7p469"
+#define NTP_KEYGEN_VERSION       "4.2.7p470"
 /** Full ntp-keygen version text */
-#define NTP_KEYGEN_FULL_VERSION  "ntp-keygen (ntp) 4.2.7p469"
+#define NTP_KEYGEN_FULL_VERSION  "ntp-keygen (ntp) 4.2.7p470"
 
 /**
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
