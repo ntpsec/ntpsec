59720193f60d82181bfb52eec2b3e6f38e337cbd
Harlan Stenn <stenn@ntp.org>
2015-01-07 08:05:51 -0500

    NTP_4_2_8P1_BETA5

--- util/ntp-keygen-opts.c
+++ util/ntp-keygen-opts.c
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (ntp-keygen-opts.c)
  *
- *  It has been AutoGen-ed  January  4, 2015 at 06:14:53 AM by AutoGen 5.18.5pre4
+ *  It has been AutoGen-ed  January  7, 2015 at 03:05:05 AM by AutoGen 5.18.5pre4
  *  From the definitions    ntp-keygen-opts.def
  *  and the template file   options
  *
@@ -72,7 +72,7 @@ extern FILE * option_usage_fp;
  *  static const strings for ntp-keygen options
  */
 static char const ntp_keygen_opt_strs[2381] =
-/*     0 */ "ntp-keygen (ntp) 4.2.8p1-beta4\n"
+/*     0 */ "ntp-keygen (ntp) 4.2.8p1-beta5\n"
             "Copyright (C) 1970-2014 The University of Delaware, all rights reserved.\n"
             "This is free software. It is licensed for use, modification and\n"
             "redistribution under the terms of the NTP License, copies of which\n"
@@ -163,14 +163,14 @@ static char const ntp_keygen_opt_strs[2381] =
 /*  2152 */ "no-load-opts\0"
 /*  2165 */ "no\0"
 /*  2168 */ "NTP_KEYGEN\0"
-/*  2179 */ "ntp-keygen (ntp) - Create a NTP host key - Ver. 4.2.8p1-beta4\n"
+/*  2179 */ "ntp-keygen (ntp) - Create a NTP host key - Ver. 4.2.8p1-beta5\n"
             "Usage:  %s [ -<flag> [<val>] | --<name>[{=| }<val>] ]...\n\0"
 /*  2299 */ "$HOME\0"
 /*  2305 */ ".\0"
 /*  2307 */ ".ntprc\0"
 /*  2314 */ "http://bugs.ntp.org, bugs@ntp.org\0"
 /*  2348 */ "\n\0"
-/*  2350 */ "ntp-keygen (ntp) 4.2.8p1-beta4";
+/*  2350 */ "ntp-keygen (ntp) 4.2.8p1-beta5";
 
 /**
  *  imbits option description:
@@ -1308,7 +1308,7 @@ static void bogus_function(void) {
      translate option names.
    */
   /* referenced via ntp_keygenOptions.pzCopyright */
-  puts(_("ntp-keygen (ntp) 4.2.8p1-beta4\n\
+  puts(_("ntp-keygen (ntp) 4.2.8p1-beta5\n\
 Copyright (C) 1970-2014 The University of Delaware, all rights reserved.\n\
 This is free software. It is licensed for use, modification and\n\
 redistribution under the terms of the NTP License, copies of which\n\
@@ -1406,14 +1406,14 @@ provided \"as is\" without express or implied warranty.\n"));
   puts(_("load options from a config file"));
 
   /* referenced via ntp_keygenOptions.pzUsageTitle */
-  puts(_("ntp-keygen (ntp) - Create a NTP host key - Ver. 4.2.8p1-beta4\n\
+  puts(_("ntp-keygen (ntp) - Create a NTP host key - Ver. 4.2.8p1-beta5\n\
 Usage:  %s [ -<flag> [<val>] | --<name>[{=| }<val>] ]...\n"));
 
   /* referenced via ntp_keygenOptions.pzExplain */
   puts(_("\n"));
 
   /* referenced via ntp_keygenOptions.pzFullVersion */
-  puts(_("ntp-keygen (ntp) 4.2.8p1-beta4"));
+  puts(_("ntp-keygen (ntp) 4.2.8p1-beta5"));
 
   /* referenced via ntp_keygenOptions.pzFullUsage */
   puts(_("<<<NOT-FOUND>>>"));
--- util/ntp-keygen-opts.h
+++ util/ntp-keygen-opts.h
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (ntp-keygen-opts.h)
  *
- *  It has been AutoGen-ed  January  4, 2015 at 06:14:52 AM by AutoGen 5.18.5pre4
+ *  It has been AutoGen-ed  January  7, 2015 at 03:05:04 AM by AutoGen 5.18.5pre4
  *  From the definitions    ntp-keygen-opts.def
  *  and the template file   options
  *
@@ -94,9 +94,9 @@ typedef enum {
 /** count of all options for ntp-keygen */
 #define OPTION_CT    26
 /** ntp-keygen version */
-#define NTP_KEYGEN_VERSION       "4.2.8p1-beta4"
+#define NTP_KEYGEN_VERSION       "4.2.8p1-beta5"
 /** Full ntp-keygen version text */
-#define NTP_KEYGEN_FULL_VERSION  "ntp-keygen (ntp) 4.2.8p1-beta4"
+#define NTP_KEYGEN_FULL_VERSION  "ntp-keygen (ntp) 4.2.8p1-beta5"
 
 /**
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
