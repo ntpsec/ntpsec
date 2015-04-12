44aaad399f92235ff77c108ee5ce92b8517d4ed1
Juergen Perlinger <perlinger@ntp.org>
2015-04-12 17:49:34 -0500

    WinBuild: cleanup of VS2008/VS2013 build, add 64bit build to VS2008

--- ports/winnt/vs2008/ntpd/gen-ntp_keyword.bat
+++ ports/winnt/vs2008/ntpd/gen-ntp_keyword.bat
@@ -4,9 +4,9 @@ REM helper to invoke keyword-gen and possibly update ntp_keyword.h
 REM Usage:
 REM   gen-ntp_keyword dir_containing_keyword-gen.exe
 REM
-if "%1"=="" goto Usage
-if not exist %1\keyword-gen.exe goto ExeNotFound
-%1\keyword-gen.exe ..\..\..\..\ntpd\ntp_parser.h > new_keyword.h
+if "x%1"=="x" goto Usage
+if not exist "%1\keyword-gen.exe" goto ExeNotFound
+"%1\keyword-gen.exe" ..\..\..\..\ntpd\ntp_parser.h > new_keyword.h
 findstr /v diff_ignore_line new_keyword.h > new_keyword_cmp.h
 findstr /v diff_ignore_line ..\..\..\..\ntpd\ntp_keyword.h > ntp_keyword_cmp.h
 set meat_changed=0
@@ -14,13 +14,15 @@ fc /L ntp_keyword_cmp.h new_keyword_cmp.h > NUL
 if errorlevel 1 set meat_changed=1
 del ntp_keyword_cmp.h new_keyword_cmp.h
 if "0"=="%meat_changed%" goto SkipUpdate
-xcopy /yf new_keyword.h ..\..\..\..\ntpd\ntp_keyword.h
+copy /y /v new_keyword.h ..\..\..\..\ntpd\ntp_keyword.h
 findstr diff_ignore_line new_keyword.h > ..\..\..\..\ntpd\keyword-gen-utd
 echo updated keyword-gen-utd and ntp_keyword.h
 goto SkipSkipMsg
 
 :skipUpdate
 echo ntp_keyword.h is unchanged
+REM 'touch' the file by replacing it with a concatenation of itself and NUL:
+copy /b ..\..\..\..\ntpd\ntp_keyword.h+NUL: ..\..\..\..\ntpd\ntp_keyword.h
 
 :SkipSkipMsg
 set meat_changed=
