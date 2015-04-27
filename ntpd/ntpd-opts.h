5cad67e20ebbee1d405f22f0a0b11c8a35102a7e
Harlan Stenn <stenn@ntp.org>
2015-04-27 11:59:22 -0500

    NTP_4_3_21

--- ntpd/ntpd-opts.h
+++ ntpd/ntpd-opts.h
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (ntpd-opts.h)
  *
- *  It has been AutoGen-ed  April 26, 2015 at 01:41:19 PM by AutoGen 5.18.5
+ *  It has been AutoGen-ed  April 27, 2015 at 11:54:22 AM by AutoGen 5.18.5
  *  From the definitions    ntpd-opts.def
  *  and the template file   options
  *
@@ -106,9 +106,9 @@ typedef enum {
 /** count of all options for ntpd */
 #define OPTION_CT    38
 /** ntpd version */
-#define NTPD_VERSION       "4.3.20"
+#define NTPD_VERSION       "4.3.21"
 /** Full ntpd version text */
-#define NTPD_FULL_VERSION  "ntpd 4.3.20"
+#define NTPD_FULL_VERSION  "ntpd 4.3.21"
 
 /**
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
--- sntp/sntp-opts.h
+++ sntp/sntp-opts.h
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (sntp-opts.h)
  *
- *  It has been AutoGen-ed  April 26, 2015 at 02:40:41 PM by AutoGen 5.18.5
+ *  It has been AutoGen-ed  April 27, 2015 at 11:48:53 AM by AutoGen 5.18.5
  *  From the definitions    sntp-opts.def
  *  and the template file   options
  *
@@ -91,9 +91,9 @@ typedef enum {
 /** count of all options for sntp */
 #define OPTION_CT    23
 /** sntp version */
-#define SNTP_VERSION       "4.3.20"
+#define SNTP_VERSION       "4.3.21"
 /** Full sntp version text */
-#define SNTP_FULL_VERSION  "sntp 4.3.20"
+#define SNTP_FULL_VERSION  "sntp 4.3.21"
 
 /**
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
