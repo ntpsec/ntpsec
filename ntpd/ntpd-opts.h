aecbdacb17bfd41de07bd4853c931d78c0ebaf14
Harlan Stenn <stenn@ntp.org>
2015-04-29 11:58:54 -0500

    NTP_4_3_23

--- ntpd/ntpd-opts.h
+++ ntpd/ntpd-opts.h
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (ntpd-opts.h)
  *
- *  It has been AutoGen-ed  April 28, 2015 at 11:54:32 AM by AutoGen 5.18.5
+ *  It has been AutoGen-ed  April 29, 2015 at 11:53:58 AM by AutoGen 5.18.5
  *  From the definitions    ntpd-opts.def
  *  and the template file   options
  *
@@ -106,9 +106,9 @@ typedef enum {
 /** count of all options for ntpd */
 #define OPTION_CT    38
 /** ntpd version */
-#define NTPD_VERSION       "4.3.22"
+#define NTPD_VERSION       "4.3.23"
 /** Full ntpd version text */
-#define NTPD_FULL_VERSION  "ntpd 4.3.22"
+#define NTPD_FULL_VERSION  "ntpd 4.3.23"
 
 /**
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
