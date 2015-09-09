6d4b0c3540ebed2e99f5f6544b9e5b6325826ded
Amar Takhar <amar@ntpsec.org>
2015-09-09 17:47:20 -0400

    Fix internal header dependencies.
    I havn't hit this before in previous projects.  I was appending to INCPATHS when
    I should have been appending to includes for a target.  This ensures the
    dependencies are picked up properly.
    
    For some reason, this breaks the build I don't know why I will be following up
    with another commit to fix breakage.

--- pylib/waf.py
+++ pylib/waf.py
@@ -3,38 +3,38 @@ from waflib.TaskGen import feature, before, after, extension, after_method, befo
 from waflib.Task import Task
 
 
-@after_method('apply_incpaths')
+@before_method('apply_incpaths')
 @feature('bld_include')
 def insert_blddir(self):
-	srcnode = self.bld.bldnode.abspath()
-	self.env.prepend_value('INCPATHS', srcnode)
+	bldnode = self.bld.bldnode.abspath()
+	self.includes += [bldnode]
 
-@after_method('apply_incpaths')
+@before_method('apply_incpaths')
 @feature('src_include')
 def insert_srcdir(self):
 	srcnode = self.bld.srcnode.abspath()
-	self.env.prepend_value('INCPATHS', ["%s/include" % srcnode])
+	self.includes += ["%s/include" % srcnode]
 
 
-@after_method('apply_incpaths')
+@before_method('apply_incpaths')
 @feature('libisc_include')
 def insert_libiscdir(self):
 	srcnode = self.bld.srcnode.abspath()
-	self.env.prepend_value('INCPATHS', ["%s/lib/isc/include/" % srcnode, "%s/lib/isc/unix/include/" % srcnode])
+	self.includes += ["%s/lib/isc/include/" % srcnode, "%s/lib/isc/unix/include/" % srcnode]
 
 
-@after_method('apply_incpaths')
+@before_method('apply_incpaths')
 @feature('libisc_pthread_include')
 def insert_libiscpthreaddir(self):
 	srcnode = self.bld.srcnode.abspath()
-	self.env.prepend_value('INCPATHS', ["%s/lib/isc/pthreads/include/" % srcnode])
+	self.includes += ["%s/lib/isc/pthreads/include/" % srcnode]
 
 
-@after_method('apply_incpaths')
+@before_method('apply_incpaths')
 @feature('libopts_include')
 def insert_libopts(self):
 	srcnode = self.bld.srcnode.abspath()
-	self.env.prepend_value('INCPATHS', ["%s/sntp/libopts/" % srcnode])
+	self.includes += ["%s/sntp/libopts/" % srcnode]
 
 
 class version(Task):
