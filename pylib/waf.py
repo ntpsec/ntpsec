f01cef660ef16a14edce5bd7b06a1ca4c0b5ad40
Amar Takhar <verm@darkbeer.org>
2015-09-17 09:39:04 -0400

    Remove version.c as it is no longer needed.

--- pylib/waf.py
+++ pylib/waf.py
@@ -28,17 +28,3 @@ def insert_libiscdir(self):
 def insert_libiscpthreaddir(self):
 	srcnode = self.bld.srcnode.abspath()
 	self.includes += ["%s/lib/isc/pthreads/include/" % srcnode]
-
-
-class version(Task):
-	vars = ['NTPS_VERSION_STRING', 'TARGET']
-	def run(self):
-		self.outputs[0].write("const char *Version = \"%s %s\";" % (self.env.TARGET, self.env.NTPS_VERSION_STRING))
-
-@before_method('process_source')
-@feature('ntp_version')
-def ntp_version(self):
-	n = self.path.find_or_declare('version.c')
-	tsk = self.create_task('version', [], [n])
-	tsk.env.TARGET = self.target
-	self.source.extend([n])
