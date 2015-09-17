b5aabb7184fc306e0ffefe2ba79563d538b8453b
Eric S. Raymond <esr@thyrsus.com>
2015-09-17 15:13:07 -0400

    Test only for HAVE_STDATOMIC_H; atomic_thread_fence() can be a macro.

--- pylib/configure.py
+++ pylib/configure.py
@@ -223,7 +223,6 @@ def cmd_configure(ctx):
 	functions = (
 		('adjtimex', "sys/timex.h"),
 		('arc4random', "stdlib.h"),
-		('atomic_thread_fence', "stdatomic.h"),
 		('arc4random_buf', "stdlib.h"),
 		('getclock', "sys/timers.h"),
 		('getpassphrase', "stdlib.h"), # Sun systems
