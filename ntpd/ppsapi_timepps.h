3e100f5829e03ba3318ec5beabcee40c44c5b018
Eric S. Raymond <esr@thyrsus.com>
2015-09-19 07:00:28 -0400

    Remove support for Sun and SCO-specific PPS API from before RFC2783.
    This allows us to remove a bunch of tangled #ifdefs and some local
    PPS API headers that are almost certainly obsolete.
    
    Affects old Sun, Solaris, and SCO systems. Touches the parse driver
    and the MX4200 refclock (the latter only trivially).  Leaves the
    RFC2783 support itself in place.
    
    Leaves the parse driver documentation possibly a bit more out of sync
    with reality than it was, but the reader has already been warned that
    it is stale.
    
    Research suggests that the last system for which any of this stuff was
    relevant shipped in 1996. Compilation should break noisily if anyone tries
    to build on a system old enough for it to matter.

--- ntpd/ppsapi_timepps.h
+++ ntpd/ppsapi_timepps.h
@@ -1,8 +1,8 @@
 /* ppsapi_timepps.h */
 
 /*
- * This logic first tries to get the timepps.h file from a standard
- * location, and then from our include/ subdirectory.
+ * This logic tries to get the timepps.h file from standard
+ * location.
  */
 
 #ifdef HAVE_TIMEPPS_H
@@ -10,17 +10,5 @@
 #else
 # ifdef HAVE_SYS_TIMEPPS_H
 #  include <sys/timepps.h>
-# else
-#  ifdef HAVE_CIOGETEV
-#   include "timepps-SunOS.h"
-#  else
-#   ifdef HAVE_TIOCGPPSEV
-#    include "timepps-Solaris.h"
-#   else
-#    ifdef TIOCDCDTIMESTAMP
-#     include "timepps-SCO.h"
-#    endif
-#   endif
-#  endif
 # endif
 #endif
