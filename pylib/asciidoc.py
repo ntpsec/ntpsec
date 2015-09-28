0e4bdf782a60dc72ed40cd5e95bb092f2d25de42
Amar Takhar <verm@darkbeer.org>
2015-09-28 16:01:16 -0400

    Add a note about ASCIIDOC_FLAGS.

--- pylib/asciidoc.py
+++ pylib/asciidoc.py
@@ -3,7 +3,7 @@
 from waflib import Task
 from waflib.TaskGen import extension
 
-
+# ASCIIDOC_FLAGS are almost always needed and need to be set by the user.
 class asciidoc(Task.Task):
 	color	= "BLUE"
 	run_str = '${BIN_ASCIIDOC} -b html5 -a linkcss ${ASCIIDOC_FLAGS} -o ${TGT[0].name} ${SRC[0].abspath()}'
