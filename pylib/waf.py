4b948482cc4bb9f95b111dc38bfd157ed69cc148
Amar Takhar <verm@darkbeer.org>
2015-09-28 19:05:50 -0400

    Document some functions.

--- pylib/waf.py
+++ pylib/waf.py
@@ -29,6 +29,8 @@ def insert_libiscpthreaddir(self):
 	srcnode = self.bld.srcnode.abspath()
 	self.includes += ["%s/lib/isc/pthreads/include/" % srcnode]
 
+
+# Create version.c
 class version(Task):
 	vars = ['NTPS_VERSION_STRING', 'TARGET']
 	def run(self):
@@ -37,6 +39,8 @@ const char *Version = "%s " __DATE__ " " __TIME__;
 const char *VVersion = "CFLAGS=%s LDFLAGS=%s";
 """ % (self.env.NTPS_VERSION_STRING, "Need-CFLAGS", "Need-LDFLAGS"))
 
+# Use in features= to generate a version.c for that target.
+# This uses the target name for the version string.
 @before_method('process_source')
 @feature('ntp_version')
 def ntp_version(self):
