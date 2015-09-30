3ef966183a9d1bfa626c703f872b93879e979b34
Eric S. Raymond <esr@thyrsus.com>
2015-09-30 11:29:05 -0400

    Forward-port util/rs6000.c and write a build recipe for it.

--- util/testrs6000.c
+++ util/testrs6000.c
@@ -5,15 +5,31 @@
  * the program will print an occasional "result=nnnnnn" (the residual
  * slew from adjtime()).
  *
+ * This version is in modern ANSI C rather than the archaic dialect of
+ * the RS6000's day.
+ *
  * Run this as root!
  */
+#include <stdlib.h>
+#include <unistd.h>
 #include <signal.h>
 #include <sys/time.h>
 #include <time.h>
 #include <stdio.h>
 
-int timeout();
 struct timeval adjustment, result;
+
+static void
+timeout(int sig)
+{
+	if (adjtime(&adjustment, &result))
+	    printf("adjtime call failed\n");
+	if (result.tv_sec != 0 || result.tv_usec != 0) {
+		printf("result = %d.%6d  ",
+		       (int) result.tv_sec,
+		       (int) result.tv_usec);
+	}
+}
 
 int
 main (
@@ -25,32 +41,23 @@ main (
 	int i;
 	time_t curtime;
 	char ascbuf[BUFSIZ];
+	struct sigaction sa;
 
 	curtime = time(0);
-	printf("Starting: %s", ctime_r(&curtime));
+	printf("Starting: %s", ctime_r(&curtime, ascbuf));
 	value.it_interval.tv_sec = value.it_value.tv_sec = 1;
 	value.it_interval.tv_usec = value.it_value.tv_usec = 0;
 	adjustment.tv_sec = 0;
 	adjustment.tv_usec = -2000;
+	sa.sa_handler = timeout;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
 	signal(SIGALRM, timeout);
 	setitimer(ITIMER_REAL, &value, &oldvalue);
 	for (i=0; i<10; i++) {
 		pause();
 	}
+
+	exit(0);
 }
 
-int
-timeout(
-	int sig,
-	int code,
-	struct sigcontext *scp
-	)
-{
-	signal (SIGALRM, timeout);
-	if (adjtime(&adjustment, &result)) 
-	    printf("adjtime call failed\n");
-	if (result.tv_sec != 0 || result.tv_usec != 0) {
-		printf("result.u = %d.%06.6d  ", (int) result.tv_sec,
-		       (int) result.tv_usec);
-	}
-}
