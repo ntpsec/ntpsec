3c7444ec9182e63e1cc099d297dfdf38c654fb1f
Daniel Fox Franke <dfoxfranke@gmail.com>
2015-10-06 16:23:20 -0400

    libsodium: remove initialization of unpackaged functionality from sodium_init()

--- libsodium/sodium/core.c
+++ libsodium/sodium/core.c
@@ -1,8 +1,6 @@
 
 #include "core.h"
-#include "crypto_onetimeauth.h"
 #include "randombytes.h"
-#include "runtime.h"
 #include "utils.h"
 
 static int initialized;
@@ -13,13 +11,8 @@ sodium_init(void)
     if (initialized != 0) {
         return 1;
     }
-    sodium_runtime_get_cpu_features();
-    if (crypto_onetimeauth_pick_best_implementation() == NULL) {
-        return -1; /* LCOV_EXCL_LINE */
-    }
     randombytes_stir();
-    _sodium_alloc_init();
-    initialized = 1;
 
+    initialized = 1;
     return 0;
 }
