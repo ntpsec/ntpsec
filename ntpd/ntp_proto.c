47054154b6e59ddd05c89a1961283b2c82f68b58
Matthew Van Gundy <mvangund@cisco.com>
2015-10-20 22:02:24 -0400

    Prevent improperly authenticated packets from mobilizing passive associations
    AKA "NAK to the future"

--- ntpd/ntp_proto.c
+++ ntpd/ntp_proto.c
@@ -1097,6 +1097,27 @@ receive(
 				sys_restricted++;
 				return;
 			}
+
+			/*
+			 * If we got here, the packet isn't part of an
+			 * existing association, it isn't correctly
+			 * authenticated, and it didn't meet either of
+			 * the previous two special cases so we should
+			 * just drop it on the floor.  For example,
+			 * crypto-NAKs (is_authentic == AUTH_CRYPTO)
+			 * will make it this far.
+			 */
+#ifdef DEBUG
+			if (debug) {
+				 printf(
+					 "receive: at %ld refusing to mobilize passive association"
+					 " with unknown peer %s mode %d keyid %08x len %d auth %d\n",
+					 current_time, stoa(&rbufp->recv_srcadr), hismode, skeyid,
+					 authlen + has_mac, is_authentic);
+			}
+#endif
+			sys_declined++;
+			return;
 		}
 
 		/*
