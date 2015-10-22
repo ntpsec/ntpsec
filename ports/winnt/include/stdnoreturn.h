77c553db3e402e7693312c719779e3a8e73ff655
Eric S. Raymond <esr@thyrsus.com>
2015-10-22 07:09:11 -0400

    License exception cleanup in ports/winnt/include/stdnoreturn.h
    Contained /* DO NOT EDIT! GENERATED AUTOMATICALLY! */ and a copy of
    the GPL.  This was some mistake with a templating system; Paul is
    not the sort to try to GPLize an entire codebase by stealth.  Deleted.
    
    Approved-by: Mark Atwood <me@mark.atwood.name>

--- ports/winnt/include/stdnoreturn.h
+++ ports/winnt/include/stdnoreturn.h
@@ -1,23 +1,8 @@
-/* DO NOT EDIT! GENERATED AUTOMATICALLY! */
-/* A substitute for ISO C11 <stdnoreturn.h>.
-
-   Copyright 2012-2014 Free Software Foundation, Inc.
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU Lesser General Public License as published by
-   the Free Software Foundation; either version 2.1, or (at your option)
-   any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public License
-   along with this program; if not, see <http://www.gnu.org/licenses/>.  */
-
-/* Written by Paul Eggert.  */
-
+/*
+ * A substitute for ISO C11 <stdnoreturn.h>
+ *
+ * Written by Paul Eggert.
+ */
 #ifndef noreturn
 
 /* ISO C11 <stdnoreturn.h> for platforms that lack it.
