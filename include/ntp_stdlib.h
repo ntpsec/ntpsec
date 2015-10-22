457363a28dcc8665e5ffa0fe13151c22bb11eb83
Eric S. Raymond <esr@thyrsus.com>
2015-10-22 10:45:29 -0400

    Forward-port fixes for NTP Classic [Bug 2845] Harden memory allocation in ntpd.

--- include/ntp_stdlib.h
+++ include/ntp_stdlib.h
@@ -121,26 +121,35 @@ extern	uint32_t	addr2refid	(sockaddr_u *);
 /* emalloc.c */
 #ifndef EREALLOC_CALLSITE	/* ntp_malloc.h defines */
 extern	void *	ereallocz	(void *, size_t, size_t, int);
-#define	erealloczsite(p, n, o, z, f, l) ereallocz(p, n, o, (z))
-#define	emalloc(n)		ereallocz(NULL, n, 0, false)
+extern	void *	oreallocarray	(void *optr, size_t nmemb, size_t size);
+#define	erealloczsite(p, n, o, z, f, l) ereallocz((p), (n), (o), (z))
+#define	emalloc(n)		ereallocz(NULL, (n), 0, false)
 #define	emalloc_zero(c)		ereallocz(NULL, (c), 0, true)
-#define	erealloc(p, c)		ereallocz(p, (c), 0, false)
-#define erealloc_zero(p, n, o)	ereallocz(p, n, (o), true)
-extern	char *	estrdup_impl	(const char *);
+#define	erealloc(p, c)		ereallocz((p), (c), 0, false)
+#define erealloc_zero(p, n, o)	ereallocz((p), (n), (o), true)
+#define ereallocarray(p, n, s)	oreallocarray((p), (n), (s))
+#define eallocarray(n, s)	oreallocarray(NULL, (n), (s))
+extern	char *	estrdup_impl(const char *);
 #define	estrdup(s)		estrdup_impl(s)
 #else
 extern	void *	ereallocz	(void *, size_t, size_t, int,
+				 const char *, int);
+extern	void *	oreallocarray	(void *optr, size_t nmemb, size_t size,
 				 const char *, int);
 #define erealloczsite		ereallocz
 #define	emalloc(c)		ereallocz(NULL, (c), 0, false, \
 					  __FILE__, __LINE__)
 #define	emalloc_zero(c)		ereallocz(NULL, (c), 0, true, \
 					  __FILE__, __LINE__)
-#define	erealloc(p, c)		ereallocz(p, (c), 0, false, \
+#define	erealloc(p, c)		ereallocz((p), (c), 0, false,	\
 					  __FILE__, __LINE__)
-#define	erealloc_zero(p, n, o)	ereallocz(p, n, (o), true, \
+#define	erealloc_zero(p, n, o)	ereallocz((p), n, (o), true,	\
 					  __FILE__, __LINE__)
-extern	char *	estrdup_impl	(const char *, const char *, int);
+#define ereallocarray(p, n, s)	oreallocarray((p), (n), (s), \
+ 					  __FILE__, __LINE__)
+#define eallocarray(n, s)	oreallocarray(NULL, (n), (s), \
+ 					  __FILE__, __LINE__)
+extern	char *	estrdup_impl(const char *, const char *, int);
 #define	estrdup(s) estrdup_impl((s), __FILE__, __LINE__)
 #endif
 
