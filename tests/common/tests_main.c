7d023c7fe8b9238223f48341d76ff3955f9b6dd2
Amar Takhar <verm@darkbeer.org>
2015-11-22 12:06:35 -0500

    Add test group runners.

--- tests/common/tests_main.c
+++ tests/common/tests_main.c
@@ -34,7 +34,38 @@ static void RunAllTests(void)
 #endif
 
 #ifdef TEST_LIBNTP_SSL
-	RUN_TEST_GROUP(ssl_init);
+//	RUN_TEST_GROUP(a_md5encrypt);
+//	RUN_TEST_GROUP(authkeys);
+//	RUN_TEST_GROUP(calendar);
+//	RUN_TEST_GROUP(caljulian);
+//	RUN_TEST_GROUP(caltontp);
+//	RUN_TEST_GROUP(calyearstart);
+//	RUN_TEST_GROUP(clocktime);
+//	RUN_TEST_GROUP(decodenetnum);
+//	RUN_TEST_GROUP(hextolfp);
+//	RUN_TEST_GROUP(humandate);
+//	RUN_TEST_GROUP(lfp);
+//	RUN_TEST_GROUP(lfptostr);
+//	RUN_TEST_GROUP(modetoa);
+//	RUN_TEST_GROUP(msyslog);
+//	RUN_TEST_GROUP(netof);
+//	RUN_TEST_GROUP(numtoa);
+//	RUN_TEST_GROUP(numtohost);
+//	RUN_TEST_GROUP(octtoint);
+//	RUN_TEST_GROUP(prettydate);
+//	RUN_TEST_GROUP(recvbuff);
+//	RUN_TEST_GROUP(refnumtoa);
+//	RUN_TEST_GROUP(sfptostr);
+//	RUN_TEST_GROUP(socktoa);
+//	RUN_TEST_GROUP(ssl_init);
+//	RUN_TEST_GROUP(statestr);
+//	RUN_TEST_GROUP(strtolfp);
+//	RUN_TEST_GROUP(timespec);
+//	RUN_TEST_GROUP(timevalops);
+	RUN_TEST_GROUP(tstotv);
+//	RUN_TEST_GROUP(tvtots);
+//	RUN_TEST_GROUP(vi64);
+//	RUN_TEST_GROUP(ymd2yd);
 #endif
 
 }
