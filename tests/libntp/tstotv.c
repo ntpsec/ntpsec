e204ee142369de0798ea3be938ae42fa62a56996
Amar Takhar <verm@darkbeer.org>
2015-11-22 12:07:03 -0500

    Add tstotv.c
     * Also add stubs for the rest of the tests in tests/wscript
     * Cleanup / conversion of common headers.

--- tests/libntp/tstotv.c
+++ tests/libntp/tstotv.c
@@ -1,7 +1,9 @@
-extern "C" {
+#include "config.h"
+
 #include "unity.h"
 #include "unity_fixture.h"
-}
+
+#include <sys/time.h>
 
 TEST_GROUP(tstotv);
 
@@ -11,60 +13,55 @@ TEST_TEAR_DOWN(tstotv) {}
 
 #include "libntptest.h"
 
-extern "C" {
 #include "ntp_fp.h"
 #include "timevalops.h"
-};
+//#include "ntpd.h"
 
-class tstotvTest : public libntptest {
-protected:
-	bool IsEqual(const timeval& expected,
-									   const timeval& actual) {
-		if (expected.tv_sec == actual.tv_sec &&
-			expected.tv_usec == actual.tv_usec) {
-			// Success
-			return true;
-		} else {
-			return false
-				<< "expected: " << expected.tv_sec << "."
-				<< expected.tv_usec
-				<< " but was: " << actual.tv_sec << "."
-				<< actual.tv_usec;
-		}
+u_long      current_time;
+
+static bool IsEqual(const struct timeval *expected, const struct timeval *actual) {
+	if (expected->tv_sec == actual->tv_sec &&
+		expected->tv_usec == actual->tv_usec) {
+		// Success
+		return true;
+	} else {
+		printf("Expected: %ld.%ld but was %ld.%ld\n", expected->tv_sec, expected->tv_usec, actual->tv_sec, actual->tv_usec);
+		return false;
 	}
+}
 
-	static const u_long HALF = 2147483648UL;
-};
+static const u_long HALF = 2147483648UL;
+
 
 TEST(tstotv, Seconds) {
 	const l_fp input = {50, 0}; // 50.0 s
-	const timeval expected = {50, 0};
-	timeval actual;
+	const struct timeval expected = {50, 0};
+	struct timeval actual;
 
 	TSTOTV(&input, &actual);
 
-	TEST_ASSERT_TRUE(IsEqual(expected, actual));
+	TEST_ASSERT_TRUE(IsEqual(&expected, &actual));
 }
 
 TEST(tstotv, MicrosecondsExact) {
 	const l_fp input = {50, HALF}; // 10.5 s
-	const timeval expected = {50, 500000};
-	timeval actual;
+	const struct timeval expected = {50, 500000};
+	struct timeval actual;
 
 	TSTOTV(&input, &actual);
 
-	TEST_ASSERT_TRUE(IsEqual(expected, actual));
+	TEST_ASSERT_TRUE(IsEqual(&expected, &actual));
 
 }
 
 TEST(tstotv, MicrosecondsRounding) {
 	const l_fp input = {50, 3865471UL}; // Should round to 50.0009
-	const timeval expected = {50, 900};
-	timeval actual;
+	const struct timeval expected = {50, 900};
+	struct timeval actual;
 
 	TSTOTV(&input, &actual);
 
-	TEST_ASSERT_TRUE(IsEqual(expected, actual));
+	TEST_ASSERT_TRUE(IsEqual(&expected, &actual));
 }
 
 TEST_GROUP_RUNNER(tstotv) {
