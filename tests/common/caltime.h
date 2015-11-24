bce7a4b7ddbd1998c367be12f19a5d73d27cb90e
Amar Takhar <verm@darkbeer.org>
2015-11-24 13:23:30 -0500

    Convert calyearstart.c and do some header cleanup.
      * Move time functions to caltime.(c|h)
      * Rename TEST_LIBNTP_SSL to TEST_LIBNTP

---
+++ tests/common/caltime.h
@@ -0,0 +1,3 @@
+time_t timefunc(time_t*);
+void   settime(int y, int m, int d, int H, int M, int S);
+
--- tests/libntp/cal.h
+++
@@ -1,3 +0,0 @@
-static time_t timefunc(time_t*);
-static void   settime(int y, int m, int d, int H, int M, int S);
-
