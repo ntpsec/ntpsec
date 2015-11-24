8d4472ee7e18779a9719ccf4e22dbf46291aa4c9
Amar Takhar <verm@darkbeer.org>
2015-11-24 17:56:54 -0500

    Convert prettydate.c

--- tests/libntp/prettydate.c
+++ tests/libntp/prettydate.c
@@ -1,7 +1,5 @@
-extern "C" {
 #include "unity.h"
 #include "unity_fixture.h"
-}
 
 TEST_GROUP(prettydate);
 
@@ -11,17 +9,12 @@ TEST_TEAR_DOWN(prettydate) {}
 
 #include "libntptest.h"
 
-extern "C" {
 #include "ntp_fp.h"
-};
 
-class prettydateTest : public libntptest {
-protected:
-	static const u_int32 HALF = 2147483648UL;
-};
+static const u_int32_t HALF = 2147483648UL;
 
 TEST(prettydate, ConstantDate) {
-	l_fp time = {3485080800UL, HALF}; // 2010-06-09 14:00:00.5
+	l_fp time = {{3485080800UL}, HALF}; // 2010-06-09 14:00:00.5
 
 	TEST_ASSERT_EQUAL_STRING("cfba1ce0.80000000  2010-06-09T14:00:00.500", gmprettydate(&time));
 }
