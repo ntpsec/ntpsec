be314ebfd48fc9830b98f44676bf8a1479b2527a
Amar Takhar <verm@darkbeer.org>
2015-11-25 18:53:19 -0500

    Convert decodenetnum.c
    One test is disabled I will get back to it later.

--- tests/libntp/sockaddrtest.h
+++ tests/libntp/sockaddrtest.h
@@ -3,56 +3,45 @@
 
 #include "libntptest.h"
 
-extern "C" {
 #include "ntp.h"
-};
 
-class sockaddrtest : public libntptest {
-protected:
-	bool IsEqual(const sockaddr_u &expected, const sockaddr_u &actual) {
-		if (expected.sa.sa_family != actual.sa.sa_family) {
-			return false
-				<< "Expected sa_family: " << expected.sa.sa_family
-				<< " but got: " << actual.sa.sa_family;
-		}
-
-		if (actual.sa.sa_family == AF_INET) { // IPv4
-			if (expected.sa4.sin_port == actual.sa4.sin_port &&
-				memcmp(&expected.sa4.sin_addr, &actual.sa4.sin_addr,
-					   sizeof(in_addr)) == 0) {
-				return true;
-			} else {
-				return false
-					<< "IPv4 comparision failed, expected: "
-					<< expected.sa4.sin_addr.s_addr
-					<< "(" << socktoa(&expected) << ")"
-					<< " but was: "
-					<< actual.sa4.sin_addr.s_addr
-					<< "(" << socktoa(&actual) << ")";
-			}
-		} else if (actual.sa.sa_family == AF_INET6) { //IPv6
-			if (expected.sa6.sin6_port == actual.sa6.sin6_port &&
-				memcmp(&expected.sa6.sin6_addr, &actual.sa6.sin6_addr,
-					   sizeof(in6_addr)) == 0) {
-				return true;
-			} else {
-				return false
-					<< "IPv6 comparision failed";
-			}
-		} else { // Unknown family
-			return false
-				<< "Unknown sa_family: " << actual.sa.sa_family;
-		}
+bool IsEqualS(const sockaddr_u *expected, const sockaddr_u *actual) {
+	if (expected->sa.sa_family != actual->sa.sa_family) {
+		printf("Expected sa_family: %hhu but got: %hhu\n", expected->sa.sa_family, actual->sa.sa_family);
+		return false;
 	}
 
-	sockaddr_u CreateSockaddr4(const char* address, unsigned int port) {
-		sockaddr_u s;
-		s.sa4.sin_family = AF_INET;
-		s.sa4.sin_addr.s_addr = inet_addr(address);
-		SET_PORT(&s, port);
-
-		return s;
+	if (actual->sa.sa_family == AF_INET) { // IPv4
+		if (expected->sa4.sin_port == actual->sa4.sin_port &&
+			memcmp(&expected->sa4.sin_addr, &actual->sa4.sin_addr,
+				   sizeof(in_addr_t)) == 0) {
+			return true;
+		} else {
+			printf("IPv4 comparision failed, expected: %u (%s) but was: %u (%s)\n", expected->sa4.sin_addr.s_addr, socktoa(expected), actual->sa4.sin_addr.s_addr, socktoa(actual));
+			return false;
+		}
+	} else if (actual->sa.sa_family == AF_INET6) { //IPv6
+		if (expected->sa6.sin6_port == actual->sa6.sin6_port &&
+			memcmp(&expected->sa6.sin6_addr, &actual->sa6.sin6_addr,
+				   sizeof(struct in6_addr)) == 0) {
+			return true;
+		} else {
+			printf("IPv6 comparision failed\n");
+			return false;
+		}
+	} else { // Unknown family
+		printf("Unknown sa_family: %hhu\n", actual->sa.sa_family);
+		return false;
 	}
-};
+}
+
+sockaddr_u CreateSockaddr4(const char* address, unsigned int port) {
+	sockaddr_u s;
+	s.sa4.sin_family = AF_INET;
+	s.sa4.sin_addr.s_addr = inet_addr(address);
+	SET_PORT(&s, port);
+	return s;
+}
+
 
 #endif // GUARD_TESTS_SOCKADDRTEST_H
