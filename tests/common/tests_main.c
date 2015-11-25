860b755666b36a1aaf3115d16803c0b66fcd91ee
Amar Takhar <verm@darkbeer.org>
2015-11-24 19:54:39 -0500

    Convert numtoa.c.

--- tests/common/tests_main.c
+++ tests/common/tests_main.c
@@ -52,7 +52,7 @@ static void RunAllTests(void)
 //	RUN_TEST_GROUP(modetoa);
 //	RUN_TEST_GROUP(msyslog);
 //	RUN_TEST_GROUP(netof);
-//	RUN_TEST_GROUP(numtoa);
+	RUN_TEST_GROUP(numtoa);
 //	RUN_TEST_GROUP(numtohost);
 	RUN_TEST_GROUP(prettydate);
 //	RUN_TEST_GROUP(recvbuff);
--- tests/libntp/numtoa.c
+++ tests/libntp/numtoa.c
@@ -1,7 +1,5 @@
-extern "C" {
 #include "unity.h"
 #include "unity_fixture.h"
-}
 
 TEST_GROUP(numtoa);
 
@@ -11,19 +9,16 @@ TEST_TEAR_DOWN(numtoa) {}
 
 #include "libntptest.h"
 
-class numtoaTest : public libntptest {
-};
-
 TEST(numtoa, Address) {
-	u_int32 input = htonl(3221225472UL+512UL+1UL); // 192.0.2.1
+	u_int32_t input = htonl(3221225472UL+512UL+1UL); // 192.0.2.1
 
 	TEST_ASSERT_EQUAL_STRING("192.0.2.1", numtoa(input));
 }
 
 TEST(numtoa, Netmask) {
 	// 255.255.255.0
-	u_int32 hostOrder = 255UL*256UL*256UL*256UL + 255UL*256UL*256UL + 255UL*256UL;
-	u_int32 input = htonl(hostOrder);
+	u_int32_t hostOrder = 255UL*256UL*256UL*256UL + 255UL*256UL*256UL + 255UL*256UL;
+	u_int32_t input = htonl(hostOrder);
 
 	TEST_ASSERT_EQUAL_STRING("255.255.255.0", numtoa(input));
 }
