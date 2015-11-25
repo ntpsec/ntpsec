60dad5d1f5f9664203879d672784311b9596fecc
Amar Takhar <verm@darkbeer.org>
2015-11-24 20:16:18 -0500

    Convert numtohost.c

--- tests/common/tests_main.c
+++ tests/common/tests_main.c
@@ -53,7 +53,7 @@ static void RunAllTests(void)
 //	RUN_TEST_GROUP(msyslog);
 //	RUN_TEST_GROUP(netof);
 	RUN_TEST_GROUP(numtoa);
-//	RUN_TEST_GROUP(numtohost);
+	RUN_TEST_GROUP(numtohost);
 	RUN_TEST_GROUP(prettydate);
 //	RUN_TEST_GROUP(recvbuff);
 //	RUN_TEST_GROUP(refnumtoa);
