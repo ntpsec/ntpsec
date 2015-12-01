30b743da9e35f7d95ecf50640260f35a8d71900f
Amar Takhar <verm@darkbeer.org>
2015-12-01 02:35:00 -0500

    Convert libntp/clocktime.c.

--- tests/libntp/clocktime.c
+++ tests/libntp/clocktime.c
@@ -1,18 +1,13 @@
 #include "config.h"
 #include "ntp_stdlib.h"
 
-extern "C" {
 #include "unity.h"
 #include "unity_fixture.h"
-}
+
+#include "ntp_calendar.h"
+#include "caltime.h"
 
 TEST_GROUP(clocktime);
-
-TEST_SETUP(clocktime) {}
-
-TEST_TEAR_DOWN(clocktime) {}
-
-#include "cal.h"
 
 // ---------------------------------------------------------------------
 // test fixture
@@ -21,20 +16,13 @@ TEST_TEAR_DOWN(clocktime) {}
 // function for getting the current system time, so the tests are not
 // dependent on the actual system time.
 
-class clocktimeTest : public libntptest {
-	virtual void SetUp();
-	virtual void TearDown();
-};
-
-void clocktimeTest::SetUp()
-{
-    ntpcal_set_timefunc(timefunc);
-    settime(2000, 1, 1, 0, 0, 0);
+TEST_SETUP(clocktime) {
+	ntpcal_set_timefunc(timefunc);
+	settime(2000, 1, 1, 0, 0, 0);
 }
 
-void clocktimeTest::TearDown()
-{
-    ntpcal_set_timefunc(NULL);
+TEST_TEAR_DOWN(clocktime) {
+	ntpcal_set_timefunc(NULL);
 }
 
 // ---------------------------------------------------------------------
@@ -42,13 +30,13 @@ void clocktimeTest::TearDown()
 
 TEST(clocktime, CurrentYear) {
 	// Timestamp: 2010-06-24 12:50:00Z
-	const u_int32 timestamp = 3486372600UL;
-	const u_int32 expected	= timestamp; // exactly the same.
+	const u_int32_t timestamp = 3486372600UL;
+	const u_int32_t expected	= timestamp; // exactly the same.
 
 	const int yday=175, hour=12, minute=50, second=0, tzoff=0;
 
 	u_long yearstart=0;
-	u_int32 actual;
+	u_int32_t actual;
 
 	TEST_ASSERT_TRUE(clocktime(yday, hour, minute, second, tzoff, timestamp,
 						  &yearstart, &actual));
@@ -56,7 +44,7 @@ TEST(clocktime, CurrentYear) {
 }
 
 TEST(clocktime, CurrentYearFuzz) {
-	/* 
+	/*
 	 * Timestamp (rec_ui) is: 2010-06-24 12:50:00
 	 * Time sent into function is 12:00:00.
 	 *
@@ -64,13 +52,13 @@ TEST(clocktime, CurrentYearFuzz) {
 	 * timestamp for the 12:00:00 time.
 	 */
 
-	const u_int32 timestamp = 3486372600UL; // 2010-06-24 12:50:00Z
-	const u_int32 expected	= 3486369600UL; // 2010-06-24 12:00:00Z
+	const u_int32_t timestamp = 3486372600UL; // 2010-06-24 12:50:00Z
+	const u_int32_t expected	= 3486369600UL; // 2010-06-24 12:00:00Z
 
 	const int yday=175, hour=12, minute=0, second=0, tzoff=0;
 
 	u_long yearstart=0;
-	u_int32 actual;
+	u_int32_t actual;
 
 	TEST_ASSERT_TRUE(clocktime(yday, hour, minute, second, tzoff, timestamp,
 						  &yearstart, &actual));
@@ -84,13 +72,13 @@ TEST(clocktime, TimeZoneOffset) {
 	 *
 	 * Time sent into function is 04:00:00 +0800
 	 */
-	const u_int32 timestamp = 3486369600UL;
-	const u_int32 expected	= timestamp;
+	const u_int32_t timestamp = 3486369600UL;
+	const u_int32_t expected	= timestamp;
 
 	const int yday=175, hour=4, minute=0, second=0, tzoff=8;
 
 	u_long yearstart=0;
-	u_int32 actual;
+	u_int32_t actual;
 
 	TEST_ASSERT_TRUE(clocktime(yday, hour, minute, second, tzoff, timestamp,
 						  &yearstart, &actual));
@@ -103,13 +91,13 @@ TEST(clocktime, WrongYearStart) {
 	 * Time sent into function is 11:00:00.
 	 * Yearstart sent into function is the yearstart of 2009!
 	 */
-	const u_int32 timestamp = 3471418800UL;
-	const u_int32 expected	= timestamp;
+	const u_int32_t timestamp = 3471418800UL;
+	const u_int32_t expected	= timestamp;
 
 	const int yday=2, hour=11, minute=0, second=0, tzoff=0;
 
 	u_long yearstart = 302024100UL; // Yearstart of 2009.
-	u_int32 actual;
+	u_int32_t actual;
 
 	TEST_ASSERT_TRUE(clocktime(yday, hour, minute, second, tzoff, timestamp,
 						  &yearstart, &actual));
@@ -122,13 +110,13 @@ TEST(clocktime, PreviousYear) {
 	 * Time sent into function is 23:00:00
 	 * (which is meant to be 2009-12-31 23:00:00Z)
 	 */
-	const u_int32 timestamp = 3471296400UL;
-	const u_int32 expected	= 3471289200UL;
+	const u_int32_t timestamp = 3471296400UL;
+	const u_int32_t expected	= 3471289200UL;
 
 	const int yday=365, hour=23, minute=0, second=0, tzoff=0;
 
 	u_long yearstart = 0;
-	u_int32 actual;
+	u_int32_t actual;
 
 	TEST_ASSERT_TRUE(clocktime(yday, hour, minute, second, tzoff, timestamp,
 						  &yearstart, &actual));
@@ -141,12 +129,12 @@ TEST(clocktime, NextYear) {
 	 * Time sent into function is 01:00:00
 	 * (which is meant to be 2010-01-01 01:00:00Z)
 	 */
-	const u_int32 timestamp = 3471289200UL;
-	const u_int32 expected	= 3471296400UL;
+	const u_int32_t timestamp = 3471289200UL;
+	const u_int32_t expected	= 3471296400UL;
 
 	const int yday=1, hour=1, minute=0, second=0, tzoff=0;
 	u_long yearstart = 0;
-	u_int32 actual;
+	u_int32_t actual;
 
 	TEST_ASSERT_TRUE(clocktime(yday, hour, minute, second, tzoff, timestamp,
 						  &yearstart, &actual));
@@ -155,11 +143,11 @@ TEST(clocktime, NextYear) {
 
 TEST(clocktime, NoReasonableConversion) {
 	/* Timestamp is: 2010-01-02 11:00:00Z */
-	const u_int32 timestamp = 3471418800UL;
+	const u_int32_t timestamp = 3471418800UL;
 
 	const int yday=100, hour=12, minute=0, second=0, tzoff=0;
 	u_long yearstart = 0;
-	u_int32 actual;
+	u_int32_t actual;
 
 	TEST_ASSERT_FALSE(clocktime(yday, hour, minute, second, tzoff, timestamp,
 						   &yearstart, &actual));
@@ -167,7 +155,7 @@ TEST(clocktime, NoReasonableConversion) {
 
 TEST(clocktime, AlwaysInLimit) {
 	/* Timestamp is: 2010-01-02 11:00:00Z */
-	const u_int32 timestamp = 3471418800UL;
+	const u_int32_t timestamp = 3471418800UL;
 	const u_short prime_incs[] = { 127, 151, 163, 179 };
 	int	cyc;
 	int	yday;
@@ -175,10 +163,9 @@ TEST(clocktime, AlwaysInLimit) {
 	u_short	ydayinc;
 	int	hour;
 	int	minute;
-	int	second;
 	u_long	yearstart;
-	u_int32	actual;
-	u_int32	diff;
+	u_int32_t	actual;
+	u_int32_t	diff;
 
 	yearstart = 0;
 	for (cyc = 0; cyc < 5; cyc++) {
@@ -191,9 +178,10 @@ TEST(clocktime, AlwaysInLimit) {
 					clocktime(yday, hour, minute, 30, 0,
 						  timestamp, &yearstart, &actual);
 					diff = actual - timestamp;
-					if (diff >= 0x80000000UL)
+					if (diff >= 0x80000000UL) {
 						diff = ~diff + 1;
-					TEST_ASSERT_LESS_THAN_OR_EQUAL(diff, (183u * SECSPERDAY));
+					}
+					TEST_ASSERT_TRUE(diff <= (183u * SECSPERDAY));
 				}
 			}
 		}
