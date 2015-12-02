6fe6aea4bd828b4077e32a38dd9ece6e4d941454
Amar Takhar <verm@darkbeer.org>
2015-12-02 10:32:00 -0500

    Convert libntp/netof.c.

--- tests/libntp/netof.c
+++ tests/libntp/netof.c
@@ -1,10 +1,8 @@
 #include "config.h"
 #include "ntp_stdlib.h"
 
-extern "C" {
 #include "unity.h"
 #include "unity_fixture.h"
-}
 
 TEST_GROUP(netof);
 
@@ -14,8 +12,6 @@ TEST_TEAR_DOWN(netof) {}
 
 #include "sockaddrtest.h"
 
-class netofTest : public sockaddrtest  {
-};
 
 TEST(netof, ClassBAddress) {
 	sockaddr_u input = CreateSockaddr4("172.16.2.1", NTP_PORT);
@@ -24,7 +20,7 @@ TEST(netof, ClassBAddress) {
 	sockaddr_u* actual = netof(&input);
 
 	TEST_ASSERT_TRUE(actual != NULL);
-	TEST_ASSERT_TRUE(IsEqual(expected, *actual));
+	TEST_ASSERT_TRUE(IsEqualS(&expected, actual));
 }
 
 TEST(netof, ClassCAddress) {
@@ -34,7 +30,7 @@ TEST(netof, ClassCAddress) {
 	sockaddr_u* actual = netof(&input);
 
 	TEST_ASSERT_TRUE(actual != NULL);
-	TEST_ASSERT_TRUE(IsEqual(expected, *actual));
+	TEST_ASSERT_TRUE(IsEqualS(&expected, actual));
 }
 
 TEST(netof, ClassAAddress) {
@@ -47,24 +43,24 @@ TEST(netof, ClassAAddress) {
 	sockaddr_u* actual = netof(&input);
 
 	TEST_ASSERT_TRUE(actual != NULL);
-	TEST_ASSERT_TRUE(IsEqual(expected, *actual));
+	TEST_ASSERT_TRUE(IsEqualS(&expected, actual));
 }
 
 TEST(netof, IPv6Address) {
 	/* IPv6 addresses are assumed to have 64-bit host- and 64-bit network parts. */
-	const struct in6_addr input_address = {
+	const struct in6_addr input_address = {{{
 		0x20, 0x01, 0x0d, 0xb8,
-        0x85, 0xa3, 0x08, 0xd3, 
+        0x85, 0xa3, 0x08, 0xd3,
         0x13, 0x19, 0x8a, 0x2e,
         0x03, 0x70, 0x73, 0x34
-	}; // 2001:0db8:85a3:08d3:1319:8a2e:0370:7334
+	}}}; // 2001:0db8:85a3:08d3:1319:8a2e:0370:7334
 
-	const struct in6_addr expected_address = {
+	const struct in6_addr expected_address = {{{
 		0x20, 0x01, 0x0d, 0xb8,
-        0x85, 0xa3, 0x08, 0xd3, 
+        0x85, 0xa3, 0x08, 0xd3,
         0x00, 0x00, 0x00, 0x00,
         0x00, 0x00, 0x00, 0x00
-	}; // 2001:0db8:85a3:08d3:0000:0000:0000:0000
+	}}}; // 2001:0db8:85a3:08d3:0000:0000:0000:0000
 
 	sockaddr_u input;
 	input.sa6.sin6_family = AF_INET6;
@@ -79,7 +75,7 @@ TEST(netof, IPv6Address) {
 	sockaddr_u* actual = netof(&input);
 
 	TEST_ASSERT_TRUE(actual != NULL);
-	TEST_ASSERT_TRUE(IsEqual(expected, *actual));
+	TEST_ASSERT_TRUE(IsEqualS(&expected, actual));
 }
 
 TEST_GROUP_RUNNER(netof) {
