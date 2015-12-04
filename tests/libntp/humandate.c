a26e3a0a7f93ebcd5878049162d8533a53a3cc08
Amar Takhar <verm@darkbeer.org>
2015-12-04 13:55:04 -0500

    Convert libntp/humandate.c

--- tests/libntp/humandate.c
+++ tests/libntp/humandate.c
@@ -1,10 +1,9 @@
 #include "config.h"
 #include "ntp_stdlib.h"
 
-extern "C" {
 #include "unity.h"
 #include "unity_fixture.h"
-}
+
 
 TEST_GROUP(humandate);
 
@@ -13,46 +12,36 @@ TEST_SETUP(humandate) {}
 TEST_TEAR_DOWN(humandate) {}
 
 
-#include <sstream>
-#include <string>
-
-class humandateTest : public libntptest {
-};
-
 TEST(humandate, RegularTime) {
 	time_t sample = 1276601278;
-	std::ostringstream expected;
-	struct tm;
+	char expected[255];
 
-	tm* time;
+	struct tm tmbuf;
+
+	struct tm* time;
 	time = localtime_r(&sample, &tmbuf);
 	TEST_ASSERT_TRUE(time != NULL);
 
-	expected << std::setfill('0')
-			 << std::setw(2) << time->tm_hour << ":"
-			 << std::setw(2) << time->tm_min << ":"
-			 << std::setw(2) << time->tm_sec;
+	snprintf(expected, 255, "%02d:%02d:%02d", time->tm_hour, time->tm_min, time->tm_sec);
 
-	TEST_ASSERT_EQUAL_STRING(expected.str().c_str(), humantime(sample));
+	TEST_ASSERT_EQUAL_STRING(expected, humantime(sample));
 }
 
 TEST(humandate, CurrentTime) {
 	time_t sample;
-	std::ostringstream expected;
-	struct tm;
+	char expected[255];
+
+	struct tm tmbuf;
 
 	time(&sample);
 
-	tm* time;
+	struct tm* time;
 	time = localtime_r(&sample, &tmbuf);
 	TEST_ASSERT_TRUE(time != NULL);
 
-	expected << std::setfill('0')
-			 << std::setw(2) << time->tm_hour << ":"
-			 << std::setw(2) << time->tm_min << ":"
-			 << std::setw(2) << time->tm_sec;
+	snprintf(expected, 255, "%02d:%02d:%02d", time->tm_hour, time->tm_min, time->tm_sec);
 
-	TEST_ASSERT_EQUAL_STRING(expected.str().c_str(), humantime(sample));
+	TEST_ASSERT_EQUAL_STRING(expected, humantime(sample));
 }
 
 TEST_GROUP_RUNNER(humandate) {
