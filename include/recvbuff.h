9a83182e2be7fa20d6d23ed6e8d42be5ea29c23d
Eric S. Raymond <esr@thyrsus.com>
2015-12-06 07:40:51 -0500

    Remove unused fields in the recvbuf structure.

--- include/recvbuff.h
+++ include/recvbuff.h
@@ -50,11 +50,9 @@ struct recvbuf {
 	recvbuf_t *	link;	/* next in list */
 	union {
 		sockaddr_u	X_recv_srcadr;
-		void *		X_recv_srcclock;
 		struct peer *	X_recv_peer;
 	} X_from_where;
 #define recv_srcadr		X_from_where.X_recv_srcadr
-#define	recv_srcclock		X_from_where.X_recv_srcclock
 #define recv_peer		X_from_where.X_recv_peer
 #ifndef HAVE_IO_COMPLETION_PORT
 	sockaddr_u	srcadr;		/* where packet came from */
@@ -63,7 +61,6 @@ struct recvbuf {
 #endif
 	endpt *		dstadr;		/* address pkt arrived on */
 	SOCKET		fd;		/* fd on which it was received */
-	int		msg_flags;	/* Flags received about the packet */
 	l_fp		recv_time;	/* time of arrival */
 	void		(*receiver)(struct recvbuf *); /* callback */
 	size_t		recv_length;	/* number of octets received */
