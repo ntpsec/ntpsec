8377be51b63975f1cff3c8928bb22140beb495e0
Eric S. Raymond <esr@thyrsus.com>
2015-12-07 07:40:47 -0500

    Record ucast/bcast/mcast flags in a packet's buffer when received...
    ...rather than computing them in ntp_monitor.c, in order to avoid having
    to keep data (the socket fd) that cannot be serialized into a replay file.
    
    The SOCKET fd field is now used only for purging fd-associated recvbufs (which
    will be bypassed in replay mode) and for diagnostic messages on the refclock
    side of things.

--- ntpd/ntp_io.c
+++ ntpd/ntp_io.c
@@ -3282,6 +3282,7 @@ read_refclock_packet(
 	rb->recv_length = buflen;
 	rb->recv_peer = rp->srcclock;
 	rb->dstadr = 0;
+	rb->cast_flags = 0;
 	rb->fd = fd;
 	rb->recv_time = ts;
 	rb->receiver = rp->clock_recv;
@@ -3540,6 +3541,9 @@ read_network_packet(
 	 * put it on the full list and do bookkeeping.
 	 */
 	rb->dstadr = itf;
+	rb->cast_flags = (uint8_t)(((rb->dstadr->flags &
+				     INT_MCASTOPEN) && rb->fd == rb->dstadr->fd) ? MDF_MCAST
+				   : rb->fd == rb->dstadr->bfd ? MDF_BCAST : MDF_UCAST);
 	rb->fd = fd;
 #ifdef USE_PACKET_TIMESTAMP
 	/* pick up a network time stamp if possible */
