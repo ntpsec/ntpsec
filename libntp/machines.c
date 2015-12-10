a945a4fecdc7b02187e5398509c7d26766f443b3
Eric S. Raymond <esr@thyrsus.com>
2015-12-10 08:59:38 -0500

    Port cleanup - move Mac shim for clock_gettime(2) to machines.c.
    No logic changes.

--- libntp/machines.c
+++ libntp/machines.c
@@ -26,6 +26,57 @@
 int _getch(void);	/* Declare the one function rather than include conio.h */
 #else
 
+/*
+ * Simulate ANSI/POSIX conformance on platforms that don't have it
+ */
+#ifndef HAVE_CLOCK_GETTIME
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#elif HAVE_GETCLOCK
+#include <sys/timers.h>
+#endif
+
+int clock_gettime(clockid_t clk_id, struct timespec *tp)
+{
+#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
+    clock_serv_t cclock;
+    mach_timespec_t mts;
+    int mode;
+    switch (clk_id) {
+    case CLOCK_REALTIME:
+	mode = CALENDAR_CLOCK;
+	break;
+    case CLOCK_MONOTONIC:
+	/* http://stackoverflow.com/questions/11680461/monotonic-clock-on-osx */
+	mode = SYSTEM_CLOCK;
+	break;
+    default:
+	return -1;
+    }
+    host_get_clock_service(mach_host_self(), mode, &cclock);
+    clock_get_time(cclock, &mts);
+    mach_port_deallocate(mach_task_self(), cclock);
+    tp->tv_sec = mts.tv_sec;
+    tp->tv_nsec = mts.tv_nsec;
+#elif HAVE_GETCLOCK
+    (void) getclock(TIMEOFDAY, &tp);
+#else
+#error Either POSIX clock_gettime(2) or Tru64/HP-UX getclock(2) is required
+/*
+ * Note: as a result of the refactoring of time handing, the support for
+ * compiling ntpdsim is currently broken.  It used to have an intercept point
+ * in unixtime.h, these definitions:
+   #define GETTIMEOFDAY(a, b) (node_gettime(&ntp_node, a))
+   #define SETTIMEOFDAY(a, b) (node_settime(&ntp_node, a))
+   #define ADJTIMEOFDAY(a, b) (node_adjtime(&ntp_node, a, b))
+ * To work again it will need one here. 
+ */
+#endif
+    return 0;
+}
+#endif /* HAVE_CLOCK_GETTIME */
+
 #if !defined(HAVE_NTP_GETTIME) && defined(HAVE_NTP_ADJTIME)
 int ntp_gettime(struct ntptimeval *ntv)
 {
