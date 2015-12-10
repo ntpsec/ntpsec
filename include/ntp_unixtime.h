4b306df6aeac4c0243720bcb7316bb28220967a1
Eric S. Raymond <esr@thyrsus.com>
2015-12-10 08:59:38 -0500

    Port cleanup - move Mac shim for clock_gettime(2) to machines.c.
    No logic changes.

--- include/ntp_unixtime.h
+++ include/ntp_unixtime.h
@@ -18,22 +18,15 @@
 #define CLOCK_REALTIME	0
 #define CLOCK_MONOTONIC	1
 typedef int clockid_t;
-#endif
-
-#ifndef HAVE_CLOCK_GETTIME
-/*
- * Pacify platforms that don't have a real clock_gettime(2),
- * notably Mac OS X.
- */
 int clock_gettime(clockid_t clock_id, struct timespec *tp);
 #endif
 
 /*
- * Time of day conversion constant.  Ntp's time scale starts in 1900,
+ * Time of day conversion constant.  NTP's time scale starts in 1900,
  * Unix in 1970.  The value is 1970 - 1900 in seconds, 0x83aa7e80 or
  * 2208988800.  This is larger than 32-bit INT_MAX, so unsigned
  * type is forced.
  */
 #define	JAN_1970 ((u_int)NTP_TO_UNIX_DAYS * (u_int)SECSPERDAY)
 
-#endif /* GUARD_!defined(NTP_UNIXTIME_H) */
+#endif /* GUARD_NTP_UNIXTIME_H */
