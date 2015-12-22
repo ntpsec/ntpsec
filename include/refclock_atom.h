2807ed51ec562bed4bee2034b9d12143cb251914
Hal Murray <hmurray@megapathdsl.net>
2015-12-21 18:31:18 -0800

    Add clockstats line to ATOM/PPS driver

--- include/refclock_atom.h
+++ include/refclock_atom.h
@@ -1,15 +1,22 @@
 /*
  * Definitions for the atom driver and its friends
  */
-#undef NANOSECOND	/* some systems define it differently */
-#define NANOSECOND	1000000000 /* one second (ns) */
 
 struct refclock_atom {
 	pps_handle_t handle;
 	pps_params_t pps_params;
 	struct timespec ts;
+	unsigned long sequence;
 };
+
+typedef enum {
+  PPS_OK,
+  PPS_SETUP,  /* PPS not setup */
+  PPS_KERNEL, /* PPS error from kernel */
+  PPS_NREADY  /* PPS not ready */
+} pps_status;
 
 extern	bool	refclock_ppsapi(int, struct refclock_atom *);
 extern	bool	refclock_params(int, struct refclock_atom *);
-extern	bool	refclock_pps(struct peer *, struct refclock_atom *, int);
+extern  pps_status refclock_pps(struct peer *, struct refclock_atom *, int);
+
