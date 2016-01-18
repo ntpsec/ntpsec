9bd05b082c87b79a157c2382955302e9f8c70a27
Daniel Fox Franke <dfoxfranke@gmail.com>
2016-01-17 19:22:39 -0500

    CVE-2015-7975: Add missing length check to nextvar()

--- ntpq/ntpq.c
+++ ntpq/ntpq.c
@@ -2861,6 +2861,8 @@ nextvar(
 	len = srclen;
 	while (len > 0 && isspace((unsigned char)cp[len - 1]))
 		len--;
+        if(len >= sizeof name)
+                return 0;
 	if (len > 0)
 		memcpy(name, cp, len);
 	name[len] = '\0';
