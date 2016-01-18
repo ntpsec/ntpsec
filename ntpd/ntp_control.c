e31cdda32bc52aff047b4bd7e9b885aa56b51e15
Daniel Fox Franke <dfoxfranke@gmail.com>
2016-01-17 19:23:58 -0500

    CVE-2015-8139: Don't allow rv of expected origin timestamp

--- ntpd/ntp_control.c
+++ ntpd/ntp_control.c
@@ -251,7 +251,7 @@ static const struct ctl_proc control_codes[] = {
 #define	CP_ROOTDISPERSION	15
 #define	CP_REFID		16
 #define	CP_REFTIME		17
-#define	CP_ORG			18
+/* #define	CP_ORG			18 */
 #define	CP_REC			19
 #define	CP_XMT			20
 #define	CP_REACH		21
@@ -491,7 +491,9 @@ static const struct ctl_var peer_var[] = {
 	{ CP_ROOTDISPERSION, RO, "rootdisp" },	/* 15 */
 	{ CP_REFID,	RO, "refid" },		/* 16 */
 	{ CP_REFTIME,	RO, "reftime" },	/* 17 */
-	{ CP_ORG,	RO, "org" },		/* 18 */
+        /* This variable is disabled because leaking it creates a
+           vulnerability */
+        /*	{ CP_ORG,	RO, "org" }, */	/* 18 */
 	{ CP_REC,	RO, "rec" },		/* 19 */
 	{ CP_XMT,	RO, "xleave" },		/* 20 */
 	{ CP_REACH,	RO, "reach" },		/* 21 */
@@ -2445,10 +2447,6 @@ ctl_putpeer(
 
 	case CP_REFTIME:
 		ctl_putts(peer_var[id].text, &p->reftime);
-		break;
-
-	case CP_ORG:
-		ctl_putts(peer_var[id].text, &p->aorg);
 		break;
 
 	case CP_REC:
