b9ca7849da913a9dd4a2c0b70781154cc89d60b9
Amar Takhar <verm@darkbeer.org>
2016-04-19 23:48:15 -0400

    Fix missing string variable.

--- pylib/tool.py
+++ pylib/tool.py
@@ -4,8 +4,8 @@ def check_sanity(ctx, cond, lib):
 	define = "HAVE_%s" % lib.upper()
 
 	if cond and (not ctx.get_define(define)):
-		pprint("RED", "Warning %s headers detected, binaries do not build/run")
+		pprint("RED", "Warning %s headers detected, binaries do not build/run" % lib)
 	elif (not cond) and ctx.get_define(define):
-		pprint("RED", "Warning %s headers not detected, binaries build/run")
+		pprint("RED", "Warning %s headers not detected, binaries build/run" % lib)
 
 
