6d765ece38bce78b85eb58fb9113bd3f98ec92ba
Eric S. Raymond <esr@thyrsus.com>
2016-05-12 12:57:42 -0400

    Fix fatal typos in revised ntpdig argument processing.

--- ntpdig/main.c
+++ ntpdig/main.c
@@ -216,7 +216,7 @@ ntpdig_main (
 #endif
 		break;
 	    case 'g':
-		opt_gap = atoi(optarg);
+		opt_gap = atoi(ntp_optarg);
 		if (opt_gap <= 0) {
 			printf("%s: invalid gap value!\n", progname);
 			exit(1);
@@ -236,7 +236,7 @@ ntpdig_main (
 		opt_logfile = ntp_optarg;
 		break;
 	    case 'M':
-		opt_ntpversion = atoi(optarg);
+		opt_ntpversion = atoi(ntp_optarg);
 		if (opt_steplimit <= 0) {
 			printf("%s: invalid steplimit value!\n", progname);
 			exit(1);
