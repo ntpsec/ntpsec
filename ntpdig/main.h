67fb54dddf5e6b40423bd15dbc7ba59a1c9b1ab7
Eric S. Raymond <esr@thyrsus.com>
2016-07-02 08:31:21 -0400

    Remove a now-unused prototype.

--- ntpdig/main.h
+++ ntpdig/main.h
@@ -8,7 +8,6 @@
 #include <ntp.h>
 #include <ntp_unixtime.h>
 #include <ntp_stdlib.h>
-#include <ntp_intres.h>
 #include <ntp_debug.h>
 #include <timevalops.h>
 
@@ -23,7 +22,6 @@ int	handle_pkt(int rpktl, struct pkt *rpkt, sockaddr_u *host,
 void	offset_calculation(struct pkt *rpkt, int rpktl,
 			   struct timeval *tv_dst, double *offset,
 			   double *precision, double *root_dispersion);
-int	on_wire(struct addrinfo *host, struct addrinfo *bcastaddr);
 int	set_time(double offset);
 
 #endif /* GUARD_MAIN_H */
