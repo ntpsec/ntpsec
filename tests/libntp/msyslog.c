45949dcb265672ac5074f33c1507c77f6c1e2e72
Matt Selsky <matthew.selsky@twosigma.com>
2016-07-23 15:38:08 -0400

    Fix warnings on OpenBSD 5.8 related to gcc pragma diagnostics
    gcc 4.6 added support for push/pop of diagnostics per
    https://gcc.gnu.org/gcc-4.6/changes.html
    
    Resolves:
    ../../tests/libntp/msyslog.c:142: warning: expected [error|warning|ignored] after '#pragma GCC diagnostic'
    ../../tests/libntp/msyslog.c:171: warning: expected [error|warning|ignored] after '#pragma GCC diagnostic'

--- tests/libntp/msyslog.c
+++ tests/libntp/msyslog.c
@@ -129,16 +129,15 @@ TEST(msyslog, msnprintfTruncate)
 /* NetBSD 7.0, gcc 4.8.4 is OK.
  * FreeBSD 10.0 uses clang
  * NetBSD 6.1.5, gcc 4.5.3 doesn't support pragma inside procedures.
- * NetBSD 6.1.5, gcc 4.5.3 doesn't support push/pop.
  * FreeBSD 9.3, gcc 4.2.1 doesn't support pragma inside procedures.
- * FreeBSD 9.3, gcc 4.2.1 doesn't support push/pop.
  * FreeBSD 9.3, gcc 4.2.1 doesn't support ignoring -Wformat-contains-nul.
+ * gcc 4.6 added support for push/pop
  * Put this test last since we can't undo turning off some warnings. */
 #ifdef __clang__
 #  pragma clang diagnostic push
 #  pragma clang diagnostic ignored "-Wformat"
 #else /* GCC */
-#if ! defined(__NetBSD__) && ! defined(__FreeBSD__)
+#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)
 # pragma GCC diagnostic push
 #endif
 #ifndef __FreeBSD__
@@ -167,7 +166,7 @@ TEST(msyslog, msnprintfHangingPercent)
 #ifdef __clang__
 #  pragma clang diagnostic pop
 #else
-#if ! defined(__NetBSD__) && ! defined(__FreeBSD__)
+#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)
 # pragma GCC diagnostic pop
 #endif
 #endif
