ce3e268e5b163bf41cc5c3c2323c75b90d56d3e5
Christian Ehrhardt <christian.ehrhardt@canonical.com>
2016-08-09 12:51:25 +0200

    snap: provide and consume config in user accessible place
    To make the config more than just a default it has to be made available
    in the $SNAP_DATA directory. This is a system config, so it didn't
    qualify for $SNAP_USER_DATA.
    
    Then on the other hand ntpd has to be made to consume that configuration
    and at the same time avoid https://gitlab.com/NTPsec/ntpsec/issues/98.
    
    Signed-off-by: Christian Ehrhardt <christian.ehrhardt@canonical.com>

---
+++ etc/snap/ntpd.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if [ ! -e "$SNAP_DATA/etc/ntpd.conf" ]; then
+    echo "make packaged default config user accessible in $SNAP_DATA"
+    mkdir -p "$SNAP_DATA/etc/"
+    cp -v $SNAP/etc/* "$SNAP_DATA/etc/"
+fi
+
+# avoid failing to include config files
+# (https://gitlab.com/NTPsec/ntpsec/issues/98)
+cd "$SNAP_DATA/etc/"
+
+exec "$SNAP/usr/local/sbin/ntpd" "$@"
