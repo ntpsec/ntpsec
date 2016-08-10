40dc209422e47942ecabe5db1093e1b8088732b6
Eric S. Raymond <esr@thyrsus.com>
2016-08-10 07:46:30 -0400

    Address GitLab issue #92: ntpkeygen documentation needs to be rewritten.

--- ntpkeygen/ntpkeygen.c
+++ ntpkeygen/ntpkeygen.c
@@ -1,77 +1,16 @@
 /*
  * Program to generate cryptographic keys for ntp clients and servers
  *
- * This program generates password encrypted data files for use with
- * the Network Time Protocol Version 4. Files are prefixed with a
- * header giving the name and date of creation followed by a
- * type-specific descriptive label and PEM-encoded data structure
- * compatible with programs of the OpenSSL library.
- *
  * All file names are like "ntpkey_<type>_<hostname>.<filestamp>", where
  * <type> is the file type, <hostname> the generating host name and
  * <filestamp> the generation time in NTP seconds. The NTP programs
  * expect generic names such as "ntpkey_<type>_whimsy.udel.edu" with the
  * association maintained by soft links. Following is a list of file
- * types; the first line is the file name and the second link name.
+ * types.
  *
  * ntpkey_MD5key_<hostname>.<filestamp>
  * 	MD5 (128-bit) keys used to compute message digests in symmetric
  *	key cryptography
- *
- * ntpkey_RSAhost_<hostname>.<filestamp>
- * ntpkey_host_<hostname>
- *	RSA private/public host key pair used for public key signatures
- *
- * ntpkey_RSAsign_<hostname>.<filestamp>
- * ntpkey_sign_<hostname>
- *	RSA private/public sign key pair used for public key signatures
- *
- * ntpkey_DSAsign_<hostname>.<filestamp>
- * ntpkey_sign_<hostname>
- *	DSA Private/public sign key pair used for public key signatures
- *
- * Available digest/signature schemes
- *
- * RSA:	RSA-MD2, RSA-MD5, RSA-SHA, RSA-SHA1, RSA-MDC2, EVP-RIPEMD160
- * DSA:	DSA-SHA, DSA-SHA1
- *
- * ntpkey_XXXcert_<hostname>.<filestamp>
- * ntpkey_cert_<hostname>
- *	X509v3 certificate using RSA or DSA public keys and signatures.
- *	XXX is a code identifying the message digest and signature
- *	encryption algorithm
- *
- * Identity schemes. The key type par is used for the challenge; the key
- * type key is used for the response.
- *
- * ntpkey_IFFkey_<groupname>.<filestamp>
- * ntpkey_iffkey_<groupname>
- *	Schnorr (IFF) identity parameters and keys
- *
- * ntpkey_GQkey_<groupname>.<filestamp>,
- * ntpkey_gqkey_<groupname>
- *	Guillou-Quisquater (GQ) identity parameters and keys
- *
- * ntpkey_MVkeyX_<groupname>.<filestamp>,
- * ntpkey_mvkey_<groupname>
- *	Mu-Varadharajan (MV) identity parameters and keys
- *
- * Note: Once in a while because of some statistical fluke this program
- * fails to generate and verify some cryptographic data, as indicated by
- * exit status -1. In this case simply run the program again. If the
- * program does complete with exit code 0, the data are correct as
- * verified.
- *
- * These cryptographic routines are characterized by the prime modulus
- * size in bits. The default value of 512 bits is a compromise between
- * cryptographic strength and computing time and is ordinarily
- * considered adequate for this application. The routines have been
- * tested with sizes of 256, 512, 1024 and 2048 bits. Not all message
- * digest and signature encryption schemes work with sizes less than 512
- * bits. The computing time for sizes greater than 2048 bits is
- * prohibitive on all but the fastest processors. An UltraSPARC Blade
- * 1000 took something over nine minutes to generate and verify the
- * values with size 2048. An old SPARC IPC would take a week.
  *
  * The OpenSSL library used by this program expects a random seed file.
  * As described in the OpenSSL documentation, the file name defaults to
@@ -106,7 +45,6 @@
 #include <sodium.h>
 #include <ssl_applink.c>
 
-#define _UC(str)	((char *)(intptr_t)(str))
 /*
  * Cryptodefines
  */
@@ -125,17 +63,11 @@ void	followlink	(char *, size_t);
  */
 extern char *optarg;		/* command line argument */
 char	*progname;
-u_int	lifetime = DAYSPERYEAR;	/* certificate lifetime (days) */
-int	nkeys;			/* MV keys */
 time_t	epoch;			/* Unix epoch (seconds) since 1970 */
 u_int	fstamp;			/* NTP filestamp */
 char	hostbuf[MAXHOSTNAME + 1];
 char	*hostname = NULL;	/* host, used in cert filenames */
 char	*groupname = NULL;	/* group name */
-char	certnamebuf[2 * sizeof(hostbuf)];
-char	*certname = NULL;	/* certificate subject/issuer name */
-char	*passwd1 = NULL;	/* input private key password */
-char	*passwd2 = NULL;	/* output private key password */
 char	filename[MAXFILENAME + 1]; /* file name */
 
 #ifdef SYS_WINNT
@@ -243,20 +175,11 @@ followlink(
 	fname[len] = '\0';
 }
 
-#define ALL_OPTIONS "b:c:dD:e:G:HI:i:l:Mm:P:p:q:S:s:T:V:x:"
+#define ALL_OPTIONS "M"
 static const struct option longoptions[] = {
-    { "imbits",		    1, 0, 'i' },
-    { "cipher",		    1, 0, 'C' },
-    { "debug",		    0, 0, 'd' },
-    { "set-debug-level",    1, 0, 'D' },
     { "md5key",		    0, 0, 'M' },
-    { "modulus",	    1, 0, 'm' },
     { NULL,                 0, 0, '\0'},
 };
-
-static char *opt_imbits = NULL;
-static bool opt_md5key = false;
-static int opt_modulus = -1;
 
 /*
  * Main program
@@ -268,7 +191,6 @@ main(
 	)
 {
 	struct timespec ts;	/* initialization vector */
-	int	md5key = 0;	/* generate MD5 keys */
 	int     op;
 
 	progname = argv[0];
@@ -298,8 +220,6 @@ main(
 	hostbuf[COUNTOF(hostbuf) - 1] = '\0';
 	hostname = hostbuf;
 	groupname = hostbuf;
-	passwd1 = hostbuf;
-	passwd2 = NULL;
 	clock_gettime(CLOCK_REALTIME, &ts);
 	epoch = ts.tv_sec;
 	fstamp = (u_int)(epoch + JAN_1970);
@@ -308,25 +228,12 @@ main(
 				     ALL_OPTIONS, longoptions, NULL)) != -1) {
 
 	    switch (op) {
-	    case 'b':
-		opt_imbits = ntp_optarg;
-		break;
-	    case 'd':
-#ifdef DEBUG
-		++debug;
-#endif
-		break;
-	    case 'D':
-#ifdef DEBUG
-		debug = atoi(ntp_optarg);
-#endif
-		break;
 	    case 'M':
-		opt_md5key = true;
+		/* dummy MD5 option for backwards compatibility */
 		break;
-	    case 'm':
-		opt_modulus = atoi(ntp_optarg);
-		break;
+	    default:
+		fprintf(stderr, "usage: ntpkeygen [-M]" );
+		exit(1);
 	    }
 	}
 		
@@ -342,17 +249,7 @@ main(
 			OPENSSL_VERSION_TEXT, SSLeay_version(SSLEAY_VERSION));
 #endif /* HAVE_OPENSSL */
 
-	if (opt_md5key)
-		md5key++;
-
-	/*
-	 * Create new unencrypted MD5 keys file if requested. If this
-	 * option is selected, ignore all other options.
-	 */
-	if (md5key) {
-		gen_md5("md5");
-		exit (0);
-	}
+	gen_md5("md5");
 
 	exit(0);
 }
@@ -439,12 +336,7 @@ fheader	(
 		exit (-1);
 	}
 	/* NAMECHANGE */
-        if (strcmp(ulink, "md5") == 0) {
-		strlcpy(linkname,"ntp.keys",sizeof(linkname));
-        } else {
-          snprintf(linkname, sizeof(linkname), "ntpkey_%s_%s", ulink,
-                   hostname);
-        }
+	strlcpy(linkname,"ntp.keys",sizeof(linkname));
 	(void)remove(linkname);		/* The symlink() line below matters */
 	temp = symlink(filename, linkname);
 	if (temp < 0)
