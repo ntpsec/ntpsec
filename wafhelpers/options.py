2e75cb0dba307c9c03b3f92c49014f15318de77b
Matt Selsky <matthew.selsky@twosigma.com>
2016-08-24 09:33:26 -0400

    Typo

--- wafhelpers/options.py
+++ wafhelpers/options.py
@@ -18,7 +18,7 @@ def options_cmd(ctx, config):
 	grp.add_option('--disable-dns-retry', action='store_true', default=False, help="Disable retrying DNS lookups.")
 	grp.add_option('--disable-mdns-registration', action='store_true', default=False, help="Disable MDNS registration.")
 	grp.add_option('--enable-classic-mode', action='store_true', default=False, help="Strict configuration and log-format compatibility with NTP Classic")
-	grp.add_option('--enable-debug-timing', action='store_true', default=False, help="Cillect timing statistics for debugging.")
+	grp.add_option('--enable-debug-timing', action='store_true', default=False, help="Collect timing statistics for debugging.")
 	grp.add_option('--disable-lineeditlibs', action='store_true', default=False, help="Disable line editing support")
 
 	grp = ctx.add_option_group("NTP cross compile options")
