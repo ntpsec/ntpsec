2611e7e55b50d14836bcfe05788ce7ec5d5aacce
Christian Ehrhardt <christian.ehrhardt@canonical.com>
2016-08-25 14:18:50 +0200

    snapcraft: set default driftfile path to a snap writable dir
    The default dir for the driftfile isn't a writable one inside the snap.
    The wrapper providing the initial config sets a path based on whatever
    the current SNAP_DATA dir is to have a valid location.
    
    Signed-off-by: Christian Ehrhardt <christian.ehrhardt@canonical.com>

--- etc/snap/ntpd.sh
+++ etc/snap/ntpd.sh
@@ -3,6 +3,9 @@ if [ ! -e "$SNAP_DATA/etc/ntpd.conf" ]; then
     echo "make packaged default config user accessible in $SNAP_DATA"
     mkdir -p "$SNAP_DATA/etc/"
     cp -v $SNAP/etc/* "$SNAP_DATA/etc/"
+
+    # set default driftfile path to a snap writable dir
+    echo "driftfile $SNAP_DATA/ntp.drift" > "$SNAP_DATA/etc/use-minimal-logging"
 fi
 
 # avoid failing to include config files
