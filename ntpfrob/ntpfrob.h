caf4cb47fb91bed64409eea3783e3fb45be0dcfc
Eric S. Raymond <esr@thyrsus.com>
2016-09-09 08:47:52 -0400

    Fix argument-passing both in ntpfrob.

--- ntpfrob/ntpfrob.h
+++ ntpfrob/ntpfrob.h
@@ -4,7 +4,7 @@
 typedef enum {plain_text, raw, json} iomode;
 
 extern void ppscheck(char *device);
-extern void tickadj(const iomode mode, const int tick);
+extern void tickadj(const bool mode, const int tick);
 extern void bumpclock(int);
 extern void jitter(const iomode mode);
 extern void stepback(void);
