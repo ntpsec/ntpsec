7e137a7fd457089f24770068eb46adbd57ef0e52
Gary E. Miller <gem@rellim.com>
2016-09-10 18:33:13 -0700

    Convert tabs to spaces.
    The indent space is not PEP 8, but at least it runs on Python 3.
    
    PEP 8 says indent levels s/b 4 spaces.
    
    Somone forgot that tabs (8) is not shiftwidth (4)

--- wafhelpers/probes.py
+++ wafhelpers/probes.py
@@ -4,56 +4,56 @@ up the logic in the main configure.py.
 """
 
 def probe_header_with_prerequisites(ctx, header, prerequisites, use=None):
-	"Check that a header (with its prerequisites) compiles."
-	src = ""
+        "Check that a header (with its prerequisites) compiles."
+        src = ""
         for hdr in prerequisites + [header]:
-        	src += "#include <%s>\n" % hdr
+                src += "#include <%s>\n" % hdr
         src += "int main() { return 0; }\n"
-	have_name = "HAVE_%s" % header.replace(".","_").replace("/","_").upper()
-	ctx.check_cc(
-		fragment=src,
-		define_name=have_name,
-		msg = "Checking for header %s" % header,
-		use = use or [],
-		mandatory = False,
-		comment = "<%s> header" % header)
-	return ctx.get_define(have_name)
+        have_name = "HAVE_%s" % header.replace(".","_").replace("/","_").upper()
+        ctx.check_cc(
+                fragment=src,
+                define_name=have_name,
+                msg = "Checking for header %s" % header,
+                use = use or [],
+                mandatory = False,
+                comment = "<%s> header" % header)
+        return ctx.get_define(have_name)
 
 def probe_function_with_prerequisites(ctx, function, prerequisites, use=None):
-	"Check that a function (with its prerequisites) compiles."
-	src = ""
+        "Check that a function (with its prerequisites) compiles."
+        src = ""
         for hdr in prerequisites:
-        	src += "#include <%s>\n" % hdr
+                src += "#include <%s>\n" % hdr
         src += """int main() {
-	void *p = (void*)(%s);
-	return (int)p;
+        void *p = (void*)(%s);
+        return (int)p;
 }
 """ % function
-	have_name = "HAVE_%s" % function.upper()
-	ctx.check_cc(
-		fragment=src,
-		define_name=have_name,
-		msg = "Checking for function %s" % function,
-		use = use or [],
-		mandatory = False,
-		comment = "Whether %s() exists" % function)
-	return ctx.get_define(have_name)
+        have_name = "HAVE_%s" % function.upper()
+        ctx.check_cc(
+                fragment=src,
+                define_name=have_name,
+                msg = "Checking for function %s" % function,
+                use = use or [],
+                mandatory = False,
+                comment = "Whether %s() exists" % function)
+        return ctx.get_define(have_name)
 
 def probe_multicast(ctx, symbol, legend):
-	"Probe for IP multicast capability."
-	ctx.check_cc(
-		fragment="""
+        "Probe for IP multicast capability."
+        ctx.check_cc(
+                fragment="""
 #include <netinet/in.h>
 int main() {
-	struct ip_mreq ipmr;
-	ipmr.imr_interface.s_addr = 0;
-	return 0;
+        struct ip_mreq ipmr;
+        ipmr.imr_interface.s_addr = 0;
+        return 0;
 }
 """,
-		define_name=symbol,
-		msg = legend,
-		mandatory = False,
-		comment = "IP multicast capability")
+                define_name=symbol,
+                msg = legend,
+                mandatory = False,
+                comment = "IP multicast capability")
 
 # What we really want to do here is test to see if the following program
 # compiles *and exits with 9 status*.  Because we don't know how to check
@@ -66,35 +66,35 @@ int main() {
 
 int call_vsnprintf(char *dst, size_t sz, const char *fmt,...)
 {
-	va_list	ap;
-	int		rc;
+        va_list ap;
+        int             rc;
 
-	va_start(ap, fmt);
-	rc = vsnprintf(dst, sz, fmt, ap);
-	va_end(ap);
+        va_start(ap, fmt);
+        rc = vsnprintf(dst, sz, fmt, ap);
+        va_end(ap);
 
-	return rc;
+        return rc;
 }
 
 int main()
 {
-	char	sbuf[512];
-	char	pbuf[512];
-	int		slen;
+        char    sbuf[512];
+        char    pbuf[512];
+        int             slen;
 
-	strcpy(sbuf, strerror(ENOENT));
-	errno = ENOENT;
-	slen = call_vsnprintf(pbuf, sizeof(pbuf), "%m",
-			  "wrong");
-	return strcmp(sbuf, pbuf);
+        strcpy(sbuf, strerror(ENOENT));
+        errno = ENOENT;
+        slen = call_vsnprintf(pbuf, sizeof(pbuf), "%m",
+                          "wrong");
+        return strcmp(sbuf, pbuf);
 }
 '''
 
 
 def probe_vsprintfm(ctx, symbol, legend):
-	"Probe for %m expanding to strerror(error) in glibc style."
-	ctx.check_cc(
-		fragment='''
+        "Probe for %m expanding to strerror(error) in glibc style."
+        ctx.check_cc(
+                fragment='''
 #include <features.h>
 int main()
 {
@@ -103,7 +103,7 @@ int main()
 #endif
 }
 ''',
-		define_name=symbol,
-		msg = legend,
-		mandatory = False,
-		comment="%m expanding to strerror(error) in glibc style")
+                define_name=symbol,
+                msg = legend,
+                mandatory = False,
+                comment="%m expanding to strerror(error) in glibc style")
