4f505dc541e94e92989533a2aab6394e9a3994dd
Matt Selsky <matthew.selsky@twosigma.com>
2016-09-11 00:11:16 -0400

    Fix PEP8 E703 statement ends with a semicolon

--- wafhelpers/check_seccomp.py
+++ wafhelpers/check_seccomp.py
@@ -6,7 +6,7 @@ def check_seccomp(ctx):
     if  not ctx.options.enable_seccomp:
         return
     if not sys.platform.startswith("linux"):
-        return;
+        return
 
 
     ctx.check_cc(header_name="seccomp.h", mandatory=False)
