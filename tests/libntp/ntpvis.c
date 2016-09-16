5381fca5c197f6a57301b0642b4e7bfe6980c731
Eric S. Raymond <esr@thyrsus.com>
2016-09-16 08:09:32 -0400

    TESTFRAME: Verified round-tripping of packet dumps.

--- tests/libntp/ntpvis.c
+++ tests/libntp/ntpvis.c
@@ -64,7 +64,29 @@ TEST(ntpvis, PacketDump2) {
     TEST_ASSERT_EQUAL_STRING(ExampleDump2, buf);
 }
 
+TEST(ntpvis, Roundtrip1) {
+    char buf1[BUFSIZ], buf2[BUFSIZ], bin[BUFSIZ];
+    size_t binlen;
+
+    packet_dump(buf1, sizeof(buf1), &ExamplePacket1, LEN_PKT_NOMAC);
+    binlen = packet_undump(bin, sizeof(bin), buf1);
+    packet_dump(buf2, sizeof(buf2), (struct pkt *)bin, binlen);
+    TEST_ASSERT_EQUAL_STRING(buf1, buf2);
+}
+
+TEST(ntpvis, Roundtrip2) {
+    char buf1[BUFSIZ], buf2[BUFSIZ], bin[BUFSIZ];
+    size_t binlen;
+
+    packet_dump(buf1, sizeof(buf1), &ExamplePacket2, LEN_PKT_NOMAC);
+    binlen = packet_undump(bin, sizeof(bin), buf1);
+    packet_dump(buf2, sizeof(buf2), (struct pkt *)bin, binlen);
+    TEST_ASSERT_EQUAL_STRING(buf1, buf2);
+}
+
 TEST_GROUP_RUNNER(ntpvis) {
     RUN_TEST_CASE(ntpvis, PacketDump1);
     RUN_TEST_CASE(ntpvis, PacketDump2);
+    RUN_TEST_CASE(ntpvis, Roundtrip1);
+    RUN_TEST_CASE(ntpvis, Roundtrip2);
 }
