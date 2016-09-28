49c5562f04861378a0275623f472d65eefb2bb19
Eric S. Raymond <esr@thyrsus.com>
2016-09-28 01:09:31 -0400

    Delete fallback to Tru64/HP-UX getclock(2).
    All our target systems should have POSIX clock_gettime() or the
    deprecated POSIX gettimeofday().

--- libntp/machines.c
+++ libntp/machines.c
@@ -29,8 +29,6 @@
 #ifdef __MACH__
 #include <mach/clock.h>
 #include <mach/mach.h>
-#elif HAVE_GETCLOCK
-#include <sys/timers.h>
 #endif
 
 int clock_gettime(clockid_t clk_id, struct timespec *tp)
@@ -55,10 +53,7 @@ int clock_gettime(clockid_t clk_id, struct timespec *tp)
     mach_port_deallocate(mach_task_self(), cclock);
     tp->tv_sec = mts.tv_sec;
     tp->tv_nsec = mts.tv_nsec;
-#elif HAVE_GETCLOCK
-    (void) getclock(TIMEOFDAY, &tp);
-#else
-#error Either POSIX clock_gettime(2) or Tru64/HP-UX getclock(2) is required
+#error POSIX clock_gettime(2) is required
 #endif
     return 0;
 }
--- wafhelpers/configure.py
+++ wafhelpers/configure.py
@@ -291,7 +291,6 @@ def cmd_configure(ctx, config):
                 ('clock_gettime', ["time.h"], "RT"),
                 ('clock_settime', ["time.h"], "RT"),
                 ('EVP_MD_do_all_sorted', ["openssl/evp.h"], "CRYPTO"),
-                ('getclock', ["sys/timers.h"]),
 		('getdtablesize', ["unistd.h"]),
                 ('getpassphrase', ["stdlib.h"]),                # Sun systems
                 ('getrusage', ["sys/time.h", "sys/resource.h"]),
