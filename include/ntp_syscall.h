838227e1abf76a98fcdec8721b27c9be7991134c
Eric S. Raymond <esr@thyrsus.com>
2016-10-11 16:20:05 -0400

    Small refactoring step - hide STA_NANO conditionalization.

--- include/ntp_syscall.h
+++ include/ntp_syscall.h
@@ -27,6 +27,20 @@ struct ntptimeval
 int ntp_gettime(struct ntptimeval *);
 #endif	/* !HAVE_STRUCT_NTPTIMEVAL */
 
+/*
+ * The units of the maxerror and esterror fields vary by platform.  If
+ * STA_NANO is defined, they're in nanoseconds;, otherwise in
+ * microseconds. Hide the difference by normalizing everything to
+ * float seconds.
+ */
+# ifdef STA_NANO
+#define ntp_error_in_seconds(n)	((n)/1.0e9)
+# else
+#define ntp_error_in_seconds(n)	((n)/1.0e6)
+# endif
+
+
+
 /* MUSL port shim */
 #if !defined(HAVE_NTP_ADJTIME) && defined(HAVE_ADJTIMEX)
 #define ntp_adjtime adjtimex
