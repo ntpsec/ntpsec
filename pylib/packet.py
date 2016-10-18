088f898c1b509edd0fbd60989a24ba1810f8d257
Matt Selsky <matthew.selsky@twosigma.com>
2016-10-18 00:44:59 -0400

    PEP8: E231 missing whitespace after ','

--- pylib/packet.py
+++ pylib/packet.py
@@ -64,7 +64,7 @@ class Packet:
     def VN_MODE(v, m):          return ((((v) & 7) << 3) | ((m) & 0x7))
 
     @staticmethod
-    def PKT_LI_VN_MODE(l,v,m):  return ((((l) & 3) << 6) | Packet.VN_MODE((v), (m)))
+    def PKT_LI_VN_MODE(l, v, m):  return ((((l) & 3) << 6) | Packet.VN_MODE((v), (m)))
 
     def __init__(self, session, version, mode):
         self.session = session  # Where to get session context
@@ -509,7 +509,7 @@ class Mode6Session:
                         sys.stderr.write("***Packet error %d" % res)
                     else:
                         sys.stderr.write(res.format(associd))
-                if not done and res in (SERR_TIMEOUT,SERR_INCOMPLETE):
+                if not done and res in (SERR_TIMEOUT, SERR_INCOMPLETE):
                     done = True
                     continue
             break
