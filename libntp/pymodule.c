e65a34e96291ed36edc2d3daa7c62d533bd44ed9
Eric S. Raymond <esr@thyrsus.com>
2016-10-20 08:28:51 -0400

    Create a C extension to make libntp functions available to Python.
    Initially only has one entry point, statustoa(), tested in pyntpq.

---
+++ libntp/pymodule.c
@@ -0,0 +1,75 @@
+/*
+ * This file is Copyright (c) 2010 by the GPSD project
+ * BSD terms apply: see the file COPYING in the distribution root for details.
+ *
+ * Python binding for selected libntp library functions
+ */
+#include <Python.h>
+
+#include <config.h>
+
+#include "ntp_machine.h"
+#include "ntpd.h"
+#include "ntp_io.h"
+#include "ntp_stdlib.h"
+#include "ntp_random.h"
+
+#include "ntp_config.h"
+#include "ntp_assert.h"
+#include "isc/error.h"
+#include "isc/formatcheck.h"
+
+#include "ntp_control.h"
+
+#include "python_compatibility.h"
+
+/*
+ * Client utility functions
+ */
+
+static PyObject *
+libntpc_statustoa(PyObject *self, PyObject *args)
+{
+    int status1, status2;
+    char *gs;
+
+    UNUSED_ARG(self);
+    if (!PyArg_ParseTuple(args, "ii", &status1, &status2))
+	return NULL;
+    gs = statustoa(status1, status2);
+    return Py_BuildValue("s", gs);
+}
+
+/* List of functions defined in the module */
+
+static PyMethodDef libntpc_methods[] = {
+    {"statustoa",      	libntpc_statustoa,  	METH_VARARGS,
+     PyDoc_STR("Status string display from peer status word.")},
+    {NULL,		NULL, 0, NULL}		/* sentinel */
+};
+
+PyDoc_STRVAR(module_doc,
+"Python wrapper for selected libntp C library routines.\n\
+");
+
+/* banishes a pointless compiler warning */
+extern PyMODINIT_FUNC initlibntpc(void);
+
+// cppcheck-suppress unusedFunction
+NTPSEC_PY_MODULE_INIT(libntpc)
+{
+    PyObject *m;
+
+    /* Create the module and add the functions */
+    NTPSEC_PY_MODULE_DEF(m, "libntpc", module_doc, libntpc_methods)
+
+    /* for statustoa() */ 
+    PyModule_AddIntConstant(m, "TYPE_SYS", TYPE_SYS);
+    PyModule_AddIntConstant(m, "TYPE_PEER", TYPE_PEER);
+    PyModule_AddIntConstant(m, "TYPE_CLOCK", TYPE_CLOCK);
+
+    if (m == NULL)
+	return NTPSEC_PY_MODULE_ERROR_VAL;
+
+    return NTPSEC_PY_MODULE_SUCCESS_VAL(m);
+}
---
+++ libntp/python_compatibility.h
@@ -0,0 +1,44 @@
+/*
+ * python_compatibility.h -- macros for Python 2/3 compatibility
+ *
+ * This file is Copyright (c) 2016 by the NTPsec project
+ * BSD terms apply: see the file COPYING in the distribution root for details.
+ *
+ * Definitions based on examples in "Supporting Python 3 - The Book Site"
+ *     http://python3porting.com/cextensions.html
+ */
+
+#ifndef _PYTHON_COMPATIBILITY_H_
+#define _PYTHON_COMPATIBILITY_H_
+
+#include <Python.h>
+
+#if PY_MAJOR_VERSION >= 3
+
+#define NTPSEC_PY_MODULE_INIT(name) PyMODINIT_FUNC PyInit_##name(void)
+
+#define NTPSEC_PY_MODULE_DEF(mod, name, doc, methods) \
+    static struct PyModuleDef moduledef = { \
+        PyModuleDef_HEAD_INIT, name, doc, -1, methods, }; \
+    mod = PyModule_Create(&moduledef);
+
+#define NTPSEC_PY_MODULE_ERROR_VAL NULL
+#define NTPSEC_PY_MODULE_SUCCESS_VAL(val) val
+
+#define NTPSEC_PY_BYTE_FORMAT "y#"
+
+#else /* !Python 3 */
+
+#define NTPSEC_PY_MODULE_INIT(name) PyMODINIT_FUNC init##name(void)
+
+#define NTPSEC_PY_MODULE_DEF(mod, name, doc, methods) \
+    mod = Py_InitModule3(name, methods, doc);
+
+#define NTPSEC_PY_MODULE_ERROR_VAL
+#define NTPSEC_PY_MODULE_SUCCESS_VAL(val)
+
+#define NTPSEC_PY_BYTE_FORMAT "s#"
+
+#endif /* !Python 3 */
+
+#endif /* _PYTHON_COMPATIBILITY_H_ */
