5a16aed05afc06d7f46b820615e46af18cafc2a5
Eric S. Raymond <esr@thyrsus.com>
2016-12-01 21:25:00 -0500

    Address GitLab issue #180: Build failure on commit/31f0296a

--- pylib/packet.py
+++ pylib/packet.py
@@ -792,14 +792,14 @@ class ControlSession:
         self.port = sockaddr[1]
         try:
             self.sock = socket.socket(family, socktype, protocol)
-        except socket.error as (errno, msg):
+        except socket.error as e:
             raise ControlException("Error opening %s: %s [%d]" \
-                                   % (hname, msg, errno))
+                                   % (hname, e.msg, e.errno))
         try:
             self.sock.connect(sockaddr)
-        except socket.error as (errno, msg):            
+        except socket.error as e:
             raise ControlException("Error connecting to %s: %s [%d]" \
-                                   % (hname, msg, errno))
+                                   % (hname, e.msg, e.errno))
         return True
 
     def password(self):
