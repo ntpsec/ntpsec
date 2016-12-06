17cfabc1732c6cf77a430bb2d6a2ed442220be29
Matt Selsky <matthew.selsky@twosigma.com>
2016-12-06 01:52:00 -0500

    Fix update ntp.util.rfc3339 so that all tests pass
    Treat the fractional seconds as a string so that we don't change the precision

--- tests/pylib/test_util.py
+++ tests/pylib/test_util.py
@@ -6,6 +6,7 @@ class TestPylibUtilMethods(unittest.TestCase):
     def test_rfc3339(self):
         self.assertEqual(ntp.util.rfc3339(1480999786), '2016-12-06T04:49:46Z')
         self.assertEqual(ntp.util.rfc3339(1480999786.5), '2016-12-06T04:49:46.5Z')
+        self.assertEqual(ntp.util.rfc3339(1480999786.025), '2016-12-06T04:49:46.025Z')
 
     def test_portsplit(self):
         self.assertEqual(ntp.util.portsplit("host.invalid"), ("host.invalid", ""))
