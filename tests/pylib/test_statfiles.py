60f5dadf86f913648eb8c0162a8d9133c85ed1e9
Matt Selsky <matthew.selsky@twosigma.com>
2016-12-07 03:09:10 -0500

    Add some tests for pylib/statfiles.py and fix bugs identified by the tests.
    Including:
    - missing import
    - calling isdigit() on an int
    - time.struct_time has no member m_isdst (and tm_isdst is read-only)
    - posix_to_iso should return ISO 8601 in UTC

---
+++ tests/pylib/test_statfiles.py
@@ -0,0 +1,20 @@
+import unittest
+import ntp.statfiles
+
+class TestPylibStatfilesMethods(unittest.TestCase):
+
+    def test_iso_to_posix(self):
+        self.assertEqual(ntp.statfiles.iso_to_posix(1480999786), 1480999786)
+        self.assertEqual(ntp.statfiles.iso_to_posix("2016-12-06T04:49:46"), 1480999786)
+
+    def test_posix_to_iso(self):
+        self.assertEqual(ntp.statfiles.posix_to_iso(1480999786), "2016-12-06T04:49:46")
+
+    def test_iso_to_posix_inverts_posix_to_iso(self):
+        self.assertEqual(ntp.statfiles.iso_to_posix(ntp.statfiles.posix_to_iso(1480999786)), 1480999786)
+
+    def test_posix_to_iso_inverts_iso_to_posix(self):
+        self.assertEqual(ntp.statfiles.posix_to_iso(ntp.statfiles.iso_to_posix("2016-12-06T04:49:46")), "2016-12-06T04:49:46")
+
+if __name__ == '__main__':
+    unittest.main()
