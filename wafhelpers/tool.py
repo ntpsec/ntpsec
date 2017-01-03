599b16bfb71d258cc270bd1bab479d8a5c577d35
Gary E. Miller <gem@rellim.com>
2017-01-03 14:46:15 -0800

    tool.py: conform to pep8

--- wafhelpers/tool.py
+++ wafhelpers/tool.py
@@ -1,9 +1,14 @@
 from waflib.Logs import pprint
 
-def check_sanity(ctx, cond, lib):
-	define = "HAVE_%s" % lib.upper()
 
-	if cond and (not ctx.get_define(define)):
-		pprint("RED", "Warning %s headers detected, binaries do not build/run" % lib)
-	elif (not cond) and ctx.get_define(define):
-		pprint("RED", "Warning %s headers not detected, binaries build/run" % lib)
+def check_sanity(ctx, cond, lib):
+        define = "HAVE_%s" % lib.upper()
+
+        if cond and (not ctx.get_define(define)):
+                pprint("RED",
+                       "Warning %s headers detected, binaries do not build/run"
+                       % lib)
+        elif (not cond) and ctx.get_define(define):
+                pprint("RED",
+                       "Warning %s headers not detected, binaries build/run"
+                       % lib)
