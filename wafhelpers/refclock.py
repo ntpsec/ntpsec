adfe834b30f71ad2238f4180a1ccce653cd59bc2
Gary E. Miller <gem@rellim.com>
2017-01-03 14:56:27 -0800

    refclock.py: conform to pep8

--- wafhelpers/refclock.py
+++ wafhelpers/refclock.py
@@ -8,186 +8,189 @@ from waflib.Logs import pprint
 
 refclock_map = {
 
-	"local": {
-		"descr":	"Undisciplined Local Clock",
-		"define":	"CLOCK_LOCAL",
-		"file":		"local"
-	},
+    "local": {
+        "descr":    "Undisciplined Local Clock",
+        "define":   "CLOCK_LOCAL",
+        "file":     "local"
+    },
 
-	"spectracom": {
-		"descr":	"Spectracom GPS Receivers",
-		"define":	"CLOCK_SPECTRACOM",
-		"file":		"spectracom"
-	},
+    "spectracom": {
+        "descr":    "Spectracom GPS Receivers",
+        "define":   "CLOCK_SPECTRACOM",
+        "file":     "spectracom"
+    },
 
-	"truetime": {
-		"descr":	"TrueTime GPS/GOES/OMEGA Receivers",
-		"define":	"CLOCK_TRUETIME",
-		"file":		"truetime"
-	},
+    "truetime": {
+        "descr":    "TrueTime GPS/GOES/OMEGA Receivers",
+        "define":   "CLOCK_TRUETIME",
+        "file":     "truetime"
+    },
 
-	"generic": {
-		"descr":	"Generic Reference Driver (Parse)",
-		"define":	"CLOCK_GENERIC",
-		"require":	["parse"],
-		"file":		"generic"
-	},
+    "generic": {
+        "descr":    "Generic Reference Driver (Parse)",
+        "define":   "CLOCK_GENERIC",
+        "require":  ["parse"],
+        "file":     "generic"
+    },
 
-	"magnavox": {
-		"descr":	"Magnavox MX4200 and related GPS receivers",
-		"define":	"CLOCK_MAGNAVOX",
-		"require":	["ppsapi"],
-		"file":		"magnavox"
-	},
+    "magnavox": {
+        "descr":    "Magnavox MX4200 and related GPS receivers",
+        "define":   "CLOCK_MAGNAVOX",
+        "require":  ["ppsapi"],
+        "file":     "magnavox"
+    },
 
-	"arbiter": {
-		"descr":	"Arbiter 1088A/B GPS Receiver",
-		"define":	"CLOCK_ARBITER",
-		"file":		"arbiter"
-	},
+    "arbiter": {
+        "descr":    "Arbiter 1088A/B GPS Receiver",
+        "define":   "CLOCK_ARBITER",
+        "file":     "arbiter"
+    },
 
-	"modem": {
-		"descr":	"NIST/USNO/PTB Modem Time Services",
-		"define":	"CLOCK_MODEM",
-		"file":		"modem"
-	},
+    "modem": {
+        "descr":    "NIST/USNO/PTB Modem Time Services",
+        "define":   "CLOCK_MODEM",
+        "file":     "modem"
+    },
 
-	"nmea": {
-		"descr":	"Generic NMEA GPS Receiver",
-		"define":	"CLOCK_NMEA",
-		"file":		"nmea"
-	},
+    "nmea": {
+        "descr":    "Generic NMEA GPS Receiver",
+        "define":   "CLOCK_NMEA",
+        "file":     "nmea"
+    },
 
-	"pps": {
-		"descr":	"PPS Clock Discipline",
-		"define":	"CLOCK_PPS",
-		"require":	["ppsapi"],
-		"file":		"pps"
-	},
+    "pps": {
+        "descr":    "PPS Clock Discipline",
+        "define":   "CLOCK_PPS",
+        "require":  ["ppsapi"],
+        "file":     "pps"
+    },
 
-	"hpgps": {
-		"descr":	"Hewlett Packard 58503A GPS Receiver",
-		"define":	"CLOCK_HPGPS",
-		"file":		"hpgps"
-	},
+    "hpgps": {
+        "descr":    "Hewlett Packard 58503A GPS Receiver",
+        "define":   "CLOCK_HPGPS",
+        "file":     "hpgps"
+    },
 
-	"shm": {
-		"descr":	"Shared Memory Driver",
-		"define":	"CLOCK_SHM",
-		"file":		"shm"
-	},
+    "shm": {
+        "descr":    "Shared Memory Driver",
+        "define":   "CLOCK_SHM",
+        "file":     "shm"
+    },
 
-	"trimble": {
-		"descr":	"Trimble Navigation GPSes",
-		"define":	"CLOCK_TRIMBLE",
-		"file":		"trimble"
-	},
+    "trimble": {
+        "descr":    "Trimble Navigation GPSes",
+        "define":   "CLOCK_TRIMBLE",
+        "file":     "trimble"
+    },
 
-	"oncore": {
-		"descr":	"Motorola UT Oncore GPS",
-		"define":	"CLOCK_ONCORE",
-		"require":	["ppsapi"],
-		"file":		"oncore"
-	},
+    "oncore": {
+        "descr":    "Motorola UT Oncore GPS",
+        "define":   "CLOCK_ONCORE",
+        "require":  ["ppsapi"],
+        "file":     "oncore"
+    },
 
-	"jupiter": {
-		"descr":	"Rockwell Jupiter GPS",
-		"define":	"CLOCK_JUPITER",
-		"require":	["ppsapi"],
-		"file":		"jupiter"
-	},
+    "jupiter": {
+        "descr":    "Rockwell Jupiter GPS",
+        "define":   "CLOCK_JUPITER",
+        "require":  ["ppsapi"],
+        "file":     "jupiter"
+    },
 
-	"dumbclock": {
-		"descr":	"Dumb Clock",
-		"define":	"CLOCK_DUMBCLOCK",
-		"file":		"dumbclock"
-	},
+    "dumbclock": {
+        "descr":    "Dumb Clock",
+        "define":   "CLOCK_DUMBCLOCK",
+        "file":     "dumbclock"
+    },
 
-	"jjy": {
-		"descr":	"JJY Receivers",
-		"define":	"CLOCK_JJY",
-		"file":		"jjy"
-	},
+    "jjy": {
+        "descr":    "JJY Receivers",
+        "define":   "CLOCK_JJY",
+        "file":     "jjy"
+    },
 
-	"zyfer": {
-		"descr":	"Zyfer GPStarplus Receiver",
-		"define":	"CLOCK_ZYFER",
-		"file":		"zyfer"
-	},
+    "zyfer": {
+        "descr":    "Zyfer GPStarplus Receiver",
+        "define":   "CLOCK_ZYFER",
+        "file":     "zyfer"
+    },
 
-	"neoclock": {
-		"descr":	"NeoClock4X - DCF77 / TDF serial line",
-		"define":	"CLOCK_NEOCLOCK",
-		"file":		"neoclock"
-	},
+    "neoclock": {
+        "descr":    "NeoClock4X - DCF77 / TDF serial line",
+        "define":   "CLOCK_NEOCLOCK",
+        "file":     "neoclock"
+    },
 
-	"gpsd": {
-		"descr":	"GPSD NG client protocol",
-		"define":	"CLOCK_GPSDJSON",
-		"file":		"gpsd"
-	}
+    "gpsd": {
+        "descr":    "GPSD NG client protocol",
+        "define":   "CLOCK_GPSDJSON",
+        "file":     "gpsd"
+    }
 }
 
 
 @conf
 def refclock_config(ctx):
-	from wafhelpers.refclock import refclock_map
+    from wafhelpers.refclock import refclock_map
 
-	if ctx.options.refclocks == "all":
-		ids = refclock_map.keys()
-	else:
-		# XXX: better error checking
-		ids = ctx.options.refclocks.split(",")
+    if ctx.options.refclocks == "all":
+        ids = refclock_map.keys()
+    else:
+        # XXX: better error checking
+        ids = ctx.options.refclocks.split(",")
 
-	ctx.env.REFCLOCK_SOURCE = []
+    ctx.env.REFCLOCK_SOURCE = []
 
-	# Remove duplicate IDs while preserving order.
-	unique_id = []
-	[unique_id.append(x) for x in ids if x not in unique_id]
+    # Remove duplicate IDs while preserving order.
+    unique_id = []
+    [unique_id.append(x) for x in ids if x not in unique_id]
 
-	refclock = False
-	for id in unique_id:
-		if id not in refclock_map:
-			ctx.fatal("'%s' is not a valid Refclock ID" % id)
+    refclock = False
+    for id in unique_id:
+        if id not in refclock_map:
+            ctx.fatal("'%s' is not a valid Refclock ID" % id)
 
-		rc = refclock_map[id]
+        rc = refclock_map[id]
 
-		if rc['define'] == "CLOCK_GENERIC":
-			parse_clocks = (
-				"CLOCK_COMPUTIME",
-				"CLOCK_DCF7000",
-				"CLOCK_HOPF6021",
-				"CLOCK_MEINBERG",
-				"CLOCK_RAWDCF",
-				"CLOCK_RCC8000",
-				"CLOCK_SCHMID",
-				"CLOCK_SEL240X",
-				"CLOCK_TRIMTAIP",
-				"CLOCK_TRIMTSIP",
-				"CLOCK_VARITEXT",
-				"CLOCK_WHARTON_400A",
-			)
-			for subtype in parse_clocks:
-				ctx.define(subtype, 1, comment="Enable individual parse clock")
+        if rc['define'] == "CLOCK_GENERIC":
+            parse_clocks = (
+                "CLOCK_COMPUTIME",
+                "CLOCK_DCF7000",
+                "CLOCK_HOPF6021",
+                "CLOCK_MEINBERG",
+                "CLOCK_RAWDCF",
+                "CLOCK_RCC8000",
+                "CLOCK_SCHMID",
+                "CLOCK_SEL240X",
+                "CLOCK_TRIMTAIP",
+                "CLOCK_TRIMTSIP",
+                "CLOCK_VARITEXT",
+                "CLOCK_WHARTON_400A",
+            )
+            for subtype in parse_clocks:
+                ctx.define(subtype, 1, comment="Enable individual parse clock")
 
-		ctx.start_msg("Enabling Refclock %s (%s):" % (rc["descr"], id))
+        ctx.start_msg("Enabling Refclock %s (%s):" % (rc["descr"], id))
 
-		if "require" in rc:
-			if "ppsapi" in rc["require"]:
-				if not ctx.get_define("HAVE_PPSAPI"):
-					ctx.end_msg("No")
-					pprint("RED", "Refclock \"%s\" disabled, PPS API has not been detected as working." % rc["descr"])
-					continue
+        if "require" in rc:
+            if "ppsapi" in rc["require"]:
+                if not ctx.get_define("HAVE_PPSAPI"):
+                    ctx.end_msg("No")
+                    pprint("RED",
+                           "Refclock \"%s\" disabled, PPS API has not "
+                           "been detected as working." % rc["descr"])
+                    continue
 
-		ctx.env.REFCLOCK_SOURCE.append((rc["file"], rc["define"]))
-		ctx.env["REFCLOCK_%s" % rc["file"].upper()] = True
-		ctx.define(rc["define"], 1, comment="Enable '%s' refclock" % rc["descr"])
-		ctx.env.REFCLOCK_LIST += [str(id)]
+        ctx.env.REFCLOCK_SOURCE.append((rc["file"], rc["define"]))
+        ctx.env["REFCLOCK_%s" % rc["file"].upper()] = True
+        ctx.define(rc["define"], 1,
+                   comment="Enable '%s' refclock" % rc["descr"])
+        ctx.env.REFCLOCK_LIST += [str(id)]
 
-		ctx.end_msg("Yes")
+        ctx.end_msg("Yes")
 
-		refclock = True
+        refclock = True
 
-	if refclock:
-		ctx.env.REFCLOCK_ENABLE = True
-		ctx.define("REFCLOCK", 1, comment="Enable refclock support")
+    if refclock:
+        ctx.env.REFCLOCK_ENABLE = True
+        ctx.define("REFCLOCK", 1, comment="Enable refclock support")
