83b552f9936a618774f0b3b0bd8f6c4420450e99
Gary E. Miller <gem@rellim.com>
2017-01-03 16:13:02 -0800

    check_structfield.py: conform to pep8

--- wafhelpers/check_structfield.py
+++ wafhelpers/check_structfield.py
@@ -12,17 +12,18 @@ int main(void) {
 }
 """
 
+
 @conf
 def check_structfield(ctx, fld, type, hdrs, mandatory=False):
-        name = "STRUCT_%s_HAS_%s" % (type.upper(), fld.upper().replace('.', '_'))
-        src = ""
-        for hdr in hdrs:
-                src += "#include <%s>\n" % hdr
-        ctx.check_cc(
-                fragment    = TYPE_FRAG % (src, type, fld),
-                define_name = name,
-                execute     = False,
-                msg         = "Checking for %s in struct %s" % (fld, type),
-                mandatory   = mandatory,
-                comment         = "Whether struct '%s' has field '%s'" % (fld, type)
-        )
+    name = "STRUCT_%s_HAS_%s" % (type.upper(), fld.upper().replace('.', '_'))
+    src = ""
+    for hdr in hdrs:
+        src += "#include <%s>\n" % hdr
+    ctx.check_cc(
+        fragment=TYPE_FRAG % (src, type, fld),
+        define_name=name,
+        execute=False,
+        msg="Checking for %s in struct %s" % (fld, type),
+        mandatory=mandatory,
+        comment="Whether struct '%s' has field '%s'" % (fld, type)
+    )
