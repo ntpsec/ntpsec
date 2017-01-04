c186e7b7a4dfd5f0815ba5512cca8ee14510bfea
Gary E. Miller <gem@rellim.com>
2017-01-03 16:29:08 -0800

    check_fortify.py: conform to pep8

--- wafhelpers/check_fortify.py
+++ wafhelpers/check_fortify.py
@@ -1,10 +1,15 @@
 from shlex import split
 
+
 def check_fortify(ctx):
-	ctx.find_program("sourceanalyzer", var="BIN_SOURCEANALYZER", mandatory=True)
+    ctx.find_program("sourceanalyzer", var="BIN_SOURCEANALYZER",
+                     mandatory=True)
 
-	if not ctx.options.fortify_flags:
-		ctx.fatal("You must supply --fortify-flags when using --enable-fortify.")
+    if not ctx.options.fortify_flags:
+        ctx.fatal(
+            "You must supply --fortify-flags when using --enable-fortify.")
 
-	ctx.env.CC = ctx.env.BIN_SOURCEANALYZER + split(ctx.options.fortify_flags) + ctx.env.CC
-	ctx.env.AR = ctx.env.BIN_SOURCEANALYZER + split(ctx.options.fortify_flags) + ctx.env.AR
+    ctx.env.CC = ctx.env.BIN_SOURCEANALYZER + \
+        split(ctx.options.fortify_flags) + ctx.env.CC
+    ctx.env.AR = ctx.env.BIN_SOURCEANALYZER + \
+        split(ctx.options.fortify_flags) + ctx.env.AR
