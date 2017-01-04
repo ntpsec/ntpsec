4dcd4eb1f1f00822508a30dfd373e610639294f3
Gary E. Miller <gem@rellim.com>
2017-01-03 16:50:52 -0800

    test_util.py: conform to pep8

--- tests/pylib/test_util.py
+++ tests/pylib/test_util.py
@@ -1,17 +1,25 @@
 import unittest
 import ntp.util
 
+
 class TestPylibUtilMethods(unittest.TestCase):
 
     def test_rfc3339(self):
-        self.assertEqual(ntp.util.rfc3339(1480999786), '2016-12-06T04:49:46Z')
-        self.assertEqual(ntp.util.rfc3339(1480999786.5), '2016-12-06T04:49:46.5Z')
-        self.assertEqual(ntp.util.rfc3339(1480999786.025), '2016-12-06T04:49:46.025Z')
+        self.assertEqual(ntp.util.rfc3339(1480999786),
+                         '2016-12-06T04:49:46Z')
+        self.assertEqual(ntp.util.rfc3339(1480999786.5),
+                         '2016-12-06T04:49:46.5Z')
+        self.assertEqual(ntp.util.rfc3339(1480999786.025),
+                         '2016-12-06T04:49:46.025Z')
 
     def test_portsplit(self):
-        self.assertEqual(ntp.util.portsplit("host.invalid"), ("host.invalid", ""))
-        self.assertEqual(ntp.util.portsplit("host.invalid:123"), ("host.invalid", ":123"))
-        self.assertEqual(ntp.util.portsplit("[0000:1111:2222:3333:4444:5555:6666:7777]:123"), ("0000:1111:2222:3333:4444:5555:6666:7777", ":123"))
+        self.assertEqual(ntp.util.portsplit("host.invalid"),
+                         ("host.invalid", ""))
+        self.assertEqual(ntp.util.portsplit("host.invalid:123"),
+                         ("host.invalid", ":123"))
+        self.assertEqual(ntp.util.portsplit(
+            "[0000:1111:2222:3333:4444:5555:6666:7777]:123"),
+            ("0000:1111:2222:3333:4444:5555:6666:7777", ":123"))
 
 if __name__ == '__main__':
     unittest.main()
