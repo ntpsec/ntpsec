fb6fa70bf2a5fdc03584f3decbc755bf2ec9fd09
Matt Selsky <matthew.selsky@twosigma.com>
2017-01-18 02:31:11 -0500

    Merge libsodium 1.0.11
    Partially addresses GitLab issue #209  Errors like:
    "../../libsodium/include/sodium/core.h", line 11: syntax error before or at: __global
    
    Fixed upstream via https://github.com/jedisct1/libsodium/pull/309

--- libsodium/randombytes/randombytes.c
+++ libsodium/randombytes/randombytes.c
@@ -1,4 +1,5 @@
 
+#include <stdlib.h>
 #include <sys/types.h>
 
 #include <assert.h>
@@ -8,8 +9,21 @@
 #include "randombytes.h"
 #include "randombytes_sysrandom.h"
 
-static const randombytes_implementation *implementation =
-    &randombytes_sysrandom_implementation;
+/* C++Builder defines a "random" macro */
+#undef random
+
+static const randombytes_implementation *implementation;
+
+#  define RANDOMBYTES_DEFAULT_IMPLEMENTATION &randombytes_sysrandom_implementation;
+
+static void
+randombytes_init_if_needed(void)
+{
+    if (implementation == NULL) {
+        implementation = RANDOMBYTES_DEFAULT_IMPLEMENTATION;
+        randombytes_stir();
+    }
+}
 
 int
 randombytes_set_implementation(randombytes_implementation *impl)
@@ -22,19 +36,22 @@ randombytes_set_implementation(randombytes_implementation *impl)
 const char *
 randombytes_implementation_name(void)
 {
+    randombytes_init_if_needed();
     return implementation->implementation_name();
 }
 
 uint32_t
 randombytes_random(void)
 {
+    randombytes_init_if_needed();
     return implementation->random();
 }
 
 void
 randombytes_stir(void)
 {
-    if (implementation != NULL && implementation->stir != NULL) {
+    randombytes_init_if_needed();
+    if (implementation->stir != NULL) {
         implementation->stir();
     }
 }
@@ -49,7 +66,8 @@ randombytes_uniform(const uint32_t upper_bound)
     uint32_t min;
     uint32_t r;
 
-    if (implementation != NULL && implementation->uniform != NULL) {
+    randombytes_init_if_needed();
+    if (implementation->uniform != NULL) {
         return implementation->uniform(upper_bound);
     }
     if (upper_bound < 2) {
@@ -66,6 +84,7 @@ randombytes_uniform(const uint32_t upper_bound)
 void
 randombytes_buf(void * const buf, const size_t size)
 {
+    randombytes_init_if_needed();
     if (size > (size_t) 0U) {
         implementation->buf(buf, size);
     }
