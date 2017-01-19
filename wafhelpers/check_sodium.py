d8275820f5b9217aa9c0fcf2d7540fcb9c321647
Eric S. Raymond <esr@thyrsus.com>
2017-01-19 16:02:34 -0500

    First version of buildprep script.

--- wafhelpers/check_sodium.py
+++ wafhelpers/check_sodium.py
@@ -1,6 +1,6 @@
 
 
 def check_sodium(ctx):
-    ctx.check_cc(header_name="sodium.h", mandatory=True,
+    ctx.check_cc(header_name="sodium.h", includes=ctx.env.PLATFORM_INCLUDES, mandatory=True,
                  errmsg="No\nFatal Error: Your system is missing libsodium")
     ctx.check_cc(lib="sodium", comment="Sodium crypto library", mandatory=True)
