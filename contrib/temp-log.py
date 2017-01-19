0bda47772a09c85fd593e1253e0a6bd0695c0274
Gary E. Miller <gem@rellim.com>
2017-01-18 17:50:34 -0800

    temp-log: pep8 compliance

--- contrib/temp-log.py
+++ contrib/temp-log.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
-# temp-log.py: A script that will be run eventually as a daemon to log temperature of a system
+# temp-log.py: A script that will be run eventually as a daemon
+#              to log temperature of a system
 # Usage:
 # temp-log.py [-h] [-V] [-s] [-m]
 # Writes logs to /var/log/ntpstats
 # Requires root to run
 
-import sys, re, time, subprocess, os, logging, logging.handlers
+import logging
+import logging.handlers
+import os
+import re
+import subprocess
+import sys
+import time
+
 
 class CpuTemp:
     "Sensors on the CPU Core"
@@ -13,8 +21,9 @@ class CpuTemp:
         # pattern that matches the string that has the cpu temp
         self._pattern = re.compile('^\s+temp\d+_input:\s+([\d\.]+).*$')
         # Find the sensors binary and stores the path
-        self._sensors_path = subprocess.check_output(["which", "sensors"], universal_newlines=False).replace('\n', '')
-        if self._sensors_path == None:
+        self._sensors_path = subprocess.check_output(
+            ["which", "sensors"], universal_newlines=False).replace('\n', '')
+        if self._sensors_path is None:
             raise Exception("Unable to find sensors binary")
 
     def get_data(self):
@@ -36,8 +45,10 @@ class CpuTemp:
     def _record_temp(self):
         "Call the external command 'sensors -u' and get the data"
         # grab the data from the "sensors -u" command
-        output = subprocess.check_output([self._sensors_path, "-u"], universal_newlines=True)
+        output = subprocess.check_output([self._sensors_path, "-u"],
+                                         universal_newlines=True)
         self.sensors_output = output.split('\n')
+
 
 class SmartCtl:
     "Sensor on the Hard Drive"
@@ -51,7 +62,8 @@ class SmartCtl:
 
     def get_data(self):
         "Collects the data and return the output as an array"
-        _output = subprocess.check_output(["smartctl", "-a", self._device], universal_newlines=True)
+        _output = subprocess.check_output(["smartctl", "-a", self._device],
+                                          universal_newlines=True)
         _lines = _output.split('\n')
         for line in _lines:
             match = self._pat.match(line)
@@ -59,6 +71,7 @@ class SmartCtl:
             if match and match.group(1):
                 temp = match.group(1)
                 return ('%d SMART %s' % (now, temp))
+
 
 class ZoneTemp:
     "Sensors on the CPU Zones"
@@ -74,7 +87,8 @@ class ZoneTemp:
         _zone = 0
         _data = []
         for zone in self.zone_directories:
-            _zone_data = open(os.path.join(os.path.join(self._base_dir, zone), 'temp'))
+            _zone_data = open(os.path.join(os.path.join(self._base_dir, zone),
+                                           'temp'))
             for line in _zone_data:
                 temp = float(line) / 1000
                 _now = int(time.time())
@@ -86,18 +100,26 @@ class ZoneTemp:
 # Global vars
 version = 1.0
 # Create objects
-cpu  = CpuTemp()
+cpu = CpuTemp()
 zone = ZoneTemp()
-hdd  = SmartCtl('/dev/sda')
+hdd = SmartCtl('/dev/sda')
+
 
 def logging_setup(fileName, levelName, logLevel):
     "Create logging object with midnight rotation"
-    logFormat = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logFormat = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     # Create logger for cpuTemp
     tempLogger = logging.getLogger(levelName)
     tempLogger.setLevel(logLevel)
     # Create file handler
-    _file = logging.handlers.TimedRotatingFileHandler(fileName, when='midnight', interval=1, backupCount=5, encoding=None, delay=False, utc=False)
+    _file = logging.handlers.TimedRotatingFileHandler(fileName,
+                                                      when='midnight',
+                                                      interval=1,
+                                                      backupCount=5,
+                                                      encoding=None,
+                                                      delay=False,
+                                                      utc=False)
     _file.setLevel(logLevel)
     # Create the formatter and add it to the handler
     _file.setFormatter(logFormat)
@@ -105,6 +127,7 @@ def logging_setup(fileName, levelName, logLevel):
     tempLogger.addHandler(_file)
     # Logging rotate
     return tempLogger
+
 
 def log_data(logger, data):
     "Write data to the log"
@@ -114,12 +137,16 @@ def log_data(logger, data):
     else:
         logger.info(data)
 
+
 def log_with_multiple_files():
     "Write the logs to individual files depending on sensor"
     # Create logger instances
-    zoneLogger = logging_setup('/var/log/ntpstats/zoneTemp.log', 'zone_levelname', logging.INFO)
-    cpuLogger  = logging_setup('/var/log/ntpstats/cpuTemp.log', 'cpu_levelname', logging.INFO)
-    hddLogger  = logging_setup('/var/log/ntpstats/hddTemp.log', 'hdd_levelname', logging.INFO)
+    zoneLogger = logging_setup('/var/log/ntpstats/zoneTemp.log',
+                               'zone_levelname', logging.INFO)
+    cpuLogger = logging_setup('/var/log/ntpstats/cpuTemp.log',
+                              'cpu_levelname', logging.INFO)
+    hddLogger = logging_setup('/var/log/ntpstats/hddTemp.log',
+                              'hdd_levelname', logging.INFO)
 
     # Write data to their respective logs forever
     while True:
@@ -128,13 +155,17 @@ def log_with_multiple_files():
         log_data(hddLogger, hdd.get_data())
         # Sleep 15 seconds
         time.sleep(15)
+
 
 def log_with_one_file():
     "Write all temperature readings to one file"
     # Create the logger instance
-    zoneLogger = logging_setup('/var/log/ntpstats/temp.log', 'zone_levelname', logging.INFO)
-    cpuLogger  = logging_setup('/var/log/ntpstats/temp.log', 'cpu_levelname', logging.INFO)
-    hddLogger  = logging_setup('/var/log/ntpstats/temp.log', 'hdd_levelname', logging.INFO)
+    zoneLogger = logging_setup('/var/log/ntpstats/temp.log',
+                               'zone_levelname', logging.INFO)
+    cpuLogger = logging_setup('/var/log/ntpstats/temp.log',
+                              'cpu_levelname', logging.INFO)
+    hddLogger = logging_setup('/var/log/ntpstats/temp.log',
+                              'hdd_levelname', logging.INFO)
 
     # Write data to their respective logs forever
     while True:
@@ -144,9 +175,11 @@ def log_with_one_file():
         # Sleep 15 seconds
         time.sleep(15)
 
+
 def help_text():
     "Help message"
-    print("temp-log.py [-f filepath] [-h] [-s [-r {hour|day|week|month|year}] [-m] [-V]")
+    print("temp-log.py [-f filepath] [-h] "
+          "[-s [-r {hour|day|week|month|year}] [-m] [-V]")
     print("-h\tPrints this help message")
     print("-s\tWrites all data to a single log file")
     print("-m\tWrites data for each sensor type to their own log file")
