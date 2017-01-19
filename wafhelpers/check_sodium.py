88475778f57f012b64f7ab4ca21aea6520095878
Gary E. Miller <gem@rellim.com>
2017-01-18 17:36:51 -0800

    configure: pep8 compliance

--- wafhelpers/check_sodium.py
+++ wafhelpers/check_sodium.py
@@ -1,8 +1,8 @@
 import sys
 from waflib.Logs import pprint
 
+
 def check_sodium(ctx):
     ctx.check_cc(header_name="sodium.h", mandatory=True,
                  errmsg="No\nFatal Error: Your system is missing libsodium")
     ctx.check_cc(lib="sodium", comment="Sodium crypto library", mandatory=True)
-
