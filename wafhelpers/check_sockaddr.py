1527b195afc071fa47b1ad56416eba9a018aea70
Eric S. Raymond <esr@thyrsus.com>
2017-01-22 12:42:51 -0500

    No need to test for sockaddr_storage, it's POSIX.

--- wafhelpers/check_sockaddr.py
+++ wafhelpers/check_sockaddr.py
@@ -1,12 +1,3 @@
-SOCKADDR_STORAGE_FRAG = """
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-
-struct sockaddr_storage n;
-"""
-
-
 SA_LEN_FRAG = """
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -18,15 +9,6 @@ int main(void) {
 
 
 def check_sockaddr(ctx):
-    ctx.check_cc(
-        fragment=SOCKADDR_STORAGE_FRAG,
-        define_name="HAVE_STRUCT_SOCKADDR_STORAGE",
-        features="c",
-        msg="Checking for type sockaddr_storage",
-        mandatory=False,
-        comment="Whether sockaddr_storage exists"
-    )
-
     ctx.check_cc(
         fragment=SA_LEN_FRAG,
         define_name="ISC_PLATFORM_HAVESALEN",
