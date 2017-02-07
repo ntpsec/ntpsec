b08bdb4358d4e55b0c7650ec5ff234b7312fc1f0
Gary E. Miller <gem@rellim.com>
2017-02-06 17:59:04 -0800

    pep8 nits

--- pylib/util.py
+++ pylib/util.py
@@ -63,49 +63,54 @@ def portsplit(hostname):
             hostname = hostname[1:-1]   # Strip brackets
     return (hostname, portsuffix)
 
+
 def f8dot4(f):
     "Scaled floating point formatting to fit in 8 characters"
     if f >= 0:
-      if f < 1000.0:
-        return "%8.4f" % f    # xxx.xxxx  normal case
-      elif f < 10000.0:
-        return "%8.3f" % f    # xxxx.xxx
-      elif f < 100000.0:
-        return "%8.2f" % f    # xxxxx.xx
-      elif f < 1000000.0:
-        return "%8.1f" % f    # xxxxxx.x
-      return "%8d" % f        # xxxxxxxx
+        if f < 1000.0:
+            return "%8.4f" % f    # xxx.xxxx  normal case
+        elif f < 10000.0:
+            return "%8.3f" % f    # xxxx.xxx
+        elif f < 100000.0:
+            return "%8.2f" % f    # xxxxx.xx
+        elif f < 1000000.0:
+            return "%8.1f" % f    # xxxxxx.x
+        return "%8d" % f          # xxxxxxxx
+
     if f > -100.0:
-      return "%8.4f" % f      # -xx.xxxx  normal case
+        return "%8.4f" % f      # -xx.xxxx  normal case
     elif f > -1000.0:
-      return "%8.3f" % f      # -xxx.xxx
+        return "%8.3f" % f      # -xxx.xxx
     elif f > -10000.0:
-      return "%8.2f" % f      # -xxxx.xx
+        return "%8.2f" % f      # -xxxx.xx
     elif f > -100000.0:
-      return "%8.1f" % f      # -xxxxx.x
-    return "%8d" % f          # -xxxxxxx
+        return "%8.1f" % f      # -xxxxx.x
+    return "%8d" % f            # -xxxxxxx
+
 
 def f8dot3(f):
     "Scaled floating point formatting to fit in 8 characters"
     if f >= 0:
-      if f < 10000.0:
-        return "%8.3f" % f    # xxxx.xxx  normal case
-      elif f < 100000.0:
-        return "%8.2f" % f    # xxxxx.xx
-      elif f < 1000000.0:
-        return "%8.1f" % f    # xxxxxx.x
-      return "%8d" % f        # xxxxxxxx
+        if f < 10000.0:
+            return "%8.3f" % f    # xxxx.xxx  normal case
+        elif f < 100000.0:
+            return "%8.2f" % f    # xxxxx.xx
+        elif f < 1000000.0:
+            return "%8.1f" % f    # xxxxxx.x
+        return "%8d" % f          # xxxxxxxx
+
     if f > -1000.0:
-      return "%8.3f" % f      # -xxx.xxx  normal case
+        return "%8.3f" % f      # -xxx.xxx  normal case
     elif f > -10000.0:
-      return "%8.2f" % f      # -xxxx.xx
+        return "%8.2f" % f      # -xxxx.xx
     elif f > -100000.0:
-      return "%8.1f" % f      # -xxxxx.x
-    return "%8d" % f          # -xxxxxxx
+        return "%8.1f" % f      # -xxxxx.x
+    return "%8d" % f            # -xxxxxxx
 
 
 # A hack to avoid repeatedly hammering on DNS when ntpmon runs.
 canonicalization_cache = {}
+
 
 def canonicalize_dns(inhost, family=socket.AF_UNSPEC):
     "Canonicalize a hostname or numeric IP address."
