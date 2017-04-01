66a811d51b05d7605cf560b9847d4ddca4e4b54b
Gary E. Miller <gem@rellim.com>
2017-03-31 19:59:04 -0700

    Make 4 implicit casts to be explicit.
    No functional changes.

--- libparse/clk_varitext.c
+++ libparse/clk_varitext.c
@@ -182,12 +182,12 @@ inp_varitext(
 
   if (t->start_found)
     {
-      if ((rtc = parse_addchar(parseio, ch)) != PARSE_INP_SKIP)
+      if ((rtc = (int)parse_addchar(parseio, ch)) != PARSE_INP_SKIP)
 	{
 	  parseprintf(DD_PARSE, ("inp_varitext: ABORTED due to too many characters\n"));
 
 	  memset(t, 0, sizeof(struct varitext));
-	  return rtc;
+	  return (unsigned long)rtc;
 	}
 
       if (t->end_found)
@@ -197,10 +197,10 @@ inp_varitext(
 	      parseprintf(DD_PARSE, ("inp_varitext: END seen\n"));
 
 	      memset(t, 0, sizeof(struct varitext));
-	      if ((rtc = parse_addchar(parseio, 0)) == PARSE_INP_SKIP)
+	      if ((rtc = (int)parse_addchar(parseio, 0)) == PARSE_INP_SKIP)
 		return parse_end(parseio);
 	      else
-		return rtc;
+		return (unsigned long)rtc;
 	    }
 	}
 
@@ -211,7 +211,7 @@ inp_varitext(
 
     }
 
-  t->previous_ch = ch;
+  t->previous_ch = (unsigned char)ch;
 
   return PARSE_INP_SKIP;
 }
