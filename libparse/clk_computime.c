c234152d2c1882d067a3a76dd8f7e1d0d6e852ed
Gary E. Miller <gem@rellim.com>
2017-04-06 19:22:07 -0700

    Fix one cast, add one cast.

--- libparse/clk_computime.c
+++ libparse/clk_computime.c
@@ -122,7 +122,8 @@ inp_computime(
 {
 	unsigned int rtc;
 
-	parseprintf(DD_PARSE, ("inp_computime(0x%lx, 0x%x, ...)\n", (long)parseio, ch));
+	parseprintf(DD_PARSE, ("inp_computime(0x%lx, 0x%x, ...)\n",
+                    (long unsigned)parseio, (unsigned int)ch));
 
 	switch (ch)
 	{
