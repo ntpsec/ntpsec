108f1d9978496d09cfc865e9ad38a126e9b84f27
Gary E. Miller <gem@rellim.com>
2017-04-20 12:13:27 -0700

    waf: define EPOCH=[build-date], for reproducable builds.
    Sort of per: https://reproducible-builds.org/specs/source-date-epoch/
    
    EPOCH is set to the first of:
    --epoch=[seconds of UNIX epoch]
    SOURCE_DATE_EPOCH from the environment
    configure time seconds of UNIX epoch
    
    The spec would prefer the last source modification for the last one,
    but close enough for now.

--- wafhelpers/options.py
+++ wafhelpers/options.py
@@ -70,8 +70,7 @@ def options_cmd(ctx, config):
     grp.add_option('--cflags', type='string', action="callback",
                    callback=callback_flags,
                    help="Users should use CFLAGS in their environment.")
-    grp.add_option( '--epoch', type='string', action="callback",
-        callback=callback_flags,
+    grp.add_option( '--epoch', type='int', default=None,
         help="Force epoch, or use SOURCE_DATE_EPOCH in environment")
     grp.add_option('--ldflags', type='string', action="callback",
                    callback=callback_flags,
