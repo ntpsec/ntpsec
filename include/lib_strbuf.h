d15f9615479aff77c9ea7b16d63032231886a446
Gary E. Miller <gem@rellim.com>
2017-05-10 14:09:08 -0700

    Revert "LIB_BUFLENGTH: gcc complained that 128 was too short, increased to 192."
    This reverts commit aeecd73b33df8b5a31294d96d4031a9a728e9fa3.
    
    I increased the wrong buffer for bug #293

--- include/lib_strbuf.h
+++ include/lib_strbuf.h
@@ -11,8 +11,7 @@
  * Sizes of things
  */
 #define LIB_NUMBUF	16
-/* gcc 7 showed us that 128 is too short for LIB_BUFLENGTH */
-#define	LIB_BUFLENGTH	192
+#define	LIB_BUFLENGTH	128
 
 typedef char libbufstr[LIB_BUFLENGTH];
 extern libbufstr lib_stringbuf[LIB_NUMBUF];
