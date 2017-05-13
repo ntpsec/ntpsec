aeb6e44d681f473e5e76371f1d36a5c1bbc026fd
Gary E. Miller <gem@rellim.com>
2017-05-12 18:25:22 -0700

    tests: add tests for get_lsb_uint16() and get_lsb_uint32().

--- tests/libparse/binio.c
+++ tests/libparse/binio.c
@@ -17,7 +17,7 @@ TEST_SETUP(binio) {}
 
 TEST_TEAR_DOWN(binio) {}
 
-/* LSB tests */
+/* LSB int tests */
 
 TEST(binio, get_lsb_int160) {
         long ret;
@@ -124,6 +124,113 @@ TEST(binio, get_lsb_int324) {
         TEST_ASSERT_EQUAL_HEX32( 0x080000000UL, ret );
 }
 
+/* LSB uint tests */
+
+TEST(binio, get_lsb_uint160) {
+        long ret;
+        unsigned char zero[2] = { 0, 0};
+        unsigned char *bp = &zero[0];
+
+        ret = get_lsb_uint16( &bp);
+
+        TEST_ASSERT_EQUAL_INT64( 0, ret );
+}
+
+TEST(binio, get_lsb_uint161) {
+        long ret;
+        unsigned char zero[2] = { 1, 2};
+        unsigned char *bp = &zero[0];
+
+        ret = get_lsb_uint16( &bp);
+
+        TEST_ASSERT_EQUAL_HEX64( 0x0201UL, ret );
+}
+
+TEST(binio, get_lsb_uint162) {
+        long ret;
+        unsigned char zero[2] = { 2, 1};
+        unsigned char *bp = &zero[0];
+
+        ret = get_lsb_uint16( &bp);
+
+        TEST_ASSERT_EQUAL_HEX64( 0x0102UL, ret );
+}
+
+TEST(binio, get_lsb_uint163) {
+        long ret;
+        unsigned char zero[2] = { 0xff, 0xff};
+        unsigned char *bp = &zero[0];
+
+        ret = get_lsb_uint16( &bp);
+
+        TEST_ASSERT_EQUAL_HEX64( 0x0ffffUL, ret );
+}
+
+TEST(binio, get_lsb_uint164) {
+        long ret;
+        unsigned char zero[2] = { 0, 0x80};
+        unsigned char *bp = &zero[0];
+
+        ret = get_lsb_uint16( &bp);
+
+        TEST_ASSERT_EQUAL_HEX64( 0x8000L, ret );
+}
+
+
+TEST(binio, get_lsb_uint320) {
+        uint32_t ret;
+        unsigned char zero[4] = { 0, 0, 0, 0};
+        unsigned char *bp = &zero[0];
+
+        ret = get_lsb_uint32( &bp);
+
+        TEST_ASSERT_EQUAL_UINT32( 0, ret );
+}
+
+
+TEST(binio, get_lsb_uint321) {
+        uint32_t ret;
+        unsigned char zero[4] = { 1, 2, 3, 4};
+        unsigned char *bp = &zero[0];
+
+        ret = get_lsb_uint32( &bp);
+
+        TEST_ASSERT_EQUAL_HEX32( 0x04030201UL, ret );
+}
+
+
+TEST(binio, get_lsb_uint322) {
+        uint32_t ret;
+        unsigned char zero[4] = { 4, 3, 2, 1};
+        unsigned char *bp = &zero[0];
+
+        ret = get_lsb_uint32( &bp);
+
+        TEST_ASSERT_EQUAL_HEX32( 0x01020304UL, ret );
+}
+
+
+TEST(binio, get_lsb_uint323) {
+        uint32_t ret;
+        unsigned char zero[4] = { 0xff, 0xff, 0xff, 0xff};
+        unsigned char *bp = &zero[0];
+
+        ret = get_lsb_uint32( &bp);
+
+        TEST_ASSERT_EQUAL_HEX32( 0x0FFFFFFFFUL, ret );
+}
+
+
+TEST(binio, get_lsb_uint324) {
+        uint32_t ret;
+        unsigned char zero[4] = { 0, 0, 0, 0x80};
+        unsigned char *bp = &zero[0];
+
+        ret = get_lsb_uint32( &bp);
+
+        TEST_ASSERT_EQUAL_HEX32( 0x080000000UL, ret );
+}
+
 /* MSB tests */
 
 TEST(binio, get_msb_short0) {
@@ -177,7 +284,7 @@ TEST(binio, get_msb_short4) {
 }
 
 TEST_GROUP_RUNNER(binio) {
-        /* LSB tests */
+        /* LSB int tests */
         RUN_TEST_CASE(binio, get_lsb_int160);
         RUN_TEST_CASE(binio, get_lsb_int161);
         RUN_TEST_CASE(binio, get_lsb_int162);
@@ -189,6 +296,19 @@ TEST_GROUP_RUNNER(binio) {
         RUN_TEST_CASE(binio, get_lsb_int322);
         RUN_TEST_CASE(binio, get_lsb_int323);
         RUN_TEST_CASE(binio, get_lsb_int324);
+
+        /* LSB uint tests */
+        RUN_TEST_CASE(binio, get_lsb_uint160);
+        RUN_TEST_CASE(binio, get_lsb_uint161);
+        RUN_TEST_CASE(binio, get_lsb_uint162);
+        RUN_TEST_CASE(binio, get_lsb_uint163);
+        RUN_TEST_CASE(binio, get_lsb_uint164);
+
+        RUN_TEST_CASE(binio, get_lsb_uint320);
+        RUN_TEST_CASE(binio, get_lsb_uint321);
+        RUN_TEST_CASE(binio, get_lsb_uint322);
+        RUN_TEST_CASE(binio, get_lsb_uint323);
+        RUN_TEST_CASE(binio, get_lsb_uint324);
 
         /* MSB tests */
         RUN_TEST_CASE(binio, get_msb_short0);
