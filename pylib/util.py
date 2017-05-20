d92b0f984a94b7ef2efa1312b9aefc9411dbdaf5
Matt Selsky <matthew.selsky@twosigma.com>
2017-05-20 17:22:24 -0400

    Use python in wscript instead of autorevision.sh
    Fewer languages and hopefully more predictable builds

--- pylib/util.py
+++ pylib/util.py
@@ -13,7 +13,6 @@ import socket
 import sys
 import time
 import ntp.ntpc
-import ntp.version
 import ntp.magic
 import ntp.control
 
@@ -107,9 +106,7 @@ def safeargcast(arg, castfunc, errtext, usage):
 
 def stdversion():
     "Returns the NTPsec version string in a standard format"
-    return "ntpsec-%s+%s %s" % (ntp.version.VERSION,
-                                ntp.version.VCS_TICK,
-                                ntp.version.VCS_DATE)
+    return "ntpsec-%s" % "@NTPSEC_VERSION@"
 
 
 def rfc3339(t):
