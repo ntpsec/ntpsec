01905e33f8b3624e0cc0049ea314c3a7bac60658
Gary E. Miller <gem@rellim.com>
2017-05-22 21:32:58 -0700

    libparse: remove unused array, and wrong file comment.

--- libparse/info_trimble.c
+++ libparse/info_trimble.c
@@ -1,55 +1,8 @@
-/*
- * Automatically generated - do not modify
- */
 
 #include "config.h"
 #include "ntp_types.h"
 #include "ntpd.h"
 #include "trimble.h"
-
-cmd_info_t trimble_scmds[] = {
-	{ CMD_CCLROSC, "CMD_CCLROSC", "clear oscillator offset (0x1D)", "", 0 },
-	{ CMD_CCLRRST, "CMD_CCLRRST", "clear battery backup and RESET (0x1E)", "", 0 },
-	{ CMD_CVERSION, "CMD_CVERSION", "return software version (0x1F)", "", 0 },
-	{ CMD_CALMANAC, "CMD_CALMANAC", "almanac (0x20)", "", 0 },
-	{ CMD_CCURTIME, "CMD_CCURTIME", "current time (0x21)", "", 0 },
-	{ CMD_CMODESEL, "CMD_CMODESEL", "mode select (2-d, 3-D, auto) (0x22)", "", 0 },
-	{ CMD_CINITPOS, "CMD_CINITPOS", "initial position (0x23)", "", 0 },
-	{ CMD_CRECVPOS, "CMD_CRECVPOS", "receiver position fix mode (0x24)", "", 0 },
-	{ CMD_CRESET, "CMD_CRESET", "soft reset & selftest (0x25)", "", 0 },
-	{ CMD_CRECVHEALTH, "CMD_CRECVHEALTH", "receiver health (0x26)", "", 0 },
-	{ CMD_CSIGNALLV, "CMD_CSIGNALLV", "signal levels (0x27)", "", 0 },
-	{ CMD_CMESSAGE, "CMD_CMESSAGE", "GPS system message (0x28)", "", 0 },
-	{ CMD_CALMAHEALTH, "CMD_CALMAHEALTH", "almanac healt page (0x29)", "", 0 },
-	{ CMD_C2DALTITUDE, "CMD_C2DALTITUDE", "altitude for 2-D mode (0x2A)", "", 0 },
-	{ CMD_CINITPOSLLA, "CMD_CINITPOSLLA", "initial position LLA (0x2B)", "", 0 },
-	{ CMD_COPERPARAM, "CMD_COPERPARAM", "operating parameters (0x2C)", "", 0 },
-	{ CMD_COSCOFFSET, "CMD_COSCOFFSET", "oscillator offset (0x2D)", "", 0 },
-	{ CMD_CSETGPSTIME, "CMD_CSETGPSTIME", "set GPS time (0x2E)", "", 0 },
-	{ CMD_CUTCPARAM, "CMD_CUTCPARAM", "UTC parameters (0x2F)", "", 0 },
-	{ CMD_CACCPOSXYZ, "CMD_CACCPOSXYZ", "accurate initial position (XYZ/ECEF) (0x31)", "", 0 },
-	{ CMD_CACCPOS, "CMD_CACCPOS", "accurate initial position (0x32)", "", 0 },
-	{ CMD_CANALOGDIG, "CMD_CANALOGDIG", "analog to digital (0x33)", "", 0 },
-	{ CMD_CSAT1SAT, "CMD_CSAT1SAT", "satellite for 1-Sat mode (0x34)", "", 0 },
-	{ CMD_CIOOPTIONS, "CMD_CIOOPTIONS", "I/O options (0x35)", "", 0 },
-	{ CMD_CVELOCAID, "CMD_CVELOCAID", "velocity aiding of acquisition (0x36)", "", 0 },
-	{ CMD_CSTATLSTPOS, "CMD_CSTATLSTPOS", "status and values of last pos. and vel. (0x37)", "", 0 },
-	{ CMD_CLOADSSATDT, "CMD_CLOADSSATDT", "load satellite system data (0x38)", "", 0 },
-	{ CMD_CSATDISABLE, "CMD_CSATDISABLE", "satellite disable (0x39)", "", 0 },
-	{ CMD_CLASTRAW, "CMD_CLASTRAW", "last raw measurement (0x3A)", "", 0 },
-	{ CMD_CSTATSATEPH, "CMD_CSTATSATEPH", "satellite ephemeris status (0x3B)", "", 0 },
-	{ CMD_CSTATTRACK, "CMD_CSTATTRACK", "tracking status (0x3C)", "", 0 },
-	{ CMD_CCHANADGPS, "CMD_CCHANADGPS", "configure channel A for differential GPS (0x3D)", "", 0 },
-	{ CMD_CADDITFIX, "CMD_CADDITFIX", "additional fix data (0x3E)", "", 0 },
-	{ CMD_CDGPSFIXMD, "CMD_CDGPSFIXMD", "set/request differential GPS position fix mode (0x62)", "", 0 },
-	{ CMD_CDGPSCORR, "CMD_CDGPSCORR", "differential correction status (0x65)", "", 0 },
-	{ CMD_CPOSFILT, "CMD_CPOSFILT", "position filter parameters (0x71)", "", 0 },
-	{ CMD_CHEIGHTFILT, "CMD_CHEIGHTFILT", "height filter control (0x73)", "", 0 },
-	{ CMD_CHIGH8CNT, "CMD_CHIGH8CNT", "high-8 (best 4) / high-6 (overdetermined) control (0x75)", "", 0 },
-	{ CMD_CMAXDGPSCOR, "CMD_CMAXDGPSCOR", "maximum rate of DGPS corrections (0x77)", "", 0 },
-	{ CMD_CSUPER, "CMD_CSUPER", "super paket (0x8E)", "", 0 },
-	{ 0xFF, "", "", "", 0 }
-};
 
 
 cmd_info_t trimble_rcmds[] = {
