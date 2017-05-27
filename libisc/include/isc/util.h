a46c0905178e87aa042bf8be56f754b3a4d299be
Gary E. Miller <gem@rellim.com>
2017-05-26 18:36:13 -0700

    libisc: remove cruft from isc/util.h, fix includes.

--- libisc/include/isc/util.h
+++ libisc/include/isc/util.h
@@ -37,35 +37,6 @@
 #define UNUSED(x)      (void)(x)
 
 /*%
- * The opposite: silent warnings about stored values which are never read.
- */
-#define POST(x)        (void)(x)
-
-#define ISC_MAX(a, b)  ((a) > (b) ? (a) : (b))
-#define ISC_MIN(a, b)  ((a) < (b) ? (a) : (b))
-
-/*%
- * Use this to remove the const qualifier of a variable to assign it to
- * a non-const variable or pass it as a non-const function argument ...
- * but only when you are sure it won't then be changed!
- * This is necessary to sometimes shut up some compilers
- * (as with gcc -Wcast-qual) when there is just no other good way to avoid the
- * situation.
- */
-#define DE_CONST(konst, var) \
-	do { \
-		union { const void *k; void *v; } _u; \
-		_u.k = konst; \
-		var = _u.v; \
-	} while (0)
-
-/*%
- * Use this in translation units that would otherwise be empty, to
- * suppress compiler warnings.
- */
-#define EMPTY_TRANSLATION_UNIT extern void exit(int);
-
-/*%
  * We use macros instead of calling the routines directly because
  * the capital letters make the locking stand out.
  * We RUNTIME_CHECK for success since in general there's no way
@@ -86,28 +57,11 @@
  */
 #include "isc/list.h"		/* Contractual promise. */
 
-#define LIST(type)			ISC_LIST(type)
-#define INIT_LIST(type)			ISC_LIST_INIT(type)
-#define LINK(type)			ISC_LINK(type)
-#define INIT_LINK(elt, link)		ISC_LINK_INIT(elt, link)
-#define HEAD(list)			ISC_LIST_HEAD(list)
-#define TAIL(list)			ISC_LIST_TAIL(list)
-#define EMPTY(list)			ISC_LIST_EMPTY(list)
-#define PREV(elt, link)			ISC_LIST_PREV(elt, link)
-#define NEXT(elt, link)			ISC_LIST_NEXT(elt, link)
-#define APPEND(list, elt, link)		ISC_LIST_APPEND(list, elt, link)
-#define PREPEND(list, elt, link)	ISC_LIST_PREPEND(list, elt, link)
-#define UNLINK(list, elt, link)		ISC_LIST_UNLINK(list, elt, link)
-#define ENQUEUE(list, elt, link)	ISC_LIST_APPEND(list, elt, link)
-#define DEQUEUE(list, elt, link)	ISC_LIST_UNLINK(list, elt, link)
-#define INSERTBEFORE(li, b, e, ln)	ISC_LIST_INSERTBEFORE(li, b, e, ln)
-#define INSERTAFTER(li, a, e, ln)	ISC_LIST_INSERTAFTER(li, a, e, ln)
-#define APPENDLIST(list1, list2, link)	ISC_LIST_APPENDLIST(list1, list2, link)
 
 /*
  * Assertions
  */
-#include <isc/assertions.h>	/* Contractual promise. */
+#include "isc/assertions.h"	/* Contractual promise. */
 
 /*% Require Assertion */
 #define REQUIRE(e)			ISC_REQUIRE(e)
@@ -121,7 +75,7 @@
 /*
  * Errors
  */
-#include <isc/error.h>		/* Contractual promise. */
+#include "isc/error.h"		/* Contractual promise. */
 
 /*% Unexpected Error */
 #define UNEXPECTED_ERROR		isc_error_unexpected
