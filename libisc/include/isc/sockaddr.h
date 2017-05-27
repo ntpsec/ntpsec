a4ac7815787c1ff0bd0ca231e149156ef6de5705
Gary E. Miller <gem@rellim.com>
2017-05-26 18:59:10 -0700

    libisc: remove cruft from isc/sockaddr.h

--- libisc/include/isc/sockaddr.h
+++ libisc/include/isc/sockaddr.h
@@ -24,15 +24,4 @@ struct isc_sockaddr {
 	ISC_LINK(struct isc_sockaddr)	link;
 };
 
-typedef ISC_LIST(struct isc_sockaddr)	isc_sockaddrlist_t;
-
-#define ISC_SOCKADDR_CMPADDR	  0x0001	/*%< compare the address
-						 *   sin_addr/sin6_addr */
-#define ISC_SOCKADDR_CMPPORT 	  0x0002	/*%< compare the port
-						 *   sin_port/sin6_port */
-#define ISC_SOCKADDR_CMPSCOPE     0x0004	/*%< compare the scope
-						 *   sin6_scope */
-#define ISC_SOCKADDR_CMPSCOPEZERO 0x0008	/*%< when comparing scopes
-						 *   zero scopes always match */
-
 #endif /* GUARD_ISC_SOCKADDR_H */
