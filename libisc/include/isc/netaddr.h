d79c93a8092d0dd4f30adae0a5146176add8f3fd
Gary E. Miller <gem@rellim.com>
2017-05-26 18:50:36 -0700

    libisc: remove cruft from libisc/netaddr.h

--- libisc/include/isc/netaddr.h
+++ libisc/include/isc/netaddr.h
@@ -29,18 +29,6 @@ struct isc_netaddr {
 bool
 isc_netaddr_eqprefix(const isc_netaddr_t *, const isc_netaddr_t *,
 		     unsigned int) __attribute__((pure));
-/*%<
- * Format a human-readable representation of the network address '*na'
- * into the character array 'array', which is of size 'size'.
- * The resulting string is guaranteed to be null-terminated.
- */
-
-#define ISC_NETADDR_FORMATSIZE \
-	sizeof("xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:XXX.XXX.XXX.XXX%SSSSSSSSSS")
-/*%<
- * Minimum size of array to pass to isc_netaddr_format().
- */
-
 void
 isc_netaddr_fromsockaddr(isc_netaddr_t *netaddr, const isc_sockaddr_t *source);
 
@@ -50,66 +38,13 @@ isc_netaddr_fromin(isc_netaddr_t *netaddr, const struct in_addr *ina);
 void
 isc_netaddr_fromin6(isc_netaddr_t *netaddr, const struct in6_addr *ina6);
 
-isc_result_t
-isc_netaddr_frompath(isc_netaddr_t *netaddr, const char *path);
-
 void
 isc_netaddr_setzone(isc_netaddr_t *netaddr, uint32_t zone);
-
-void
-isc_netaddr_any(isc_netaddr_t *netaddr);
-/*%<
- * Return the IPv4 wildcard address.
- */
-
-void
-isc_netaddr_any6(isc_netaddr_t *netaddr);
-/*%<
- * Return the IPv6 wildcard address.
- */
-
-bool
-isc_netaddr_ismulticast(isc_netaddr_t *na);
-/*%<
- * Returns true if the address is a multicast address.
- */
-
-bool
-isc_netaddr_isexperimental(isc_netaddr_t *na);
-/*%<
- * Returns true if the address is a experimental (CLASS E) address.
- */
 
 bool
 isc_netaddr_islinklocal(isc_netaddr_t *na) __attribute__((pure));
 /*%<
  * Returns #true if the address is a link local address.
- */
-
-bool
-isc_netaddr_issitelocal(isc_netaddr_t *na);
-/*%<
- * Returns #true if the address is a site local address.
- */
-
-void
-isc_netaddr_fromv4mapped(isc_netaddr_t *t, const isc_netaddr_t *s);
-/*%<
- * Convert an IPv6 v4mapped address into an IPv4 address.
- */
-
-isc_result_t
-isc_netaddr_prefixok(const isc_netaddr_t *na, unsigned int prefixlen);
-/*
- * Test whether the netaddr 'na' and 'prefixlen' are consistent.
- * e.g. prefixlen within range.
- *      na does not have bits set which are not covered by the prefixlen.
- *
- * Returns:
- *	ISC_R_SUCCESS
- *	ISC_R_RANGE		prefixlen out of range
- *	ISC_R_NOTIMPLEMENTED	unsupported family
- *	ISC_R_FAILURE		extra bits.
  */
 
 ISC_LANG_ENDDECLS
