f32540d5bfc14a60faadd019d9a947d1a5c2b097
Gary E. Miller <gem@rellim.com>
2017-06-02 16:36:54 -0700

    assert: move the few parts of backtrace.c that work into assert.c
    Next libisc/backtrace.c goes bye-bye.

--- libntp/assert.c
+++ libntp/assert.c
@@ -19,6 +19,102 @@
 #include "isc/backtrace.h"
 #include "isc/result.h"
 
+#ifdef USEBACKTRACE
+
+/*
+ * Getting a back trace of a running process is tricky and highly platform
+ * dependent.  Our current approach is as follows:
+ * 1. If the system library supports the "backtrace()" function, use it.
+ *    OS X support this starting at with SDK 10.5.  glibc since version 2.1
+ * 2. Otherwise, if unwind.h exists then use the __Unwind_Backtrace() function.
+ *    This function is available on Linux, OS X, and FreeBSD.  It is defined
+ *    in Linux Standard Base since version 4.1
+ * 3. Otherwise, tough luck.
+ */
+
+/*
+ * The maximum number of stack frames to dump on assertion failure.
+ */
+#ifndef BACKTRACE_MAXFRAME
+#define BACKTRACE_MAXFRAME 128
+#endif
+
+
+# ifdef HAVE_BACKTRACE_SYMBOLS_FD
+#include <execinfo.h>
+
+void backtrace_log(void);
+
+void
+backtrace_log(void) {
+	int j, nptrs;
+	void *buffer[BACKTRACE_MAXFRAME];
+	char **strings;
+
+	nptrs = backtrace(buffer, BACKTRACE_MAXFRAME);
+	strings = backtrace_symbols(buffer, nptrs);
+	msyslog(LOG_ERR, "Stack trace:\n");
+	if (strings) {
+	   /* skip trace of this shim function */
+	   for (j = 1; j < nptrs; j++)
+	       msyslog(LOG_ERR, "  %s\n", strings[j]);
+
+	   free(strings);
+	}
+}
+# elif defined(HAVE__UNWIND_BACKTRACE)
+#include <unwind.h>
+
+typedef struct {
+	void **result;
+	int max_depth;
+	int skip_count;
+	int count;
+} trace_arg_t;
+
+static _Unwind_Reason_Code
+btcallback(struct _Unwind_Context *uc, void *opq) {
+	trace_arg_t *arg = (trace_arg_t *)opq;
+
+	if (arg->skip_count > 0)
+		arg->skip_count--;
+	else
+		arg->result[arg->count++] = (void *)_Unwind_GetIP(uc);
+	if (arg->count == arg->max_depth)
+		return (5); /* _URC_END_OF_STACK */
+
+	return (0); /* _URC_NO_REASON */
+}
+
+isc_result_t backtrace_log(void);
+
+isc_result_t
+backtrace_log(void) {
+	trace_arg_t arg;
+	void *buffer[BACKTRACE_MAXFRAME];
+	int i;
+
+	arg.skip_count = 1;
+	arg.result = buffer;
+	arg.max_depth = BACKTRACE_MAXFRAME;
+	arg.count = 0;
+	_Unwind_Backtrace(btcallback, &arg);
+
+	msyslog(LOG_ERR, "Stack trace:\n");
+	/* skip trace of this shim function */
+	for (i = 1; i < arg.count; i++) {
+	    msyslog(LOG_ERR, "#%d %p in ??\n", i, buffer[i]);
+	}
+
+	return (ISC_R_SUCCESS);
+}
+# else
+#  define(BACKTRACE_DISABLED)
+# endif
+#else   /* ! USEBACKTRACE */
+# define(BACKTRACE_DISABLED)
+#endif /* USEBACKTRACE */
+
 static const char *
 assertion_typetotext(assertiontype_t type)
 			__attribute__((const));
@@ -69,6 +165,9 @@ assertion_failed(
 
 	msyslog(LOG_ERR, "%s:%d: %s(%s) failed",
 		file, line, assertion_typetotext(type), cond);
+#ifndef BACKTRACE_DISABLED
+        backtrace_log();
+#endif
 	msyslog(LOG_ERR, "exiting (due to assertion failure)");
 
 	abort();
