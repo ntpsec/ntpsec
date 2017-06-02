a040a1fdd05993a7c71d1b41451c054108a52bd5
Gary E. Miller <gem@rellim.com>
2017-06-02 14:29:52 -0700

    assert: remove default_callback().
    It was only used until assertion_failed() was installed early in
    startup.  Probably never worked, did not use mysyslog().

--- libisc/assertions.c
+++ libisc/assertions.c
@@ -27,12 +27,6 @@
 #endif
 
 /*%
- * Forward.
- */
-static void
-default_callback(const char *, int, isc_assertiontype_t, const char *);
-
-/*%
  * Public.
  */
 
@@ -66,34 +60,7 @@ isc_assertion_typetotext(isc_assertiontype_t type) {
 }
 
 /*
- * Private.
- */
-
-static void
-default_callback(const char *file, int line, isc_assertiontype_t type,
-		 const char *cond)
-{
-	void *tracebuf[BACKTRACE_MAXFRAME];
-	int i, nframes = 0;
-	const char *logsuffix = ".";
-	isc_result_t result;
-
-	result = isc_backtrace_gettrace(tracebuf, BACKTRACE_MAXFRAME, &nframes);
-	if (result == ISC_R_SUCCESS && nframes > 0)
-		logsuffix = ", back trace";
-
-	fprintf(stderr, "%s:%d: %s(%s) failed%s\n",
-		file, line, isc_assertion_typetotext(type), cond, logsuffix);
-	if (result == ISC_R_SUCCESS) {
-		for (i = 0; i < nframes; i++) {
-			fprintf(stderr, "#%d %p in ??\n", i, tracebuf[i]);
-		}
-	}
-	fflush(stderr);
-}
-
-/*
- * assertion_failed - Redirect assertion failures to msyslog().
+ * assertion_failed - Redirect assertion failed output to msyslog().
  */
 void
 assertion_failed(
