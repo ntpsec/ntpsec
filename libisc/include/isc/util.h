3cf718806a258333f50e3046813c2d9bdfa03704
Gary E. Miller <gem@rellim.com>
2017-06-05 20:19:01 -0700

    libisc: remove unused TIME_NOW().

--- libisc/include/isc/util.h
+++ libisc/include/isc/util.h
@@ -68,9 +68,4 @@
 /* hack to ignore GCC Unused Result */
 #define ISC_IGNORE(r) do{if(r){}}while(0)
 
-/*%
- * Time
- */
-#define TIME_NOW(tp) 	RUNTIME_CHECK(isc_time_now((tp)) == ISC_R_SUCCESS)
-
 #endif /* GUARD_ISC_UTIL_H */
