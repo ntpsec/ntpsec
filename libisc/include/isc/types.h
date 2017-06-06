f806e83c32df49dd45acd83b73ec4128b57fc879
Gary E. Miller <gem@rellim.com>
2017-06-06 12:20:12 -0700

    libisc: remove unused isc_*_t types.

--- libisc/include/isc/types.h
+++ libisc/include/isc/types.h
@@ -15,21 +15,11 @@
 
 /* Core Types.  Alphabetized by defined type. */
 
-typedef struct isc_backtrace_symmap	isc_backtrace_symmap_t; /*%< Symbol Table Entry */
-typedef struct isc_buffer		isc_buffer_t;		/*%< Buffer */
 typedef struct isc_interface		isc_interface_t;	/*%< Interface */
 typedef struct isc_interfaceiter	isc_interfaceiter_t;	/*%< Interface Iterator */
-typedef struct isc_log 			isc_log_t;		/*%< Log */
-typedef struct isc_logcategory		isc_logcategory_t;	/*%< Log Category */
-typedef struct isc_interval		isc_interval_t;		/*%< Interval */
-typedef struct isc_logconfig		isc_logconfig_t;	/*%< Log Configuration */
-typedef struct isc_logmodule		isc_logmodule_t;	/*%< Log Module */
 typedef struct isc_mem			isc_mem_t;		/*%< Memory */
-typedef struct isc_msgcat		isc_msgcat_t;		/*%< Message Catalog */
 typedef struct isc_netaddr		isc_netaddr_t;		/*%< Net Address */
 typedef unsigned int			isc_result_t;		/*%< Result */
 typedef struct isc_sockaddr		isc_sockaddr_t;		/*%< Socket Address */
-typedef struct isc_socket		isc_socket_t;		/*%< Socket */
-typedef struct isc_time			isc_time_t;		/*%< Time */
 
 #endif /* GUARD_ISC_TYPES_H */
