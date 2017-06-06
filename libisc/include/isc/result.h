73d791ba5a136a601d3e14367069a95b6f1993f8
Gary E. Miller <gem@rellim.com>
2017-06-06 12:34:12 -0700

    libisc: move isc_result_t into isc/result.h.
    isc/reulst.h is where it is used.

--- libisc/include/isc/result.h
+++ libisc/include/isc/result.h
@@ -8,15 +8,17 @@
 #ifndef GUARD_ISC_RESULT_H
 #define GUARD_ISC_RESULT_H 1
 
-/*! \file isc/result.h */
+/* isc/result.h */
 
-#define ISC_R_SUCCESS			0	/*%< success */
-#define ISC_R_NOMEMORY			1	/*%< out of memory */
-#define ISC_R_NOTFOUND			23	/*%< not found */
-#define ISC_R_FAILURE			25	/*%< generic failure */
-#define ISC_R_NOTIMPLEMENTED		27	/*%< not implemented */
-#define ISC_R_NOMORE			29	/*%< no more */
-#define ISC_R_UNEXPECTED		34	/*%< unexpected error */
-#define ISC_R_IGNORE			36	/*%< ignore */
+typedef unsigned int			isc_result_t;	/* Result */
+
+#define ISC_R_SUCCESS			0	/* success */
+#define ISC_R_NOMEMORY			1	/* out of memory */
+#define ISC_R_NOTFOUND			23	/* not found */
+#define ISC_R_FAILURE			25	/* generic failure */
+#define ISC_R_NOTIMPLEMENTED		27	/* not implemented */
+#define ISC_R_NOMORE			29	/* no more */
+#define ISC_R_UNEXPECTED		34	/* unexpected error */
+#define ISC_R_IGNORE			36	/* ignore */
 
 #endif /* GUARD_ISC_RESULT_H */
