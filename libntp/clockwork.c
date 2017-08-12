cc9491c82a7f411f1c610e5b1edc76abcaddd512
Eric S. Raymond <esr@thyrsus.com>
2017-08-12 14:34:06 -0400

    Drop support for OS X 10.11.  Apple screwed this pooch too many different ways.
    Last straw was the ahipped headers in some versions not matching what's on
    the Apple website (Report from Leonardo Bruni Schenkel.)
    
    We also have intelligence that the clock_setting shim in some builds
    of 10.11 is broken.
    
    10.12 has clock_gettime(2) and clock_settime(2); it should be OK.

--- libntp/clockwork.c
+++ libntp/clockwork.c
@@ -19,44 +19,6 @@
 #include "ntp_debug.h"
 #include "ntp_syscall.h"
 
-/*
- * Simulate ANSI/POSIX conformance on platforms that don't have it
- */
-#ifndef HAVE_CLOCK_GETTIME
-#ifdef __MACH__
-#include <mach/clock.h>
-#include <mach/mach.h>
-#endif
-
-int clock_gettime(clockid_t clk_id, struct timespec *tp)
-{
-#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
-    clock_serv_t cclock;
-    mach_timespec_t mts;
-    int mode;
-    switch (clk_id) {
-    case CLOCK_REALTIME:
-	mode = CALENDAR_CLOCK;
-	break;
-    case CLOCK_MONOTONIC:
-	/* http://stackoverflow.com/questions/11680461/monotonic-clock-on-osx */
-	mode = SYSTEM_CLOCK;
-	break;
-    default:
-	return -1;
-    }
-    host_get_clock_service(mach_host_self(), mode, &cclock);
-    clock_get_time(cclock, &mts);
-    mach_port_deallocate(mach_task_self(), cclock);
-    tp->tv_sec = mts.tv_sec;
-    tp->tv_nsec = mts.tv_nsec;
-#else
-#error POSIX clock_gettime(2) is required
-#endif
-    return 0;
-}
-#endif /* HAVE_CLOCK_GETTIME */
-
 #ifdef HAVE_SYS_TIMEX_H
 /*
  * ntp_adjtime at nanosecond precision.  Hiding the units difference
