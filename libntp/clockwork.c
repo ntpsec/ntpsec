d903983f0dba8e9c048caf5bfdfe7c26a6f7f186
Fred Wright <fw@fwright.net>
2017-09-13 14:14:53 -0700

    Fixes clock_gettime()/clock_settime() fallbacks.
    This replaces the broken and incomplete OSX-specific clock_gettime()
    fallback for a partially generic version based on gettimeofday().  The
    fallback for CLOCK_MONOTONIC is still OSX-specific (but without the
    former port leak bug).  It also adds generic fallback for
    clock_settime() based on settimeofday().
    
    Using gettimeofday()/settimeofday() is actually optimal on OSX, in
    spite of what one might expect of clock_get_time() and
    clock_set_time().
    
    OSX builds get a couple of warnings on 10.6, and many on 10.5, but
    these are unrelated to the change.
    
    TESTED:
    Builds and runs on OSX 10.5-10.12, including 10.5 PPC.

--- libntp/clockwork.c
+++ libntp/clockwork.c
@@ -1,7 +1,8 @@
 /*
- * clockwork.c - the interface to the hardare clock
+ * clockwork.c - the interface to the hardware clock
  */
 
+#include <errno.h>
 #include <unistd.h>
 
 #include "config.h"
@@ -18,6 +19,81 @@
 #include "lib_strbuf.h"
 #include "ntp_debug.h"
 #include "ntp_syscall.h"
+
+/*
+ * Simulate ANSI/POSIX conformance on platforms that don't have it
+ */
+#ifndef HAVE_CLOCK_GETTIME
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif
+
+int clock_gettime(clockid_t clk_id, struct timespec *tp)
+{
+    struct timeval tv;
+
+    switch (clk_id) {
+    case CLOCK_REALTIME:
+	/*
+	 * On OSX, it's tempting to use clock_get_time() for its apparent
+	 * nanosecond resolution, but it really only has microsecond
+	 * resolution, and is substantially slower than gettimeofday().
+	 */
+	if (gettimeofday(&tv, NULL))
+	    return -1;
+	tp->tv_sec = tv.tv_sec;
+	tp->tv_nsec = tv.tv_usec * 1000;
+	return 0;
+    case CLOCK_MONOTONIC:
+#ifdef __MACH__
+	/*
+	 * mach_absolute_time() with proper scaling would be faster, but is
+	 * more directly analogous to CLOCK_MONOTONIC_RAW, and constancy of
+	 * the scale factors would need to be investigated.
+	 */
+	 {
+	    mach_timespec_t mts;
+	    mach_port_t mach_host;
+	    static clock_serv_t sclock = 0; // Cached SYSTEM_CLOCK port
+
+	    if (!sclock) {
+		mach_host= mach_host_self();
+		host_get_clock_service(mach_host, SYSTEM_CLOCK, &sclock);
+		mach_port_deallocate(mach_task_self(), mach_host);
+	    }
+	    clock_get_time(sclock, &mts);
+	    tp->tv_sec = mts.tv_sec;
+	    tp->tv_nsec = mts.tv_nsec;
+	    return 0;
+	}
+#else /* !__MACH__ */
+#error POSIX clock_gettime(2) is required for CLOCK_MONOTONIC
+#endif /* !__MACH__ */
+    default:
+	errno = EINVAL;
+	return -1;
+    }
+}
+#endif /* !HAVE_CLOCK_GETTIME */
+
+#ifndef HAVE_CLOCK_SETTIME
+
+int clock_settime(clockid_t clk_id, const struct timespec *tp)
+{
+    struct timeval tv;
+
+    switch (clk_id) {
+    case CLOCK_REALTIME:
+	tv.tv_sec = tp->tv_sec;
+	tv.tv_usec = (tp->tv_nsec + 500) / 1000;
+	return settimeofday(&tv, NULL);
+    default:
+	errno = EINVAL;
+	return -1;
+    }
+}
+#endif /* !HAVE_CLOCK_SETTIME */
 
 #ifdef HAVE_SYS_TIMEX_H
 /*
@@ -80,14 +156,10 @@ ntp_set_tod(
 	int		saved_errno;
 
 	TPRINT(1, ("In ntp_set_tod\n"));
-#ifdef HAVE_CLOCK_SETTIME
 	errno = 0;
 	rc = clock_settime(CLOCK_REALTIME, tvs);
 	saved_errno = errno;
 	TPRINT(1, ("ntp_set_tod: clock_settime: %d %m\n", rc));
-#else
-#error POSIX clock_settime(2) is required
-#endif /* HAVE_CLOCK_SETTIME */
 	errno = saved_errno;	/* for %m below */
 	TPRINT(1, ("ntp_set_tod: Final result: clock_settime: %d %m\n", rc));
 
