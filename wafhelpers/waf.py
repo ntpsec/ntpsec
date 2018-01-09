4e998125b833c47e1508fe79d8466f15007fe035
Richard Laager <rlaager@wiktel.com>
2018-01-08 19:46:40 -0600

    Provide default subst behavior for manpages
    The manpage build step provides a custom substitution function.
    Currently, waf provides no way to chain up to the default beahvior, so
    we must copy that code into our custom function.

--- wafhelpers/waf.py
+++ wafhelpers/waf.py
@@ -1,5 +1,7 @@
+import re
+
 from waflib.Configure import conf
-from waflib.TaskGen import feature, before_method
+from waflib.TaskGen import before_method, feature, re_m4
 
 
 @before_method('apply_incpaths')
@@ -16,8 +18,39 @@ def insert_srcdir(self):
     self.includes += ["%s/include" % srcnode]
 
 
-def manpage_subst_fun(task, text):
-    return text.replace("include::../docs/", "include::../../../docs/")
+def manpage_subst_fun(self, code):
+    # Since we are providing a custom substitution method, we must implement
+    # the default behavior, since we want that too.
+    global re_m4
+
+    # replace all % by %% to prevent errors by % signs
+    code = code.replace('%', '%%')
+
+    # extract the vars foo into lst and replace @foo@ by %(foo)s
+    lst = []
+    def repl(match):
+        g = match.group
+        if g(1):
+            lst.append(g(1))
+            return "%%(%s)s" % g(1)
+        return ''
+    code = getattr(self.generator, 're_m4', re_m4).sub(repl, code)
+
+    try:
+        d = self.generator.dct
+    except AttributeError:
+        d = {}
+        for x in lst:
+            tmp = getattr(self.generator, x, '') or self.env[x] or self.env[x.upper()]
+            try:
+                tmp = ''.join(tmp)
+            except TypeError:
+                tmp = str(tmp)
+            d[x] = tmp
+
+    code = code % d
+
+    return code.replace("include::../docs/", "include::../../../docs/")
 
 
 @conf
