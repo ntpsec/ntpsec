ed5d672c8bfa90f2ac659df9a16ef51c4bc52c43
Ian Bruene <ianbruene@gmail.com>
2018-07-30 23:26:55 +0000

    Added test for refid_str.

--- tests/libntp/numtoa.c
+++ tests/libntp/numtoa.c
@@ -25,7 +25,19 @@ TEST(numtoa, Netmask) {
 	TEST_ASSERT_EQUAL_STRING("255.255.255.0", numtoa(input));
 }
 
+TEST(numtoa, RefidStr) {
+	const char *res;
+
+	// Test stratum > 1
+	res = refid_str(0x11223344, 8);
+	TEST_ASSERT_EQUAL_STRING("68.51.34.17", res);
+	// Test !(stratum > 1)
+	res = refid_str(0x535047, 0);
+	TEST_ASSERT_EQUAL_STRING(".GPS.", res);
+}
+
 TEST_GROUP_RUNNER(numtoa) {
 	RUN_TEST_CASE(numtoa, Address);
 	RUN_TEST_CASE(numtoa, Netmask);
+	RUN_TEST_CASE(numtoa, RefidStr);
 }
