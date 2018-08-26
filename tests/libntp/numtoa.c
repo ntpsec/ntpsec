94fc06ece8beeed90b0c445d9a4f5d33a3804ccf
Fred Wright <fw@fwright.net>
2018-08-25 17:13:39 -0700

    Fixes refid_str tests for big-endian machines.
    TESTED:
    Tests now pass on both x86 and PPC.

--- tests/libntp/numtoa.c
+++ tests/libntp/numtoa.c
@@ -15,10 +15,10 @@ TEST(numtoa, RefidStr) {
 	const char *res;
 
 	// Test stratum > 1
-	res = refid_str(0x11223344, 8);
+	res = refid_str(htonl(0x44332211), 8);
 	TEST_ASSERT_EQUAL_STRING("68.51.34.17", res);
 	// Test !(stratum > 1)
-	res = refid_str(0x535047, 0);
+	res = refid_str(htonl(0x47505300), 0);
 	TEST_ASSERT_EQUAL_STRING(".GPS.", res);
 }
 
