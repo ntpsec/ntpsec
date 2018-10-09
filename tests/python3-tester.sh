faa798fa9b60ea2a9413717ede6dbe80cf0e9242
Gary E. Miller <gem@rellim.com>
2018-10-09 16:37:36 -0700

    tests/python3-tester.sh: remove bash-isms
    Many OS do not use bash.  Sometimes do to license issues.

--- tests/python3-tester.sh
+++ tests/python3-tester.sh
@@ -1,9 +1,14 @@
-#!/usr/bin/env bash
-# sh on NetBSD and FreeBSD says:
-#   sh: ${PIPESTATUS[...}: Bad substitution
+#!/bin/sh
+# keep this POSIX sh compatible.  No bash-isms!
+# replace |& with 2>&1 |
 
 # This is a clone of option-tester.sh
 # to build with python3 and do minimal (version) testing.
+
+# set pipefail to catch pipeline failures
+set -o pipefail
+# crash on unset shell variables
+set -u
 
 if [ "`which python3 2>/dev/null`" = "" ]
 then
@@ -16,47 +21,47 @@ doit ()
   DIR=test-$1
   [ ! -d $DIR ] && mkdir $DIR
   rm -rf $DIR/*
-  python3 ./waf configure --out=$DIR $2  |& tee    $DIR/test.log
-  WAF1=${PIPESTATUS[0]}
+  python3 ./waf configure --out=$DIR $2 2>&1 | tee    $DIR/test.log
+  WAF1=$?
   WAF2=0
   WAF3=0
   if [ "$WAF1" = 0 ]
   then
-  echo                                   |& tee -a $DIR/test.log
-  python3 ./waf build                    |& tee -a $DIR/test.log
-  WAF2=${PIPESTATUS[0]}
+  echo                            2>&1   | tee -a $DIR/test.log
+  python3 ./waf build             2>&1   | tee -a $DIR/test.log
+  WAF2=$?
   if [ "$WAF2" = 0 ]
   then
-  echo                                   |& tee -a $DIR/test.log
-  python3 ./waf check                    |& tee -a $DIR/test.log
-  WAF3=${PIPESTATUS[0]}
+  echo                            2>&1   | tee -a $DIR/test.log
+  python3 ./waf check             2>&1   | tee -a $DIR/test.log
+  WAF3=$?
   fi
   fi
   if [ "$WAF1" != 0 -o "$WAF2" != 0 -o "$WAF3" != 0 ] 
   then
-    echo                                 |& tee -a $DIR/test.log
-    echo "Trouble with $DIR"             |& tee -a $DIR/test.log
+    echo                          2>&1   | tee -a $DIR/test.log
+    echo "Trouble with $DIR"      2>&1   | tee -a $DIR/test.log
   else
-    echo -n "VERSION: "                                  |& tee -a $DIR/test.log
-    ./$DIR/main/ntpd/ntpd --version                      |& tee -a $DIR/test.log
-    echo -n "VERSION: "                                  |& tee -a $DIR/test.log
-    python3 ./$DIR/main/ntpclients/ntpq --version        |& tee -a $DIR/test.log
-    echo -n "VERSION: "                                  |& tee -a $DIR/test.log
-    python3 ./$DIR/main/ntpclients/ntpdig --version      |& tee -a $DIR/test.log
+    echo -n "VERSION: "                             2>&1 | tee -a $DIR/test.log
+    ./$DIR/main/ntpd/ntpd --version                 2>&1 | tee -a $DIR/test.log
+    echo -n "VERSION: "                             2>&1 | tee -a $DIR/test.log
+    python3 ./$DIR/main/ntpclients/ntpq --version   2>&1 | tee -a $DIR/test.log
+    echo -n "VERSION: "                             2>&1 | tee -a $DIR/test.log
+    python3 ./$DIR/main/ntpclients/ntpdig --version 2>&1 | tee -a $DIR/test.log
     if [ `uname -s` != "NetBSD" ]
     then
       # no Python/curses on NetBSD
-      echo -n "VERSION: "                                |& tee -a $DIR/test.log
-      python3 ./$DIR/main/ntpclients/ntpmon --version    |& tee -a $DIR/test.log
+      echo -n "VERSION: "                           2>&1 | tee -a $DIR/test.log
+      python3 ./$DIR/main/ntpclients/ntpmon --version 2>&1 | tee -a $DIR/test.log
     fi
     # if [ "`which gpsmon 2>/dev/null`" != "" ]
     # then
     # needs GPSD library - don't know how to test for python3 version
-    # echo -n "VERSION: "                                |& tee -a $DIR/test.log
-    # python3 ./$DIR/main/ntpclients/ntploggps --version |& tee -a $DIR/test.log
+    # echo -n "VERSION: "                                2>&1 | tee -a $DIR/test.log
+    # python3 ./$DIR/main/ntpclients/ntploggps --version 2>&1 | tee -a $DIR/test.log
     # fi
-    echo -n "VERSION: "                                  |& tee -a $DIR/test.log
-    python3 ./$DIR/main/ntpclients/ntplogtemp --version  |& tee -a $DIR/test.log
+    echo -n "VERSION: "                                 2>&1 | tee -a $DIR/test.log
+    python3 ./$DIR/main/ntpclients/ntplogtemp --version 2>&1 | tee -a $DIR/test.log
   fi
   echo
   echo
