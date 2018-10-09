a75366dc536f030dba5935ecbf20f76ffc7b021e
Gary E. Miller <gem@rellim.com>
2018-10-09 16:38:22 -0700

    tests/option-tester.sh: remove bash-isms
    Many OS do not use bash.  Sometimes do to license issues.

--- tests/option-tester.sh
+++ tests/option-tester.sh
@@ -1,12 +1,17 @@
-#!/usr/bin/env bash
-# sh on NetBSD and FreeBSD says:
-#   sh: ${PIPESTATUS[...}: Bad substitution
+#!/bin/sh
+# keep this POSIX sh compatible. No bash-isms!
+# replace |& with 2>&1 |
 
 # This is a hack to build with various configuration options.
 # The intent is to check building combinations that normal testing doesn't use.
 
 # Stuff goes into various test-* directories.
 # Running again starts by deleting everything in the directory.
+
+# set pipefail to catch pipeline failures
+set -o pipefail
+# crash on unset shell variables
+set -u
 
 LINUX=""
 if [ `uname -s` = "Linux" -a -f /usr/include/seccomp.h ]
@@ -20,47 +25,47 @@ doit ()
   DIR=test-$1
   [ ! -d $DIR ] && mkdir $DIR
   rm -rf $DIR/*
-  ./waf configure --out=$DIR $2          |& tee    $DIR/test.log
-  WAF1=${PIPESTATUS[0]}
+  ./waf configure --out=$DIR $2 2>&1 | tee    $DIR/test.log
+  WAF1=$?
   WAF2=0
   WAF3=0
   if [ "$WAF1" = 0 ]
   then
-  echo                                   |& tee -a $DIR/test.log
-  ./waf build                            |& tee -a $DIR/test.log
-  WAF2=${PIPESTATUS[0]}
+  echo                           2>&1    | tee -a $DIR/test.log
+  ./waf build                    2>&1    | tee -a $DIR/test.log
+  WAF2=$?
   if [ "$WAF2" = 0 ]
   then
-  echo                                   |& tee -a $DIR/test.log
-  ./waf check                            |& tee -a $DIR/test.log
-  WAF3=${PIPESTATUS[0]}
+  echo                           2>&1    | tee -a $DIR/test.log
+  ./waf check                    2>&1    | tee -a $DIR/test.log
+  WAF3=$?
   fi
   fi
   if [ "$WAF1" != 0 -o "$WAF2" != 0 -o "$WAF3" != 0 ] 
   then
-    echo                                         |& tee -a $DIR/test.log
-    echo "Trouble with $DIR"                     |& tee -a $DIR/test.log
+    echo                                  2>&1   | tee -a $DIR/test.log
+    echo "Trouble with $DIR"              2>&1   | tee -a $DIR/test.log
   else
-    echo -n "VERSION: "                          |& tee -a $DIR/test.log
-    ./$DIR/main/ntpd/ntpd --version              |& tee -a $DIR/test.log
-    echo -n "VERSION: "                          |& tee -a $DIR/test.log
-    ./$DIR/main/ntpclients/ntpq --version        |& tee -a $DIR/test.log
-    echo -n "VERSION: "                          |& tee -a $DIR/test.log
-    ./$DIR/main/ntpclients/ntpdig --version      |& tee -a $DIR/test.log
+    echo -n "VERSION: "                   2>&1   | tee -a $DIR/test.log
+    ./$DIR/main/ntpd/ntpd --version       2>&1   | tee -a $DIR/test.log
+    echo -n "VERSION: "                   2>&1   | tee -a $DIR/test.log
+    ./$DIR/main/ntpclients/ntpq --version 2>&1   | tee -a $DIR/test.log
+    echo -n "VERSION: "                   2>&1   | tee -a $DIR/test.log
+    ./$DIR/main/ntpclients/ntpdig --version 2>&1 | tee -a $DIR/test.log
     if [ `uname -s` != "NetBSD" ]
     then
       # no Python/curses on NetBSD
-      echo -n "VERSION: "                        |& tee -a $DIR/test.log
-      ./$DIR/main/ntpclients/ntpmon --version    |& tee -a $DIR/test.log
+      echo -n "VERSION: "                 2>&1   | tee -a $DIR/test.log
+      ./$DIR/main/ntpclients/ntpmon --version 2>&1 | tee -a $DIR/test.log
     fi
 if [ "`which gpsmon 2>/dev/null`" != "" ]
 then
     # needs GPSD library
-    echo -n "VERSION: "                          |& tee -a $DIR/test.log
-    ./$DIR/main/ntpclients/ntploggps --version   |& tee -a $DIR/test.log
+    echo -n "VERSION: "                         2>&1 | tee -a $DIR/test.log
+    ./$DIR/main/ntpclients/ntploggps --version  2>&1 | tee -a $DIR/test.log
 fi
-    echo -n "VERSION: "                          |& tee -a $DIR/test.log
-    ./$DIR/main/ntpclients/ntplogtemp --version  |& tee -a $DIR/test.log
+    echo -n "VERSION: "                         2>&1 | tee -a $DIR/test.log
+    ./$DIR/main/ntpclients/ntplogtemp --version 2>&1 | tee -a $DIR/test.log
   fi
   echo
   echo
