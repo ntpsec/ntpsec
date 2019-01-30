c084543006bbb3925413eb9690a1d5ee7e1d276d
Eric S. Raymond <esr@thyrsus.com>
2019-01-30 17:21:42 -0500

    REmove superfluous comments.

--- include/ascii.h
+++ include/ascii.h
@@ -1,6 +1,4 @@
 /*
- * Created: Sun Jul 20 11:42:53 1997
- *
  * Copyright (c) 1997-2005 by Frank Kardel <kardel <AT> ntp.org>
  * Copyright 2015 by the NTPsec project contributors
  * SPDX-License-Identifier: BSD-3-clause
@@ -18,23 +16,3 @@
 #define DLE	 16
 
 #endif
-/*
- * History:
- *
- * ascii.h,v
- * Revision 4.4  2005/04/16 17:32:10  kardel
- * update copyright
- *
- * Revision 4.3  2004/11/14 15:29:41  kardel
- * support PPSAPI, upgrade Copyright to Berkeley style
- *
- * Revision 4.1  1998/07/11 10:05:22  kardel
- * Release 4.0.73d reconciliation
- *
- * Revision 4.0  1998/04/10 19:50:38  kardel
- * Start 4.0 release version numbering
- *
- * Revision 4.0  1998/04/10 19:50:38  kardel
- * Start 4.0 release version numbering
- *
- */
