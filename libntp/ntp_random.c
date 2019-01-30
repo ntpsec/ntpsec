cdb90975b71e46f9438ffa6c1449b615c7c882cd
Eric S. Raymond <esr@thyrsus.com>
2019-01-30 18:48:51 -0500

    The Regents of the University of California have deprecated the 4-clause licence.

--- libntp/ntp_random.c
+++ libntp/ntp_random.c
@@ -2,7 +2,7 @@
  * Copyright (c) 1983, 1993
  *	The Regents of the University of California.  All rights reserved.
  * Copyright 2015 by the NTPsec project contributors
- * SPDX-License-Identifier: BSD-4-clause
+ * SPDX-License-Identifier: BSD-2-clause
  */
 
 #include <stdint.h>
