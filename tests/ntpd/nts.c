e17ae135616a568ce2a0c6ee8640942e5a86e980
Ian Bruene <ianbruene@gmail.com>
2019-02-14 18:26:33 -0600

    Added test for nts_append_uint16.

--- tests/ntpd/nts.c
+++ tests/ntpd/nts.c
@@ -91,6 +91,7 @@ TEST(nts, nts_append_record_bytes) {
 }
 
 TEST(nts, nts_append_header) {
+  // Test change
   // Setup
   uint8_t buf[16];
   BufCtl cursor;
@@ -106,6 +107,41 @@ TEST(nts, nts_append_header) {
   TEST_ASSERT_EQUAL(buf[4], 0);
   TEST_ASSERT_EQUAL(cursor.next, &buf[4]);
   TEST_ASSERT_EQUAL(cursor.left, 12);
+  // Test no change
+  // Setup
+  cursor.left = 0;
+  // Run test
+  nts_append_header(&cursor, 0xFEED, 0xABCD);
+  // Check
+  TEST_ASSERT_EQUAL(buf[4], 0);
+  TEST_ASSERT_EQUAL(cursor.next, &buf[4]);
+  TEST_ASSERT_EQUAL(cursor.left, 0);
+}
+
+TEST(nts, nts_append_uint16) {
+  // Test change
+  // Setup
+  uint8_t buf[16] = {0, 0, 0, 0, 0, 0, 0, 0,
+					 0, 0, 0, 0, 0, 0, 0, 0};
+  BufCtl cursor;
+  cursor.next = buf;
+  cursor.left = 16;
+  // Run test
+  nts_append_uint16(&cursor, 0x1234);
+  // Check
+  TEST_ASSERT_EQUAL(buf[0], 0x12);
+  TEST_ASSERT_EQUAL(buf[1], 0x34);
+  TEST_ASSERT_EQUAL(cursor.next, &buf[2]);
+  TEST_ASSERT_EQUAL(cursor.left, 14);
+  // Test no change
+  // Setup
+  cursor.left = 0;
+  // Run test
+  nts_append_uint16(&cursor, 0x5678);
+  // Check
+  TEST_ASSERT_EQUAL(buf[2], 0);
+  TEST_ASSERT_EQUAL(cursor.next, &buf[2]);
+  TEST_ASSERT_EQUAL(cursor.left, 0);
 }
 
 TEST_GROUP_RUNNER(nts) {
@@ -113,4 +149,5 @@ TEST_GROUP_RUNNER(nts) {
   RUN_TEST_CASE(nts, nts_append_record_uint16);
   RUN_TEST_CASE(nts, nts_append_record_bytes);
   RUN_TEST_CASE(nts, nts_append_header);
+  RUN_TEST_CASE(nts, nts_append_uint16);
 }
