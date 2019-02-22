429d8c0e9c5647bb079b73ca1b8125eabdbca972
Hal Murray <murray@shuksan.example.com>
2019-02-22 00:02:15 -0800

    Rearrange order of ntpq nts printout

--- ntpclients/ntpq.py
+++ ntpclients/ntpq.py
@@ -1494,11 +1494,11 @@ usage: authinfo
             ("nts_client_send",      "NTS client sends:         ", NTP_INT),
             ("nts_client_recv",      "NTS client recvs:         ", NTP_INT),
             ("nts_client_recv_bad",  "NTS client recvs w error: ", NTP_INT),
-            ("nts_server_send",      "NTS server sends:         ", NTP_INT),
             ("nts_server_recv",      "NTS server recvs:         ", NTP_INT),
             ("nts_server_recv_bad",  "NTS server recvs w error: ", NTP_INT),
-            ("nts_ke_serves",        "NTS KE serves:            ", NTP_INT),
+            ("nts_server_send",      "NTS server sends:         ", NTP_INT),
             ("nts_ke_probes",        "NTS KE probes:            ", NTP_INT),
+            ("nts_ke_serves",        "NTS KE serves:            ", NTP_INT),
         )
         self.collect_display(associd=0, variables=ntsinfo, decodestatus=False)
 
