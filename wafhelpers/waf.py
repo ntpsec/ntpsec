a266f40f9ea2541d5304560c45bf6bcbc4384cee
Richard Laager <rlaager@wiktel.com>
2020-02-02 22:59:08 -0600

    waf: Remove commented-out code
    This has not been used in a long time, but seems to keep getting
    re-arranged with various changes.  It was originally commented out on
    Dec 4 2016 with changes due to "NTPSEC_RELEASE".

--- wafhelpers/waf.py
+++ wafhelpers/waf.py
@@ -41,9 +41,6 @@ def manpage_subst_fun(self, code):
 @conf
 def manpage(ctx, section, source):
 
-    # ctx.install_files('${MANDIR}' + "/man%s/" % section,
-    #                   source.replace("-man.adoc", ".%s" % section))
-
     if not ctx.env.ARGS_MAN or ctx.env.DISABLE_MANPAGE:
         return
 
