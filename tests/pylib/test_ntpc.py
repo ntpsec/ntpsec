e2d545844bd6a9e89b8f862e22c70a77ac420a85
James Browning <jamesb.fe80@gmail.com>
2022-05-02 15:32:38 -0500

    ntp.packet: Add test and diagnostic info dump to triage nul-trunc

--- tests/pylib/test_ntpc.py
+++ tests/pylib/test_ntpc.py
@@ -2,6 +2,8 @@
 # SPDX-License-Identifier: BSD-2-Clause
 import unittest
 import ntp.ntpc
+import ntp.poly
+import ntp.util
 
 
 class TestPylibNtpc(unittest.TestCase):
@@ -27,6 +29,21 @@ class TestPylibNtpc(unittest.TestCase):
         for (in_string, to_float, to_string) in self.lfp_set:
             self.assertEqual(ntp.ntpc.prettydate(in_string), to_string)
             self.assertAlmostEqual(ntp.ntpc.lfptofloat(in_string), to_float)
+
+    def test_nul_trunc(self):
+        k_type = "aes-128"
+        key = ntp.util.hexstr2octets(
+            "0fd2287c1e97a50cb9d3cb9f80debcb6")
+        sample = ntp.util.hexstr2octets(
+            "e3000000000000000000000000000000" +
+            "00000000000000000000000000000000" +
+            "0000000000000000e6136a712d8c4800" +
+            "00000064" +
+            "01c167a4df28f25a4794bf00c8b35469")
+        mac2 = ntp.ntpc.mac(ntp.poly.polybytes(sample[:48]),
+                            ntp.poly.polybytes(key), k_type)
+        mac1 = ntp.poly.polybytes(sample[52:])
+        self.assertEqual([len(mac1), mac1], [len(mac2), mac2], msg="nul trunc")
 
 
 if __name__ == '__main__':
