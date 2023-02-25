42a0de82ba48bdb607c5e500cb22dd873d2f97f1
Hal Murray <hmurray@megapathdsl.net>
2023-02-25 04:01:31 -0800

    Added AES test vectors from RFC 4493

--- tests/libntp/macencrypt.c
+++ tests/libntp/macencrypt.c
@@ -2,6 +2,12 @@
 #include "ntp_stdlib.h"
 #include "ntp_auth.h"
 
+// Note that NTP doesn't use RFCs 1321 or 4631
+
+// RFC 4493: The AES-CMAC Algorithm
+//   https://www.rfc-editor.org/rfc/rfc4493
+
+
 #include "unity.h"
 #include "unity_fixture.h"
 
@@ -222,6 +228,85 @@ TEST(macencrypt, null_trunc) {
 	);
 }
 
+/* Test vectors from RFC 4493
+ * Note: All tests use the same message.
+ * Shorter tests only use the front part of it.
+ * The message gets copied into buffer because cmac_encrypt
+ * trashes the buffer by appending the MAC.
+ */
+TEST(macencrypt, CMAC_TestVectors) {
+	const unsigned char M[64] = {
+		0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96,
+		0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a,
+		0xae, 0x2d, 0x8a, 0x57, 0x1e, 0x03, 0xac, 0x9c,
+		0x9e, 0xb7, 0x6f, 0xac, 0x45, 0xaf, 0x8e, 0x51,
+		0x30, 0xc8, 0x1c, 0x46, 0xa3, 0x5c, 0xe4, 0x11,
+		0xe5, 0xfb, 0xc1, 0x19, 0x1a, 0x0a, 0x52, 0xef,
+		0xf6, 0x9f, 0x24, 0x45, 0xdf, 0x4f, 0x9b, 0x17,
+		0xad, 0x2b, 0x41, 0x7b, 0xe6, 0x6c, 0x37, 0x10
+	};
+	unsigned char key[16] = {
+		0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
+		0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c
+	};
+	const unsigned char mac0[16] = {
+//    AES-CMAC       bb1d6929 e9593728 7fa37d12 9b756746
+		0xbb, 0x1d, 0x69, 0x29,  0xe9, 0x59, 0x37, 0x28,
+		0x7f, 0xa3, 0x7d, 0x12,  0x9b, 0x75, 0x67, 0x46
+	};
+	const unsigned char mac16[16] = {
+//    AES-CMAC       070a16b4 6b4d4144 f79bdd9d d04a287c
+		0x07, 0x0a, 0x16, 0xb4,  0x6b, 0x4d, 0x41, 0x44,
+		0xf7, 0x9b, 0xdd, 0x9d,  0xd0, 0x4a, 0x28, 0x7c
+	};
+	const unsigned char mac40[16] = {
+//    AES-CMAC       dfa66747 de9ae630 30ca3261 1497c827
+		0xdf, 0xa6, 0x67, 0x47,  0xde, 0x9a, 0xe6, 0x30,
+		0x30, 0xca, 0x32, 0x61,  0x14, 0x97, 0xc8, 0x27
+	};
+	const unsigned char mac64[16] = {
+//    AES-CMAC       51f0bebf 7e3b9d92 fc497417 79363cfe
+		0x51, 0xf0, 0xbe, 0xbf,  0x7e, 0x3b, 0x9d, 0x92,
+		0xfc, 0x49, 0x74, 0x17,  0x79, 0x36, 0x3c, 0xfe
+	};
+	unsigned char buffer[64+4+16];
+	int length;
+
+	auth.keyid = 100;
+	auth.type = AUTH_CMAC;
+	auth.digest = NULL;
+	auth.key = (uint8_t *)key;
+	auth.key_size = (unsigned short)sizeof(key);
+
+#if OPENSSL_VERSION_NUMBER > 0x20000000L
+	auth.mac_ctx = Setup_MAC_CTX("AES-128-CBC", auth.key, auth.key_size);
+	TEST_ASSERT_NOT_NULL(auth.mac_ctx);
+#else
+	auth.cipher = EVP_get_cipherbyname("AES-128-CBC");
+	TEST_ASSERT_NOT_NULL(auth.cipher);
+#endif
+
+	memcpy(buffer, M, 0);
+	length = cmac_encrypt(&auth, (uint32_t*)buffer, 0);
+	TEST_ASSERT_EQUAL(4+16, length);
+	TEST_ASSERT_EQUAL_MEMORY(mac0, buffer+0+4, 16);
+
+	memcpy(buffer, M, 16);
+	length = cmac_encrypt(&auth, (uint32_t*)buffer, 16);
+	TEST_ASSERT_EQUAL(4+16, length);
+	TEST_ASSERT_EQUAL_MEMORY(mac16, buffer+16+4, 16);
+
+	memcpy(buffer, M, 40);
+	length = cmac_encrypt(&auth, (uint32_t*)buffer, 40);
+	TEST_ASSERT_EQUAL(4+16, length);
+	TEST_ASSERT_EQUAL_MEMORY(mac40, buffer+40+4, 16);
+
+	memcpy(buffer, M, 64);
+	length = cmac_encrypt(&auth, (uint32_t*)buffer, 64);
+	TEST_ASSERT_EQUAL(4+16, length);
+	TEST_ASSERT_EQUAL_MEMORY(mac64, buffer+64+4, 16);
+}
+
 /* Both digest and CMAC tests share some global variables
  * that get setup by Encrypt or CMAC_Encrypt
  * Thus the tests must be run in the right order.
@@ -231,9 +316,11 @@ TEST_GROUP_RUNNER(macencrypt) {
 	RUN_TEST_CASE(macencrypt, DecryptValid);
 	RUN_TEST_CASE(macencrypt, DecryptInvalid);
 	RUN_TEST_CASE(macencrypt, CMAC_Encrypt);
+	RUN_TEST_CASE(macencrypt, CMAC_Encrypt);
 	RUN_TEST_CASE(macencrypt, DecryptValidCMAC);
 	RUN_TEST_CASE(macencrypt, DecryptInvalidCMAC);
 	RUN_TEST_CASE(macencrypt, IPv4AddressToRefId);
 	RUN_TEST_CASE(macencrypt, IPv6AddressToRefId);
-	RUN_TEST_CASE(macencrypt, null_trunc)
+	RUN_TEST_CASE(macencrypt, null_trunc);
+	RUN_TEST_CASE(macencrypt, CMAC_TestVectors);
 }
