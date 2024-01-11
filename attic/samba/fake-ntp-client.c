ntpsec/ntpsec

commit 97fce42188073783c260a490042d183bf57bb32e

Add attic/samba/mssntp-blaster.c
It can generate lots of traffic to measure performance.

attic/samba/fake-ntp-client.c
@@ -4,7 +4,9 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-/*  Last modified on Sat Jan  6 00:11:51 PST 2001 by murray  */
+ * fake-ntp-client.c: generate NTP packets for ntpd+samba to sign.
+ * See README.
+ */
 
 #include <sys/types.h>
 #include <unistd.h>
@@ -22,9 +24,9 @@
 
 #include "fake.h"
 
-static char* host = "time.example.com";// First arg
-static int packets = 1;		// Second arg
-static useconds_t delay = 1000;		// Third arg, microseconds after each packet
+static char* host = "time.example.com"; // First arg
+static int packets = 1;	                // Second arg
+static useconds_t delay = 1000;	        // Third arg, microseconds after each packet
 
 static int sock;
 static struct ntp_packet ntp_send, ntp_recv;

attic/samba/Makefile
@@ -5,7 +5,7 @@
 # Hi
 
 
-PROGS = fake-samba fake-ntp-server fake-ntp-client
+PROGS = fake-samba fake-ntp-server fake-ntp-client mssntp-blaster
 
 # Compiler flags
 CFLAGS = -O1 -Wall -Wstrict-prototypes -Wmissing-prototypes
@@ -24,5 +24,8 @@ fake-ntp-server: fake.h fake-ntp-server.c
 
 fake-ntp-client: fake.h fake-ntp-client.c
 	cc $(CFLAGS) -g -o fake-ntp-client fake-ntp-client.c
+
+mssntp-blaster: mssntp-blaster.c
+	cc $(CFLAGS) -g -o mssntp-blaster mssntp-blaster.c
 
 

attic/samba/README
@@ -39,6 +39,8 @@ Your ntp.conf will need something like this:
 
 Note that ntpd adds "/socket" to the name from the config file.
 
+mssntp-blaster can send lots of traffic.
+ntpq has an mssntpinfo command to print out ntpd's statistics.
 
 These are simple hacks.  Look at the code and fix it to do what you want.
 

attic/samba/fake-ntp-server.c
@@ -5,9 +5,10 @@
  */
 
 /* fake-ntp-server.c
- * fake NTP server to exersize fake-samba
+ * fake-ntp-server.c -- generate traffic for fake-samba
  * it won't work with real samba since we don't know any key-IDs
  * so all it is good for is debugging fake-samba
+ * See README
  */
 #include <errno.h>
 #include <string.h>

attic/samba/fake-samba.c
@@ -6,6 +6,7 @@
 
 /*
  * fake-samba.c -- fake samba server to exercise MS-SNTP option.
+ * See README
  */
 #include <errno.h>
 #include <string.h>

attic/samba/mssntp-blaster.c
@@ -1,0 +1,138 @@
+/*
+ * mssntp-blaster.c:
+ *     hack to generate mssntp traffic so you can measure throughput.
+ * ntpq has an mssntpinfo command.
+ */
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#include "fake.h"
+
+char* host = "localhost";	// First arg
+int keyid = 6;			// Second arg
+int delay = 15;			// Third arg
+int packets = 10;		// Fourth arg
+
+int sock;
+
+struct ntp_packet ntp_send, ntp_recv;
+
+int ntp_port_number = 123;
+
+/* NTP time starts 1900, unix time starts 1970 */
+int ntp_to_unix = 0x7c558180;
+int unix_to_ntp = 0x83aa7e80;
+
+#define DATELENGTH 100
+static void bailout(char *msg)
+{
+  int saverrr = errno;
+  char timetxt[DATELENGTH];
+  time_t nowbin;
+  struct tm nowstruct;
+  time(&nowbin);
+  localtime_r(&nowbin, &nowstruct);
+  strftime(timetxt, DATELENGTH, "%Y-%b-%d %H:%M", &nowstruct);
+  printf("** %s %s: errno = %d, %s\n",
+    timetxt, msg, saverrr, strerror(saverrr));
+  sleep(10);  /* Beware of loops in scripts. */
+  exit(1);
+};
+
+int main (int argc, char *argv[])
+{
+  char *hostname;
+  struct hostent *target;
+  struct sockaddr_in server, client;
+  struct timespec delay_spec, now;
+  float timeout = 5.0;
+  int addr;
+  int i, ec, len, saved_errno;
+
+  if (argc > 4) packets = atoi(argv[4]);
+  if (packets < 0) bailout("Bad packets");
+
+  if (argc > 3) delay = atoi(argv[3]);
+  if (delay < 0) bailout("Bad delay");
+
+  if (argc > 2) keyid = atoi(argv[2]);
+  if (keyid < 0 || keyid > 0xFFFFFFFF) bailout("Bad key_id");
+
+  hostname = "localhost";
+  if (argc > 1) hostname = argv[1];
+  target = gethostbyname(hostname);
+  if (target == NULL) bailout("Bad hostname");
+
+  bcopy((char *)target->h_addr, (char *)&addr, sizeof(addr));
+  addr = ntohl(addr);
+  printf("Connecting to %s=>%d.%d.%d.%d\n",
+    hostname,
+    (addr >> 24) & 0xff,
+    (addr >> 16) & 0xff,
+    (addr >> 8) & 0xff,
+    (addr >> 0) & 0xff);
+
+  if (0) printf("Sending: len = %ld, header=%8x\n",
+    sizeof(ntp_send), ntp_send.header); 
+
+  bzero((char *)&server, sizeof(server));
+  bcopy((char *)target->h_addr, (char *)&server.sin_addr, target->h_length);
+  server.sin_family = target->h_addrtype;
+  server.sin_port = htons(ntp_port_number);
+
+  sock = socket(PF_INET, SOCK_DGRAM, 0);
+  if (sock < 0) bailout("socket");
+
+  bzero((char *)&client, sizeof(client));
+  client.sin_family = AF_INET;
+  client.sin_addr.s_addr = htonl(INADDR_ANY);
+  client.sin_port = htons(0);
+  
+  ec = bind(sock, (struct sockaddr *)&client, sizeof(client) );
+  if (ec) bailout("bind");
+  
+  ec = connect(sock, (struct sockaddr *)&server, sizeof(server) );
+  if (ec) bailout("connect");
+
+  delay_spec.tv_sec = (int)timeout;
+  delay_spec.tv_nsec = (timeout-delay_spec.tv_sec)*1E9;
+  setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, \
+    (char*)&delay_spec, sizeof(delay_spec));
+
+  ntp_send.header = htonl(0x23000000);
+  ntp_send.keyid = keyid;
+
+  for (i = 0; i<packets; i++) {
+    clock_gettime(CLOCK_REALTIME, &now);
+    ntp_send.t3.seconds = htonl(now.tv_sec+unix_to_ntp);
+    ntp_send.t3.fraction = 0;
+    len = send(sock, &ntp_send, sizeof(ntp_send), 0);
+    if (len != sizeof(ntp_send)) bailout("send");
+    printf("Sent one\n");
+    len = recv(sock, &ntp_recv, sizeof(ntp_recv), 0);
+    saved_errno = errno;
+    if (len == -1) {
+      /* probably timeout: EAGIN or EWOULDBLOCK */
+      printf("Error from recv: %s\n", strerror(saved_errno));
+    } else if (len != sizeof(ntp_recv)) {
+      printf("Wrong length from recv: %d\n", len);
+    } else {
+      printf("Got one.\n");
+    }
+    nanosleep(&delay_spec, NULL);
+  }
+  close(sock);
+  
+  return 0;
+}

commit 6386e59bbd336b23affa784b00bc7ecf113411ea

Fix missing start of multiline comment
This was broken in 97fce421 ("Add attic/samba/mssntp-blaster.c")

attic/samba/fake-ntp-client.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+/*
  * fake-ntp-client.c: generate NTP packets for ntpd+samba to sign.
  * See README.
  */