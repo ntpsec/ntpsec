98a3d24fe66f9c0f226669e6be64ab1b79fa9e13
Matt Selsky <matthew.selsky@twosigma.com>
2025-04-21 21:30:15 -0400

    Initialize `aes_key` variable as an array of bytes
    Fedora 42 x86_64 ships with a gcc 15.x snapshot that produces the following warning:
    ```
    ../../tests/libntp/authkeys.c:12:35: error: initializer-string for array of ‘unsigned char’ truncates NUL terminator but destination lacks ‘nonstring’ attribute (17 chars into 16 available) [-Werror=unterminated-string-initialization]
       12 | const unsigned char aes_key[16] = "0123456789abcdef";
          |
    ```
    
    We could use `__attribute__((nonstring))` to annotate the variable, but this is
    only supported by gcc and not by clang.
    
    I've tested this change on Fedora 42 x86_64 with gcc 15.0.1 and clang 20.1.2
    and there were no errors/warnings, and all tests passed.
    
    NTPsec/ntpsec#847

--- tests/libntp/authkeys.c
+++ tests/libntp/authkeys.c
@@ -9,7 +9,10 @@
 
 #include "ntp.h"
 
-const unsigned char aes_key[16] = "0123456789abcdef";
+const unsigned char aes_key[16] = {
+	'0', '1', '2', '3', '4', '5', '6', '7',
+	'8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+};
 
 TEST_GROUP(authkeys);
 
