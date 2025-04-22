977382e7632e4f893de654d3663f32b1b2d55ded
Hal Murray <halmurray@sonic.net>
2025-04-21 21:30:43 -0700

    Update PIVOT (missed for 1.2.4)

--- PIVOT.h
+++ PIVOT.h
@@ -1,4 +1,6 @@
-#define RELEASE_DATE 1703823396
+#define RELEASE_DATE 1745050673
+
+/* Sat Apr 19 08:17:53 AM UTC 2025 */
 
 /* We would like to use the build date.
  * But that breaks repeatable builds.
